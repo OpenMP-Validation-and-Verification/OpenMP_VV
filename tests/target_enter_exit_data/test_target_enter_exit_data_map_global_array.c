f03d459758e59f8951b45d4698d3e6d2edd7a6d9
Swaroop Pophale <pophaless@ornl.gov>
2017-10-12 17:29:29 -0400

    Adding test_target_enter_exit_data_map_global_array.c .

---
+++ tests/target_enter_exit_data/test_target_enter_exit_data_map_global_array.c
@@ -0,0 +1,49 @@
+// RUN: %libomptarget-compile-run-and-check-aarch64-unknown-linux-gnu
+// RUN: %libomptarget-compile-run-and-check-powerpc64-ibm-linux-gnu
+// RUN: %libomptarget-compile-run-and-check-powerpc64le-ibm-linux-gnu
+// RUN: %libomptarget-compile-run-and-check-x86_64-pc-linux-gnu
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+// Test for OpenMP 4.5 target enter and target exit data with global arrays.
+
+int n=10;
+int x[10];
+
+void init(){
+ #pragma omp target enter data map(to:x[:n])
+}
+
+void finalize(){
+ #pragma omp target exit data map(from:x[:n])
+}
+
+int main (){
+int isHost=-1,i,errors=0;
+
+init();
+
+#pragma omp target map(tofrom: isHost) map(to: n) 
+{
+ /*Record where the computation was executed*/
+ isHost = omp_is_initial_device();
+
+ for(i=0;i< n; i++)
+   x[i] = 10;
+}
+
+ finalize();
+ for(i=0; i<n; i++)
+   if(x[i] != 10){
+     errors += 1;
+   }
+
+  if (!errors)
+    printf("Test passed on %s\n", (isHost ? "host" : "device"));
+  else
+    printf("Test failed on %s\n", (isHost ? "host" : "device"));
+
+  return errors;
+}
