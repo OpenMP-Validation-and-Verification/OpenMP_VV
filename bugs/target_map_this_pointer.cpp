015a3d7c07043489ef2d0feeab2aca02119815f3
Jose Manuel Monsalve Diaz <josem@udel.edu>
2017-10-15 03:13:26 -0400

    applying the suggested solution to target_map_this_pointer.cpp to see if it is possible to compile it. Also adding a main function. This compiles but we don't know if it is desired

--- bugs/target_map_this_pointer.cpp
+++ bugs/target_map_this_pointer.cpp
@@ -1,4 +1,4 @@
-
+#include "ompvv.h"
 #include <iostream>
 #include <omp.h>
 #include <cassert>
@@ -17,14 +17,23 @@ public:
   Simple(int s) : size(s) { 
       sum = 0;
       h_array = new int[size];
-#pragma omp target enter data map(to:this)
+      Simple * this_ptr = this; // This is the offered solution
+#pragma omp target enter data map(to:this_ptr)
 #pragma omp target enter data map(to:h_array[0:size])
   }
 
   // TODO: Add virtual once supported
   ~Simple() { 
 #pragma omp target exit data map(delete:h_array[0:size])
-#pragma omp target exit data map(delete:this)
+      Simple * this_ptr = this; // This is the offered solution
+#pragma omp target exit data map(delete:this_ptr)
       delete[] h_array; 
   }
 };
+
+int main() {
+  OMPVV_TEST_OFFLOADING;
+  Simple mySimpleObj(10);
+  int errors = 0;
+  OMPVV_REPORT_AND_RETURN(errors);
+}
