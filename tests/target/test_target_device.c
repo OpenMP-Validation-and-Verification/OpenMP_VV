145e0e0ee0628cfc130394bfaf47d38a28ea797d
Jose Manuel Monsalve Diaz <josem@udel.edu>
2017-10-21 04:30:53 -0400

    Adding test target/test_target_device.c that makes sure that the device clause in target works fine

---
+++ tests/target/test_target_device.c
@@ -0,0 +1,59 @@
+// RUN: %libomptarget-compile-run-and-check-aarch64-unknown-linux-gnu
+// RUN: %libomptarget-compile-run-and-check-powerpc64-ibm-linux-gnu
+// RUN: %libomptarget-compile-run-and-check-powerpc64le-ibm-linux-gnu
+// RUN: %libomptarget-compile-run-and-check-x86_64-pc-linux-gnu
+
+#include <assert.h>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "ompvv.h"
+
+#define N 1000
+
+int test_target_device_clause() {
+  OMPVV_INFOMSG("test_target_device_clause");
+
+  // Get number of devices
+  int num_dev = omp_get_num_devices();
+  OMPVV_INFOMSG("num_devices tested = %d", num_dev);
+  
+  int array[N];
+  int errors = 0;
+  
+  // Array initialization
+  for (int dev = 0; dev < num_dev; ++dev) {
+    for (int i = 0; i < N; ++i) {
+      array[i] = -1;
+    }
+  }
+
+  // Map the same array to multiple devices. initialize with device number
+  for (int dev = 0; dev < num_dev; ++dev) {
+#pragma omp target enter data map(to: array[0:N]) device(dev)
+#pragma omp target map (alloc: array[0:N]) device(dev)
+    {
+      for (int i = 0; i < N; ++i) {
+        array[i] += dev + 1;
+      }
+    }
+  }
+
+  // recover the array from each of the device and make sure the value is dev;
+  for (int dev = 0; dev < num_dev; ++dev) {
+#pragma omp target exit data map(from: array[0:N]) device(dev) 
+    for (int i = 0; i < N; ++i) {
+      OMPVV_TEST_AND_SET(errors, array[i] != dev);
+    }
+  }
+
+  return errors;
+}
+
+int main() {
+  OMPVV_TEST_OFFLOADING;
+  int errors = 0;
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_target_device_clause());
+
+  OMPVV_REPORT_AND_RETURN(errors);
+}
