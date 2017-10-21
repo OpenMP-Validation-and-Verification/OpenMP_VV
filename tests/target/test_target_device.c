cbca99d38925c1bcff71feb2ac5c5bb95ccd1283
Jose Manuel Monsalve Diaz <josem@udel.edu>
2017-10-21 04:40:03 -0400

    simplifying the test to not use target enter/exit data. Instead just use a single loop and a target data

--- tests/target/test_target_device.c
+++ tests/target/test_target_device.c
@@ -22,28 +22,24 @@ int test_target_device_clause() {
   int errors = 0;
   
   // Array initialization
-  for (int dev = 0; dev < num_dev; ++dev) {
-    for (int i = 0; i < N; ++i) {
-      array[i] = -1;
-    }
+  for (int i = 0; i < N; ++i) {
+    array[i] = -1;
   }
 
   // Map the same array to multiple devices. initialize with device number
   for (int dev = 0; dev < num_dev; ++dev) {
-#pragma omp target enter data map(to: array[0:N]) device(dev)
-#pragma omp target map (alloc: array[0:N]) device(dev)
-    {
-      for (int i = 0; i < N; ++i) {
-        array[i] += dev + 1;
-      }
-    }
-  }
-
-  // recover the array from each of the device and make sure the value is dev;
-  for (int dev = 0; dev < num_dev; ++dev) {
-#pragma omp target exit data map(from: array[0:N]) device(dev) 
+#pragma omp target data map(tofrom: array[0:N]) device(dev)
+    { 
+    #pragma omp target map(alloc: array[0:N]) device(dev)
+      {
+        for (int i = 0; i < N; ++i) {
+          array[i] += dev + 1;
+        }
+      } // end of omp target 
+    } // end of omp target data
     for (int i = 0; i < N; ++i) {
       OMPVV_TEST_AND_SET(errors, array[i] != dev);
+      array[i] = -1;
     }
   }
 
