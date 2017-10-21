721cc3e41184bd2270155effb1f715fe5ec9e2d6
Jose Manuel Monsalve Diaz <josem@udel.edu>
2017-10-21 08:52:08 -0400

    Adding test defaultmap(tofrom: scalar). It will try to map different scalars with and without the clause. Scalars tried: char short int float double enum. enum is failing in xlc

---
+++ tests/target/test_target_defaultmap.c
@@ -0,0 +1,88 @@
+// RUN: %libomptarget-compile-run-and-check-aarch64-unknown-linux-gnu
+// RUN: %libomptarget-compile-run-and-check-powerpc64-ibm-linux-gnu
+// RUN: %libomptarget-compile-run-and-check-powerpc64le-ibm-linux-gnu
+// RUN: %libomptarget-compile-run-and-check-x86_64-pc-linux-gnu
+
+#include <assert.h>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "ompvv.h"
+
+int test_default_map_on() {
+  OMPVV_INFOMSG("test_default_map_on");
+
+  int errors = 0;
+
+  // we try with all the scalars
+  char scalar_char = 'a';
+  short scalar_short = 10;
+  int scalar_int = 11;
+  float scalar_float = 5.5f;
+  double scalar_double = 10.45;
+  enum { VAL1 = 1, VAL2, VAL3, VAL4} scalar_enum = VAL1;
+  
+
+  // Map the same array to multiple devices. initialize with device number
+#pragma omp target defaultmap(tofrom: scalar)
+  {
+    scalar_char = 'b';
+    scalar_short = 20;
+    scalar_int = 33;
+    scalar_float = 6.5f;
+    scalar_double = 20.45;
+    scalar_enum = VAL4;
+  } // end of omp target 
+
+  OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_char != 'b');
+  OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_short != 20);
+  OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_int != 33);
+  OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_float != 6.5f);
+  OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_double != 20.45);
+  OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_enum != VAL4);
+  
+  return errors;
+}
+
+int test_defaultmap_off() {
+  OMPVV_INFOMSG("test_defaultmap_off");
+  
+  int errors = 0;
+  
+    // we try with all the scalars
+    char scalar_char = 'a';
+    short scalar_short = 10;
+    int scalar_int = 11;
+    float scalar_float = 5.5f;
+    double scalar_double = 10.45;
+    enum { VAL1 = 1, VAL2, VAL3, VAL4} scalar_enum = VAL1;
+    
+  
+    // Map the same array to multiple devices. initialize with device number
+  #pragma omp target
+    {
+      scalar_char = 'b';
+      scalar_short = 20;
+      scalar_int = 33;
+      scalar_float = 6.5f;
+      scalar_double = 20.45;
+      scalar_enum = VAL4;
+    } // end of omp target 
+  
+    OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_char != 'a');
+    OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_short != 10);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_int != 11);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_float != 5.5f);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_double != 10.45);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_enum != VAL1);
+    
+    return errors;
+}
+int main() {
+  OMPVV_TEST_OFFLOADING;
+  int errors = 0;
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_default_map_on());
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_defaultmap_off());
+
+  OMPVV_REPORT_AND_RETURN(errors);
+}
