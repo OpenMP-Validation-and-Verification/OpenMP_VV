f4b2421740559c3c49af403969f4888a5da677cc
Swaroop Pophale <pophaless@ornl.gov>
2017-11-28 15:05:38 -0500

    Adding target/test_target_private.c . Does not work (gcc on Summitdev)

--- tests/target/test_target_private.c
+++ tests/target/test_target_private.c
@@ -3,7 +3,7 @@
 // RUN: %libomptarget-compile-run-and-check-powerpc64le-ibm-linux-gnu
 // RUN: %libomptarget-compile-run-and-check-x86_64-pc-linux-gnu
 
-//===--test_target_map_array_default.c - test default behavior of array map--===//
+//===--test_target_private.c ------------------------------------------------===//
 // 
 // OpenMP API Version 4.5 Nov 2015
 //
@@ -13,38 +13,41 @@
 #include <omp.h>
 #include <stdio.h>
 
-#define N 1000
+#define N 10
 
 int main() {
-  int compute_array[N];
+  int compute_array[4][N];
   int errors = 0;
-  int i,j, isHost = -1, tid;
+  int i,j, isHost = -1;//, p_val=-1;
   
  
-  for (i=0; i<N; i++) 
-    compute_array[i] = 0;
+  for (i=0; i<4; i++) 
+    for (j=0; j<N; j++) 
+      compute_array[i][j] = 0;
 
   omp_set_num_threads(4);
-#pragma omp target map(tofrom:compute_array) map(tofrom: isHost) private(tid)
+#pragma omp parallel
+{
+  int p_val=0;
+  int fp_val = omp_get_num_threads();
+#pragma omp target map(tofrom:compute_array,isHost) firstprivate(fp_val) private(p_val)
   {
+    p_val= fp_val;
     /*Record where the computation was executed*/
     isHost = omp_is_initial_device();
-    #pragma omp parallel private(i,tid)
-    {  
-      tid = omp_get_thread_num();
-      #pragma omp for
-      for (i = 0; i < N; i++)
-        compute_array[i] = tid;
-    }
+    for (i = 0; i < N; i++)
+      compute_array[p_val][i] = 100;
   } // End target
+}//end-parallel
 
-  for (i = 0; i < omp_get_num_threads(); i++)
-    for (j = 0; j < N/omp_get_num_threads(); j++)
-      if(compute_array[i*10+j] != i){
+  for (i=0; i<4; i++){ 
+    for (j=0; j<N; j++){
+      if(compute_array[i][j] != 100){
         errors += 1;
-        printf("compute_array[%d] = %d\n",i*10+j,compute_array[i*10+j]);
+        printf("compute_array[%d][%d] = %d\n",i,j,compute_array[i][j]);
       }
-
+    }
+  }//end-for
   if (errors) {
     printf("Test failed on %s\n",isHost ? "host":"device");
   }
