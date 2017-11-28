40de3d8ab2b432f1e7cc78bc1332bc7433d188ce
Jose Manuel Monsalve Diaz <josem@udel.edu>
2017-11-28 11:42:42 -0500

    Using bits to identify the tasks for test_target_enter_data_async to be able to point out which task was the one that didn't run

--- tests/target_enter_data/test_target_enter_data_async.c
+++ tests/target_enter_data/test_target_enter_data_async.c
@@ -10,44 +10,53 @@
 #include "ompvv.h"
 
 #define N 1000
+#define HOST_TASK1_BIT 0x1
+#define HOST_TASK2_BIT 0x2
+#define DEVICE_TASK1_BIT 0x4
+#define DEVICE_TASK2_BIT 0x8
+#define HOST_TASK3_BIT 0x10
+#define ALL_TASKS_BITS 0x1F
 
 /*
  * Test if it is possible to:
  * 1. target enter data to depend 'in' and 'out'
  * 2. target exit data to depend 'in' and 'out'
  * 3. Mix target-based tasks with host tasks.
+ *
+ * We use bits for each task to know where it fails
  */
 int test_async_between_task_target() {
   OMPVV_INFOMSG("test_async_between_task_target");
 
   int errors = 0;
   bool isHost = true;
-  double sum = 0.0;
-  double* h_array = (double *) malloc(N * sizeof(double));
-  double* h_array_copy = (double *) malloc(N * sizeof(double));
-  double* in_1 = (double *) malloc(N * sizeof(double));
-  double* in_2 = (double *) malloc(N * sizeof(double));
+  int sum = 0.0;
+  int* h_array = (int *) malloc(N * sizeof(int));
+  int* h_array_copy = (int *) malloc(N * sizeof(int));
+  int* in_1 = (int *) malloc(N * sizeof(int));
+  int* in_2 = (int *) malloc(N * sizeof(int));
   
-  // host task
+  // host task 1
 #pragma omp task depend(out: in_1) shared(in_1)
   {
     for (int i = 0; i < N; ++i) {
-      in_1[i] = 1;
+      in_1[i] = HOST_TASK1_BIT; // 0b01
     }
   }
 
-  // host task
+  // host task 2
 #pragma omp task depend(out: in_2) shared(in_2)
   {
     for (int i = 0; i < N; ++i) {
-      in_2[i] = 2;
+      in_2[i] = HOST_TASK2_BIT; // 0b10
     }
   }
 
   // target enter data
 #pragma omp target enter data map(alloc: h_array[0:N]) map(to: in_1[0:N]) map(to: in_2[0:N]) depend(out: h_array) depend(in: in_1) depend(in: in_2) 
 
-  // target task to compute on the device
+  // target task to compute on the device 
+  // device task 1
   // adding redundant depends on in_1 + in_2 to make the test work if compiled for the host
 #pragma omp task shared (isHost, h_array, in_1, in_2) depend(inout: h_array) depend(in: in_1) depend(in: in_2)
   {
@@ -55,32 +64,54 @@ int test_async_between_task_target() {
     {
       isHost = omp_is_initial_device();
       for (int i = 0; i < N; ++i) {
-        h_array[i] = in_1[i]*in_2[i];
+        h_array[i] = DEVICE_TASK1_BIT | in_1[i] | in_2[i]; // Expected = 0b111
       }
     }
   }
 
+  // Device task 2
 #pragma omp task shared (h_array, h_array_copy) depend(in: h_array) depend(out: h_array_copy)
   {
 #pragma omp target map(alloc: h_array[0:N]) map(from: h_array_copy[0:N])
     {
       for (int i = 0; i < N; ++i) {
-        h_array_copy[i] = h_array[i];
+        h_array_copy[i] = h_array[i] | DEVICE_TASK2_BIT; //  Expected = 0b1111
       }
     }
   }
 
-  // host task
+  // host task 3
 #pragma omp task depend(in: h_array_copy) shared(sum, h_array_copy)
   {
     // checking results
     for (int i = 0; i < N; ++i) {
-      sum += h_array_copy[i];
+      // Identify which task was problematic
+      h_array_copy[i] |= HOST_TASK3_BIT;
+      sum += (h_array_copy[i] & ALL_TASKS_BITS); // AND with 0b111 should produce sum
     }
   }
 #pragma omp taskwait
 
-  OMPVV_TEST_AND_SET(errors, (2.0*N != sum));
+  // We verify all the tasks without a task
+  int h_task1 = 0;
+  int h_task2 = 0;
+  int h_task3 = 0;
+  int d_task1 = 0;
+  int d_task2 = 0;
+  for (int i = 0; i < N; ++i) {
+    h_task1 |= !(h_array_copy[i] & HOST_TASK1_BIT);
+    h_task2 |= !(h_array_copy[i] & HOST_TASK2_BIT);
+    h_task2 |= !(h_array_copy[i] & HOST_TASK2_BIT);
+    d_task1 |= !(h_array_copy[i] & DEVICE_TASK1_BIT);
+    d_task2 |= !(h_array_copy[i] & DEVICE_TASK2_BIT);
+  }
+  OMPVV_ERROR_IF(h_task1 != 0, "Error in host task 1");
+  OMPVV_ERROR_IF(h_task2 != 0, "Error in host task 2");
+  OMPVV_ERROR_IF(h_task2 != 0, "Error in host task 2");
+  OMPVV_ERROR_IF(d_task1 != 0, "Error in device task 1");
+  OMPVV_ERROR_IF(d_task2 != 0, "Error in device task 2");
+
+  OMPVV_TEST_AND_SET(errors, (N * ALL_TASKS_BITS != sum));
   OMPVV_INFOMSG("Test test_async_between_task_target ran on the %s", (isHost ? "host" : "device"));
   
   return errors;
@@ -100,7 +131,7 @@ int test_async_between_target() {
   int sum = 0;
   int* h_array = (int *) malloc(N * sizeof(int));
   int* h_array_copy = (int *) malloc(N * sizeof(int));
-  int val = 2;
+  int val = DEVICE_TASK1_BIT;
 
   // target enter data
 #pragma omp target enter data map(alloc: h_array[0:N]) depend(out: h_array) 
@@ -111,24 +142,32 @@ int test_async_between_target() {
   {
     isHost = omp_is_initial_device();
     for (int i = 0; i < N; ++i) {
-      h_array[i] = val;
+      h_array[i] = val; // val = DEVICE_TASK1_BIT
     }
   }
 
 #pragma omp target map(alloc: h_array[0:N]) map(from: h_array_copy[0:N]) depend(in: h_array) depend(out: h_array_copy) 
   {
     for (int i = 0; i < N; ++i) {
-      h_array_copy[i] = h_array[i];
+      h_array_copy[i] = h_array[i] | DEVICE_TASK2_BIT;
     }
   }
 #pragma omp taskwait
 
   // checking results
+  int d_task1 = 0;
+  int d_task2 = 0;
   for (int i = 0; i < N; ++i) {
-    sum += h_array_copy[i];
+    sum += (h_array_copy[i] & (DEVICE_TASK1_BIT | DEVICE_TASK2_BIT)); // this will lead to 0b1100
+    // Checking each individual task
+    d_task1 |= !(h_array_copy[i] & DEVICE_TASK1_BIT);
+    d_task2 |= !(h_array_copy[i] & DEVICE_TASK2_BIT);
   }
+  // Report on each task
+  OMPVV_ERROR_IF(d_task1 != 0, "Error in device task 1");
+  OMPVV_ERROR_IF(d_task2 != 0, "Error in device task 2");
 
-  OMPVV_TEST_AND_SET(errors, (2.0*N != sum));
+  OMPVV_TEST_AND_SET(errors, (N * (DEVICE_TASK1_BIT | DEVICE_TASK2_BIT) != sum));
   OMPVV_INFOMSG("Test test_async_between_task_target ran on the %s", (isHost ? "host" : "device"));
 
   return errors;
