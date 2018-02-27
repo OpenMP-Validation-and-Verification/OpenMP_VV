b1fecdecee42df85b75e8f7a6a6d7a254e25f7ae
Jose Manuel Monsalve Diaz <josem@udel.edu>
2018-02-27 12:50:07 -0500

    adding stdlib.h to ompvv.h since EXIT_SUCCESS and EXIT_FAILURE are part of it

--- ompvv/ompvv.h
+++ ompvv/ompvv.h
@@ -5,6 +5,7 @@
 //===----------------------------------------------------------------------===//
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
