3c5705e5940f17bb02e5afd0a8fbecc0783157ae
Jose Manuel Monsalve Diaz <josem@udel.edu>
2018-03-12 21:23:45 -0400

    Multiple improvements to the test_target_teams_distribute_parallel_for_devices.c test
    * Adding OMPVV_INFOMSG for the number of devices.
    * Checking if all the different loops were executed in a device and not the hosts
    * printing where the test was executed in each hosts
    * Removing a bug in the second devices for loop. I had a <= and it should have been an <
    * other minor changes

--- tests/target_teams_distribute_parallel_for/test_target_teams_distribute_parallel_for_devices.c
+++ tests/target_teams_distribute_parallel_for/test_target_teams_distribute_parallel_for_devices.c
@@ -19,12 +19,15 @@ int test_target_teams_distribute_parallel_for_devices() {
   
   int num_dev = omp_get_num_devices();
   int a[SIZE_N];
+  int isHost[num_dev];
   int errors = 0;
   int i, dev;
 
   OMPVV_WARNING_IF(num_dev <= 1, "Testing devices clause without multiple devices");
+  
+  OMPVV_INFOMSG("Num of devices = %d", num_dev);
 
-  // a and b array initialization
+  // a array initialization
   for (i = 0; i < SIZE_N; i++) {
     a[i] = 1;
   }
@@ -33,19 +36,21 @@ int test_target_teams_distribute_parallel_for_devices() {
 #pragma omp target enter data map(to: a[0:SIZE_N]) device(dev)
   }
 
-  // check multiple sizes. 
-  for (dev = 0; dev <= num_dev; ++dev) {
-#pragma omp target teams distribute parallel for device(dev) \
-    map (alloc: a)
+  for (dev = 0; dev < num_dev; ++dev) {
+    // check multiple devices 
+#pragma omp target teams distribute parallel for device(dev) map(from: isHost)
     for (i = 0; i < SIZE_N; i++) {
-      a[i]++;
+      isHost[dev] = omp_is_initial_device();// Checking if running on a device
+      a[i] += dev;
     }
   }
 
   for (dev = 0; dev < num_dev; ++dev) {
 #pragma omp target exit data map(from: a[0:SIZE_N]) device(dev)
+    OMPVV_INFOMSG("Device %d ran on the %s", dev, isHost[dev] ? "host" : "device");
+    OMPVV_TEST_AND_SET(errors, isHost[dev]);
     for (i = 0; i < SIZE_N; i++) {
-      OMPVV_TEST_AND_SET(errors, a[i] != 2);
+      OMPVV_TEST_AND_SET(errors, a[i] != 1 + dev);
     }
   }
 
