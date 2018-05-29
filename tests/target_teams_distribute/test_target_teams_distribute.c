4eb2eaf5418bad602ace14388ceed8a250b4d738
Jose Manuel Monsalve Diaz <josem@udel.edu>
2018-05-29 16:59:02 -0400

    merging the test_target_teams_distribute.c test from the development branch

--- tests/target_teams_distribute/test_target_teams_distribute.c
+++ tests/target_teams_distribute/test_target_teams_distribute.c
@@ -3,49 +3,41 @@
 #include <stdlib.h>
 #include "ompvv.h"
 
-#define SIZE_THRESHOLD 512
+#define ARRAY_SIZE 1024
 
 // Test for OpenMP 4.5 target data with if
 int main() {
-  int isOffloading = 0;
-  OMPVV_TEST_AND_SET_OFFLOADING(isOffloading);
-  int a[1024];
-  int b[1024];
+  OMPVV_TEST_OFFLOADING;
+
+  int a[ARRAY_SIZE];
+  int b[ARRAY_SIZE];
   int num_teams = 0;
   int errors = 0;
   int is_host;
 
   // a and b array initialization
-  for (int x = 0; x < 1024; ++x) {
+  for (int x = 0; x < ARRAY_SIZE; ++x) {
       a[x] = 1;
       b[x] = x;
   }
 
-  #pragma omp target data map(tofrom: a[0:1024], num_teams, is_host) map(to: b[0:1024])
+  #pragma omp target data map(tofrom: a[0:ARRAY_SIZE], num_teams, is_host) map(to: b[0:ARRAY_SIZE])
   {
       #pragma omp target teams distribute
-      for (int x = 0; x < 1024; ++x){
+      for (int x = 0; x < ARRAY_SIZE; ++x){
           is_host = omp_is_initial_device();
           num_teams = omp_get_num_teams();
           a[x] += b[x];
       }
   }
 
-  for (int x = 0; x < 1024; ++x){
+  for (int x = 0; x < ARRAY_SIZE; ++x){
       OMPVV_TEST_AND_SET(errors, (a[x] != 1 + b[x]));
   }
 
 
-  if (!errors) {
-    OMPVV_INFOMSG("Test passed with offloading %s", (isOffloading ? "enabled" : "disabled"));
-    if (num_teams == 1){
-        OMPVV_WARNING("Test operated with one team.  Parallelism of teams distribute can't be guarunteed.");
-    }
-  } else if (!is_host) {
-    OMPVV_ERROR("Test failed on device with offloading %s.", (isOffloading ? "enabled" : "disabled"));
-  } else if (is_host) {
-    OMPVV_ERROR("Test failed on host with offloading %s.", (isOffloading ? "enabled" : "disabled"));
-  }
+  if (num_teams == 1)
+      OMPVV_WARNING("Test operated with one team.  Parallelism of teams distribute can't be guarunteed.");
 
   OMPVV_REPORT_AND_RETURN(errors);
 }
