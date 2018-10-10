a69edf5cdd5bae24ef426d6a03e9b5cbbcad6f31
Jose Manuel Monsalve Diaz <josem@udel.edu>
2018-10-10 19:36:55 -0400

    Adding test from development: tests/target_teams_distribute/test_target_teams_distribute_is_device_ptr.c

--- tests/target_teams_distribute/test_target_teams_distribute_is_device_ptr.c
+++ tests/target_teams_distribute/test_target_teams_distribute_is_device_ptr.c
@@ -1,3 +1,14 @@
+//===--- test_target_teams_distribute_is_device_ptr.c------------------------===//
+//
+// OpenMP API Version 4.5 Nov 2015
+//
+// This test uses is_device_ptr clause to indicate that the parameter that is
+// given is already an address to the data on the device.  The parameter is set
+// using the omp_target_alloc which returns the pointer to the location on the
+// target that was allocated.
+//
+////===----------------------------------------------------------------------===//
+
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,7 +17,6 @@
 #define SIZE_THRESHOLD 512
 #define ARRAY_SIZE 1024
 
-// Test for OpenMP 4.5 target data with if
 int main() {
   OMPVV_TEST_OFFLOADING;
   OMPVV_INFOMSG("test target_teams_distribute_is_device_ptr");
@@ -18,25 +28,24 @@ int main() {
   if (!c){
       OMPVV_WARNING("Test was unable to allocate memory on device.  Test could not procede.");
       OMPVV_REPORT_AND_RETURN(errors);
-  } 
+  }
   else {
-      // a and b array initialization
       for (int x = 0; x < ARRAY_SIZE; ++x) {
           a[x] = 1;
           b[x] = x;
       }
 
-      #pragma omp target data map(tofrom: a[0:ARRAY_SIZE]) map(to: b[0:ARRAY_SIZE])
-      {
-          #pragma omp target teams distribute is_device_ptr(c)
-          for (int x = 0; x < ARRAY_SIZE; ++x){
-              c[x] = b[x] * b[x];
-              a[x] += c[x] + b[x];
-          }
+      #pragma omp target teams distribute is_device_ptr(c) map(tofrom: a[0:ARRAY_SIZE]) map(to: b[0:ARRAY_SIZE])
+      for (int x = 0; x < ARRAY_SIZE; ++x){
+          c[x] = b[x] * b[x];
+          a[x] += c[x] + b[x];
       }
 
       for (int x = 0; x < ARRAY_SIZE; ++x){
-          OMPVV_TEST_AND_SET(errors, (a[x] != 1 + b[x] + b[x] * b[x]));
+          OMPVV_TEST_AND_SET_VERBOSE(errors, (a[x] != 1 + b[x] + b[x] * b[x]));
+          if (a[x] != 1 + b[x] + b[x] * b[x]){
+              break;
+          }
       }
 
       OMPVV_REPORT_AND_RETURN(errors);
