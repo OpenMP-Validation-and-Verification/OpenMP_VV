fbaabe2b90680de7843934084676b4a7be357f41
Jose Monsalve Diaz <josem@udel.edu>
2019-05-29 17:37:05 +0000

    Merged in new_test/test_target_teams_distribute_parallel_for_defaultmap (pull request #4)
    test_target_teams_distribute_parallel_for_defaultmap
    
    * adding test tests/target_teams_distribute_parallel_for/test_target_teams_distribute_parallel_for_defaultmap.c from development
    
    * Updating summit.def file for gcc
    
    * Updating summit.def file for gcc
    
    * synching master ompvv/ompvv.F90
    
    * synching master ompvv/ompvv.F90
    
    * Adding a single thread single team increment to test the to behavior of the tofrom. GCC is not passing, but the problem seems to be in omp_get_thread_num or omp_get_teams_num
    
    * Minor coding standard error
    
    * Changing the testing methodology to check the copy behavior when firstprivitized and the to of the tofrom behavior when the defaultmap clause is used. We use an array to store the values that each thread is reading
    
    * merging from master manually
    
    Approved-by: Kyle Friedline <kylefriedline@gmail.com>
    Approved-by: Swaroop Pophale <swaroop.pophale23@gmail.com>

---
+++ tests/4.5/target_teams_distribute_parallel_for/test_target_teams_distribute_parallel_for_defaultmap.c
@@ -0,0 +1,166 @@
+//===---- test_target_teams_distribute_parallel_for_defaultmap.c - combined consutrct -===//
+// 
+// OpenMP API Version 4.5 Nov 2015
+// 
+// testing de defaultmap of different scalar values. We check when it is off and when it is
+// on. The first one should not copy values back from the device of scalars. the second one 
+// should copy the values back even if they are not mapped explicitely
+//
+//===----------------------------------------------------------------------------------===//
+
+#include <assert.h>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "ompvv.h"
+#include <math.h>
+
+#define ITERATIONS 10000
+
+int test_defaultmap_on() {
+  OMPVV_INFOMSG("test_defaultmap_on");
+
+  int errors = 0;
+  int i;
+
+  // we try with all the scalars
+  char scalar_char = 'a';
+  char scalar_char_cpy[ITERATIONS];
+  short scalar_short = 10;
+  short scalar_short_cpy[ITERATIONS];
+  int scalar_int = 11;
+  int scalar_int_cpy[ITERATIONS];
+  float scalar_float = 5.5f;
+  float scalar_float_cpy[ITERATIONS];
+  double scalar_double = 10.45;
+  double scalar_double_cpy[ITERATIONS];
+  enum { VAL1 = 1, VAL2, VAL3, VAL4} scalar_enum = VAL1, scalar_enum_cpy[ITERATIONS];
+  
+
+  // Testing the to behavior of the tofrom we use an array to avoid data
+  // races and check that all threads get the value
+#pragma omp target teams distribute parallel for defaultmap(tofrom: scalar)
+  for (i = 0; i < ITERATIONS; ++i) {
+    scalar_char_cpy[i] = scalar_char;
+    scalar_short_cpy[i] = scalar_short;
+    scalar_int_cpy[i] = scalar_int;
+    scalar_float_cpy[i] = scalar_float;
+    scalar_double_cpy[i] = scalar_double;
+    scalar_enum_cpy[i] = scalar_enum;
+  } // end of omp target
+
+  for (i = 0; i < ITERATIONS; ++i) {
+    OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_char_cpy[i] != 'a');
+    OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_short_cpy[i] != 10);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_int_cpy[i] != 11);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, fabs(scalar_float_cpy[i] - 5.5f) > 0.0001);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, fabs(scalar_double_cpy[i] - 10.45) > 0.00001);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_enum_cpy[i] != VAL1);
+  }
+
+  // Map the same array to multiple devices. initialize with device number
+#pragma omp target teams distribute parallel for defaultmap(tofrom: scalar)
+  for (i = 0; i < ITERATIONS; ++i) {
+    scalar_char = 'b';
+    scalar_short = 20;
+    scalar_int = 33;
+    scalar_float = 6.5f;
+    scalar_double = 20.45;
+    scalar_enum = VAL4;
+  } // end of omp target 
+
+  OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_char != 'b');
+  OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_short != 20);
+  OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_int != 33);
+  OMPVV_TEST_AND_SET_VERBOSE(errors, fabs(scalar_float - 6.5f) > 0.0001);
+  OMPVV_TEST_AND_SET_VERBOSE(errors, fabs(scalar_double - 20.45) > 0.00001);
+  OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_enum != VAL4);
+  
+  return errors;
+}
+
+int test_defaultmap_off(int isOffloading, int isShared) {
+  OMPVV_INFOMSG("test_defaultmap_off");
+  
+  int errors = 0;
+  int i;
+  
+  // we try with all the scalars
+  char scalar_char = 'a';
+  char scalar_char_cpy[ITERATIONS];
+  short scalar_short = 10;
+  short scalar_short_cpy[ITERATIONS];
+  int scalar_int = 11;
+  int scalar_int_cpy[ITERATIONS];
+  float scalar_float = 5.5f;
+  float scalar_float_cpy[ITERATIONS];
+  double scalar_double = 10.45;
+  double scalar_double_cpy[ITERATIONS];
+  enum { VAL1 = 1, VAL2, VAL3, VAL4} scalar_enum = VAL1, scalar_enum_cpy[ITERATIONS];
+
+  // Testing the copy behavior of the firstprivatization. we use an array to avoid data
+  // races and check that all threads get the value
+#pragma omp target teams distribute parallel for defaultmap(tofrom: scalar)
+  for (i = 0; i < ITERATIONS; ++i) {
+    scalar_char_cpy[i] = scalar_char;
+    scalar_short_cpy[i] = scalar_short;
+    scalar_int_cpy[i] = scalar_int;
+    scalar_float_cpy[i] = scalar_float;
+    scalar_double_cpy[i] = scalar_double;
+    scalar_enum_cpy[i] = scalar_enum;
+  } // end of omp target
+
+  for (i = 0; i < ITERATIONS; ++i) {
+    OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_char_cpy[i] != 'a');
+    OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_short_cpy[i] != 10);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_int_cpy[i] != 11);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, fabs(scalar_float_cpy[i] - 5.5f) > 0.0001);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, fabs(scalar_double_cpy[i] - 10.45) > 0.00001);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_enum_cpy[i] != VAL1);
+  }
+  // Map the same array to multiple devices. initialize with device number
+#pragma omp target teams distribute parallel for map(from: isOffloading)
+  for (i = 0; i < ITERATIONS; ++i) {
+      scalar_char = 'b';
+      scalar_short = 20;
+      scalar_int = 33;
+      scalar_float = 6.5f;
+      scalar_double = 20.45;
+      scalar_enum = VAL4;
+  } // end of omp target 
+  
+  // If it is initial device then we will modify the original memory region
+  if (!isOffloading || isShared) {
+    OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_char != 'b');
+    OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_short != 20);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_int != 33);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, fabs(scalar_float - 6.5f) > 0.0001);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, fabs(scalar_double - 20.45) > 0.0001);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_enum != VAL4);
+  } else {
+    OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_char != 'a');
+    OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_short != 10);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_int != 11);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, fabs(scalar_float - 5.5f) > 0.0001);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, fabs(scalar_double - 10.45) > 0.0001);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, scalar_enum != VAL1);
+  }
+  
+  return errors;
+}
+int main() {
+  int isOffloading, isShared;
+  OMPVV_TEST_AND_SET_OFFLOADING(isOffloading);
+  OMPVV_TEST_AND_SET_SHARED_ENVIRONMENT(isShared);
+
+
+  OMPVV_WARNING_IF(!isOffloading, "Test running on host. Do not test defaultmap");
+  OMPVV_WARNING_IF(isOffloading && isShared, "Test running on shared data environment. Do not test defaultmap");
+
+  int errors = 0;
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_defaultmap_on());
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_defaultmap_off(isOffloading, isShared));
+
+  OMPVV_REPORT_AND_RETURN(errors);
+}
+
