4ceda921a335d68c02f17cf29149b59bf4170299
Josh Davis <jhdavis@udel.edu>
2019-09-07 11:44:42 -0400

    Format and syntax fixes

--- tests/4.5/target_teams_distribute/test_target_teams_distribute_nowait.F90
+++ tests/4.5/target_teams_distribute/test_target_teams_distribute_nowait.F90
@@ -13,76 +13,76 @@
 
 #define N 1024
 
-      PROGRAM test_target_teams_distribute_nowait
-        USE iso_fortran_env
-        USE ompvv_lib
-        USE omp_lib
-        implicit none
-        INTEGER :: errors
-        OMPVV_TEST_OFFLOADING()
-        errors = 0
+PROGRAM test_target_teams_distribute_nowait
+  USE iso_fortran_env
+  USE ompvv_lib
+  USE omp_lib
+  implicit none
+  INTEGER :: errors
+  OMPVV_TEST_OFFLOADING
+  errors = 0
 
-        OMPVV_TEST_VERBOSE(test_nowait() .ne. 0)
+  OMPVV_TEST_VERBOSE(test_nowait() .ne. 0)
 
-        OMPVV_REPORT_AND_RETURN()
-      CONTAINS
-        INTEGER FUNCTION test_nowait()
-          INTEGER:: errors, x
-          LOGICAL:: is_host
-          INTEGER,DIMENSION(N):: a, b, c, d, e, f, g
+  OMPVV_REPORT_AND_RETURN()
+CONTAINS
+  INTEGER FUNCTION test_nowait()
+    INTEGER:: errors, x
+    LOGICAL:: is_host
+    INTEGER,DIMENSION(N):: a, b, c, d, e, f, g
 
-          DO x = 1, N
-            a(x) = x
-            b(x) = 2 * x
-            c(x) = 0
-            d(x) = 3 * x
-            e(x) = 4 * x
-            f(x) = 0
-            g(x) = 0
-          END DO
+    DO x = 1, N
+       a(x) = x
+       b(x) = 2 * x
+       c(x) = 0
+       d(x) = 3 * x
+       e(x) = 4 * x
+       f(x) = 0
+       g(x) = 0
+    END DO
 
-          !$omp target data map(to: a(1:N), b(1:N), d(1:N), e(1:N)) map(from: &
-          !$omp& c(1:N), f(1:N), g(1:N)) map(tofrom: is_host)
-            !$omp parallel
-              !$omp target teams distribute nowait map(alloc: a(1:N), b(1:N), &
-              !$omp& c(1:N))
-              DO x = 1, N
-                c(x) = a(x) + b(x)
-              END DO
-              !$omp end target teams distribute
-              !$omp target teams distribute nowait map(alloc: d(1:N), e(1:N), &
-              !$omp& f(1:N))
-              DO x = 1, N
-                f(x) = d(x) + e(x)
-              END DO
-              !$omp end target teams distribute
-              !$omp barrier
-              !$omp target teams distribute map(alloc: is_host, c(1:N), f(1:N), &
-              !$omp& g(1:N))
-              DO x = 1, N
-                is_host = omp_is_initial_device()
-                g(x) = c(x) + f(x)
-              END DO
-              !$omp end target teams distribute
-            !$omp end parallel
-          !$omp end target data
+    !$omp target data map(to: a(1:N), b(1:N), d(1:N), e(1:N)) map(from: &
+    !$omp& c(1:N), f(1:N), g(1:N)) map(tofrom: is_host)
+    !$omp parallel
+    !$omp target teams distribute nowait map(alloc: a(1:N), b(1:N), &
+    !$omp& c(1:N))
+    DO x = 1, N
+       c(x) = a(x) + b(x)
+    END DO
+    !$omp end target teams distribute
+    !$omp target teams distribute nowait map(alloc: d(1:N), e(1:N), &
+    !$omp& f(1:N))
+    DO x = 1, N
+       f(x) = d(x) + e(x)
+    END DO
+    !$omp end target teams distribute
+    !$omp barrier
+    !$omp target teams distribute map(alloc: is_host, c(1:N), f(1:N), &
+    !$omp& g(1:N))
+    DO x = 1, N
+       is_host = omp_is_initial_device()
+       g(x) = c(x) + f(x)
+    END DO
+    !$omp end target teams distribute
+    !$omp end parallel
+    !$omp end target data
 
-          IF (is_host) THEN
-            OMPVV_WARNING("Test ran on host, nowait cannot be tested")
-          END IF
+    IF (is_host) THEN
+       OMPVV_WARNING("Test ran on host, nowait cannot be tested")
+    END IF
 
-          DO x = 1, N
-            IF (c(x) .ne. 3 * x) THEN
-              errors = errors + 1
-            END IF
-            IF (f(x) .ne. 7 * x) THEN
-              errors = errors + 1
-            END IF
-            IF (g(x) .ne. 10 * x) THEN
-              errors = errors + 1
-            END IF
-          END DO
+    DO x = 1, N
+       IF (c(x) .ne. 3 * x) THEN
+          errors = errors + 1
+       END IF
+       IF (f(x) .ne. 7 * x) THEN
+          errors = errors + 1
+       END IF
+       IF (g(x) .ne. 10 * x) THEN
+          errors = errors + 1
+       END IF
+    END DO
 
-          test_nowait = errors
-        END FUNCTION test_nowait
-      END PROGRAM test_target_teams_distribute_nowait
+    test_nowait = errors
+  END FUNCTION test_nowait
+END PROGRAM test_target_teams_distribute_nowait
--- tests/4.5/target_teams_distribute/test_target_teams_distribute_nowait.c
+++ tests/4.5/target_teams_distribute/test_target_teams_distribute_nowait.c
@@ -4,8 +4,8 @@
 //
 // This test uses the nowait clause on a target teams distribute directive and
 // uses a barrier to resyncronize the target regions.  Since we can't be sure
-// that operations will be asyncronous, we can not test to make sure that
-// the regions are executed asynchronously.
+// that operations will be asyncronous, we can only raise a warning if they are
+// not executed asynchronously.
 //
 ////===----------------------------------------------------------------------===//
 
@@ -16,6 +16,7 @@
 
 #define ARRAY_SIZE 1024
 #define ITERATIONS 1024
+
 int main() {
   int isOffloading = 0;
   OMPVV_TEST_AND_SET_OFFLOADING(isOffloading);
@@ -28,41 +29,42 @@ int main() {
   int errors = 0;
   int race_condition_found = 0;
 
-  for (int y = 0; y < ITERATIONS; ++y){
+  for (int y = 0; y < ITERATIONS; ++y) {
+    for (int x = 0; x < ARRAY_SIZE; ++x) {
+      a[x] = x + y;
+      b[x] = 2 * x + y;
+      c[x] = 0;
+      d[x] = 3 * x + y;
+      e[x] = 4 * x + y;
+      f[x] = 0;
+    }
+
+#pragma omp target data map(to: a[0:ARRAY_SIZE], b[0:ARRAY_SIZE], d[0:ARRAY_SIZE], e[0:ARRAY_SIZE]) map(from: c[0:ARRAY_SIZE], f[0:ARRAY_SIZE])
+    {
+#pragma omp target teams distribute nowait map(alloc: a[0:ARRAY_SIZE], b[0:ARRAY_SIZE], c[0:ARRAY_SIZE])
       for (int x = 0; x < ARRAY_SIZE; ++x) {
-          a[x] = x + y;
-          b[x] = 2 * x + y;
-          c[x] = 0;
-          d[x] = 3 * x + y;
-          e[x] = 4 * x + y;
-          f[x] = 0;
+	c[x] = a[x] + b[x];
       }
-
-      #pragma omp target data map(to: a[0:ARRAY_SIZE], b[0:ARRAY_SIZE], d[0:ARRAY_SIZE], e[0:ARRAY_SIZE]) map(from: c[0:ARRAY_SIZE], f[0:ARRAY_SIZE]) 
-      {
-          #pragma omp target teams distribute nowait map(alloc: a[0:ARRAY_SIZE], b[0:ARRAY_SIZE], c[0:ARRAY_SIZE])
-          for (int x = 0; x < ARRAY_SIZE; ++x){
-              c[x] = a[x] + b[x];
-          }
-          #pragma omp target teams distribute nowait map(alloc: c[0:ARRAY_SIZE], d[0:ARRAY_SIZE], e[0:ARRAY_SIZE], f[0:ARRAY_SIZE])
-          for (int x = 0; x < ARRAY_SIZE; ++x){
-              f[x] = c[x] + d[x] + e[x];
-          }
+#pragma omp target teams distribute nowait map(alloc: c[0:ARRAY_SIZE], d[0:ARRAY_SIZE], e[0:ARRAY_SIZE], f[0:ARRAY_SIZE])
+      for (int x = 0; x < ARRAY_SIZE; ++x) {
+	f[x] = c[x] + d[x] + e[x];
+      }
 #pragma omp target
-           {
-            race_condition_found = 0;
-           }
-      }
+      {
+	race_condition_found = race_condition_found;
+      } // this target clause includes an implicit barrier
+    }
 
-      for (int x = 0; x < ARRAY_SIZE; ++x){
-          OMPVV_TEST_AND_SET_VERBOSE(errors, c[x] != 3 * x + 2 * y);
-          if (f[x] != 10 * x + 4 * y){
-              race_condition_found = 1;
-          }
+    for (int x = 0; x < ARRAY_SIZE; ++x) {
+      OMPVV_TEST_AND_SET_VERBOSE(errors, c[x] != 3 * x + 2 * y);
+      if (f[x] != 10 * x + 4 * y) {
+	race_condition_found = 1;
       }
+    }
   }
 
-  OMPVV_WARNING_IF(race_condition_found == 0, "Could not show that nowait was operating on target teams distribute construct");
+  OMPVV_WARNING_IF(race_condition_found == 0, "Could not show that nowait was operating on target teams distribute construct.");
+  OMPVV_INFOMSG_IF(race_condition_found == 1, "At least one race condition was introduced, nowait was operating.");
 
   OMPVV_REPORT_AND_RETURN(errors);
 }
