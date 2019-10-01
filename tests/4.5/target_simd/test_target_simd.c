0ef003bd89a8334cf8a6e261b15300db66bc6221
Jose Monsalve Diaz <josem@udel.edu>
2019-10-01 20:53:52 +0000

    Merged in new_test/target_simd (pull request #35)
    new_test/target_simd
    
    * Adding two tests for simple target SIMD, combined and not combined
    
    * Adding the corresponding Fortran tests
    
    * Fixing typos and others
    
    * Adding space before bracket
    
    * _nested name instead of _combined name. Usually we did combined and nested is what's different here, in compariison to target teams distribute parallel for
    
    * fixing the way I check for errors in fortran
    
    * renaming test
    
    * removing extra spaces in messagE
    
    Approved-by: Josh Davis <jhdavis@udel.edu>
    Approved-by: Swaroop Pophale <swaroop.pophale23@gmail.com>

---
+++ tests/4.5/target_simd/test_target_simd.c
@@ -0,0 +1,63 @@
+//===---- test_target_simd.c - Using combined construct target simd -===//
+// 
+// OpenMP API Version 4.5 Nov 2015
+// 
+// SIMD in OpenMP 4.5 does not have any API that allows us to confirm the creation 
+// of SIMD lanes, nor the use of SIMD instructions in any architecture. Hence, our
+// tests are limited in that they check that the expected result is created, but
+// assume nothing in how they are mapped into a particular architecture
+//
+// This test creates a regular for loop and uses the combined target simd directive 
+// then it checks that the values of the array are as expected
+//===--------------------------------------------------------------------------===//
+//
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "ompvv.h"
+
+#define ARRAY_SIZE 1024
+
+int test_target_simd() {
+  OMPVV_INFOMSG("test_target_simd");
+  OMPVV_WARNING("This test cannot check if actual SIMD extensions at the hardware level"
+                 " were used, or of the generated code is different in any way");
+
+  // Variable for errors counting
+  int errors = 0;
+
+  int a[ARRAY_SIZE];
+  int b[ARRAY_SIZE];
+  int c[ARRAY_SIZE];
+  int i;
+
+  // a and b array initialization
+  for (i = 0; i < ARRAY_SIZE; ++i) {
+      a[i] = 1;
+      b[i] = i;
+      c[i] = 2 * i;
+  }
+
+
+#pragma omp target simd map(to: b[0:ARRAY_SIZE], c[0:ARRAY_SIZE]) map(tofrom: a[0:ARRAY_SIZE])
+    for (i = 0; i < ARRAY_SIZE; ++i) {
+        a[i] += b[i] * c[i];
+    }
+
+
+  for (i = 0; i < ARRAY_SIZE; ++i) {
+      OMPVV_TEST_AND_SET(errors, (a[i] != 1 + (b[i] * c[i])));
+  }
+
+  return errors;
+}
+
+int main() {
+  OMPVV_TEST_OFFLOADING;
+
+  int errors = 0;
+
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_target_simd());
+
+  OMPVV_REPORT_AND_RETURN(errors);
+}
