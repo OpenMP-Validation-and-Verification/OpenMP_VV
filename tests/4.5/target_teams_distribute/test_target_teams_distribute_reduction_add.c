bbe4023dbbf6a24acb2740ca2cee1d47e5dd6877
Kyle Friedline <kylefriedline@gmail.com>
2019-10-08 19:30:48 +0000

    Merged in TargetTeamsDistributeReduction (pull request #17)
    Added reduction test for target teams distribute
    
    * Added reduction test for target teams distribute
    
    * Added reduction test for target teams distribute
    
    * merging from master manually
    
    * Made changes based on comments on bitbucket
    
    * Another fix
    
    * Added reduction test for target teams distribute
    
    * Merged in new_test/test_target_teams_distribute_parallel_for_devices (pull request #9)
    
        new_test/target_teams_distribute_parallel_for_devices
    
        * Adding test tests/target_teams_distribute_parallel_for/test_target_teams_distribute_parallel_for_devices.c
    
        * Removing comment that should not be there
    
        Approved-by: Swaroop Pophale <swaroop.pophale23@gmail.com>
        Approved-by: Kyle Friedline <kylefriedline@gmail.com>
    
    * Merged in test_fix/test_target_update_devices.c (pull request #25)
    
        Fixing typo that @Ethan Stewart pointed out with respect to initialization of sum variable.
    
        Adding the header description that was adapted later on in the coding standards.
        Removing trailing spaces in some of the lines.
    
        Approved-by: Ethan Stewart <ethan.stewart@amd.com>
    
    * Adding format to the summary script to create a summary of the
        tests to be used by bitbucket/jenkins integration
    
    * Adding a new Makefile rule report_summary that outputs a short summary of how many runs, and what were the failures
    
    * Moving to multiple folders for multiple versions of OpenMP Specifications
    
    * merging from master manually
    
    * Made changes based on comments on bitbucket
    
    * Another fix
    
    * Progress on breaking up the reduction by operator, uncovered a condition failure
    
    * Fixed formatting on all reduce tests
    
    * Fully separated the reduction tests by operator
    
    * Adding in missing reduction operator tests
    
    * Fixing missing semicolons
    
    * Removed obsolete test with all operators
    
    * Added missing offload test to all reduction tests
    
    * Added missing map to add, still have all the other operators to get thru
    
    * Added extra mapping attribute to all reduction operator tests
    
    * Added map(tofrom) to the two failing tests
    
    * Removed target data from all operator tests
    
    * Added explanation of math for array false margin
    
    * Corrected team number warning message
    
    Approved-by: Jose Monsalve Diaz <josem@udel.edu>
    Approved-by: Swaroop Pophale <swaroop.pophale23@gmail.com>

---
+++ tests/4.5/target_teams_distribute/test_target_teams_distribute_reduction_add.c
@@ -0,0 +1,71 @@
+//===--- test_target_teams_distribute_reduction_add.c-------------------------===//
+//
+// OpenMP API Version 4.5 Nov 2015
+//
+// This test uses the reduction clause on a target teams distribute directive,
+// testing that the variable in the reduction clause is properly reduced using
+// the add operator.
+//
+////===----------------------------------------------------------------------===//
+
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "ompvv.h"
+#include <math.h>
+
+#define N 1024
+
+int test_add() {
+  int a[N];
+  int b[N];
+  int total = 0;
+  int host_total = 0;
+  int errors = 0;
+  int num_teams[N];
+  int warned = 0;
+
+  for (int x = 0; x < N; ++x) {
+    a[x] = 1;
+    b[x] = x;
+    num_teams[x] = -1;
+  }
+
+#pragma omp target teams distribute reduction(+:total) map(to: a[0:N], b[0:N]) map(tofrom: total, num_teams[0:N])
+  for (int x = 0; x < N; ++x) {
+    num_teams[x] = omp_get_num_teams();
+    total += a[x] + b[x];
+  }
+
+  for (int x = 0; x < N; ++x) {
+    host_total += a[x] + b[x];
+  }
+
+  for (int x = 1; x < N; ++x) {
+    if (num_teams[x-1] != num_teams[x]) {
+      OMPVV_WARNING("Kernel reported differing numbers of teams.  Validity of testing of reduction clause cannot be guaranteed.");
+      warned += 1;
+    }
+  }
+
+  if ((num_teams[0] == 1) && (warned == 0)) {
+    OMPVV_WARNING("Test operated with one team.  Reduction clause cannot be tested.");
+  } else if ((num_teams[0] <= 0) && (warned == 0)) {
+    OMPVV_WARNING("Test reported invalid number of teams.  Validity of testing of reduction clause cannot be guaranteed.");
+  }
+
+  OMPVV_TEST_AND_SET_VERBOSE(errors, host_total != total);
+  OMPVV_ERROR_IF(host_total != total, "Total on device is %d but expected total from host is %d.", total, host_total);
+
+  return errors;
+}
+
+int main() {
+  OMPVV_TEST_OFFLOADING;
+
+  int total_errors = 0;
+
+  OMPVV_TEST_AND_SET_VERBOSE(total_errors, test_add() != 0);
+
+  OMPVV_REPORT_AND_RETURN(total_errors);
+}
