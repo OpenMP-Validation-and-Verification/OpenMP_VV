262bf385ae71d4d085cb9e42831d0e4780a9fa84
Jose Manuel Monsalve Diaz <josem@udel.edu>
2019-10-21 19:07:23 -0400

    target_enter_data_classes_simple.cpp Adding appropriate test description Fixing mapping problem with scalars and missing tofrom: array mapping

--- tests/4.5/target_enter_data/test_target_enter_data_classes_simple.cpp
+++ tests/4.5/target_enter_data/test_target_enter_data_classes_simple.cpp
@@ -3,12 +3,24 @@
 // OpenMP API Version 4.5 Nov 2015
 //
 //
-// This test checks for mapping of classes into  the device through the use of 
-// target enter and exit data clauses. IT considers that 4.5 does not support
+// This test checks for interoperability between C++ classes and device offloading through the use of 
+// target enter and exit data clauses. It considers that 4.5 does not support
 // mapping of attributes directly, as the implicit use of the this-> pointer when 
 // using attributes inside the target region is restrictive.
 //
-// This test creates a class that, during construction maps an attribute through helper 
+// The description of the map clause says that the map clause receives a list item:
+//
+// Section 2.15.5.1, page 216, line 17
+//
+// The syntax of the map clause is as follows:
+// map([ [map-type-modifier[,]] map-type : ] list)
+// And the definition of list item is
+//
+// Section 2.1, page 27, line 20
+//
+// A list item is a variable or array section. An extended list item is a list item or a function name.
+//
+// This test creates a class that, during construction, it maps an attribute through helper 
 // variables that remove the direct use to the attributes. And during destruction of the 
 // object it maps the data back to the devices. Additionally, there is a modifier 
 // method that uses values from the class indirectly through the use of helper references
@@ -34,14 +46,15 @@ private:
 public:
   // Constructor. Maps the data into the de3vice
   Simple(int s) : size(s) { 
-    sum = 0;
-    d_array = new int[size];
+    this->sum = 0;
+    this->d_array = new int[size];
+    std::fill(d_array, d_array+size, 0);
     // Removing the  direct use  of attributes to avoid problems
     // with 4.5 specifications 
     int* helper = d_array;
     int &hs = size;
     int &hsum = sum;
-#pragma omp target enter data map(to: helper[0:size]) map(to: hs) map(to:hsum)
+#pragma omp target enter data map(to: helper[0:hs]) map(to: hs) map(to:hsum)
   }
 
   // Destructor, removes the data from the device
@@ -51,7 +64,7 @@ public:
     int* helper = d_array;
     int &hs = size;
     int &hsum = sum;
-#pragma omp target exit data map(delete: helper[0:size]) map(delete: hs) map(delete: hsum)
+#pragma omp target exit data map(delete: helper[0:hs]) map(delete: hs) map(delete: hsum)
     delete[] d_array; 
   }
   
@@ -59,14 +72,14 @@ public:
   void modify() {
     // Removing the  direct use  of attributes to avoid problems
     // with 4.5 specifications 
-    int * helper = d_array;
+    int *helper = d_array;
     int &hsize = size;
     int &hsum = sum;
 #pragma omp target defaultmap(tofrom: scalar) 
     {
       hsum = 0;
       for (int i = 0; i < hsize; ++i) {
-        helper[i] = 1;
+        helper[i] += 1;
         hsum += helper[i];
       }
     }
@@ -76,13 +89,12 @@ public:
   void getValues(int &h_sum, int* h_array) {
     int* helper = d_array;
     int &hsize = size;
-    int *help_sum = &sum;
-#pragma omp target map(from: h_sum, h_array[0:size])
+    int &help_sum = sum;
+#pragma omp target map(from: h_array[0:hsize]) defaultmap(tofrom: scalar)
     {
-      h_sum = *help_sum;
-      for (int i = 0; i < hsize; i++) {
+      h_sum = help_sum;
+      for (int i = 0; i < hsize; i++)
         h_array[i] = helper[i];
-      }
     }
   }
   int* getArray() {
@@ -109,14 +121,15 @@ int test_simple_class() {
   obj->getValues(h_sum, h_array);
 
   // checking results
-  for (int i = 0; i < N; ++i)
+  for (int i = 0; i < N; ++i) {
     sum += h_array[i];
+  }
 
   delete obj;
   delete[] h_array;
   
-  OMPVV_TEST_AND_SET_VERBOSE(errors, (N != sum));
-  OMPVV_TEST_AND_SET_VERBOSE(errors, (N != h_sum));
+  OMPVV_TEST_AND_SET_VERBOSE(errors, (3*N != sum));
+  OMPVV_TEST_AND_SET_VERBOSE(errors, (3*N != h_sum));
   OMPVV_ERROR_IF(errors != 0, "N = %d, sum = %d, h_sum = %d", N, sum, h_sum);
 
   return errors;
