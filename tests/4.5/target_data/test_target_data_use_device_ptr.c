a66d4d3541f9438781293f177f1b58ce2f5b6e8a
Swaroop Pophale <swaroop.pophale23@gmail.com>
2019-10-30 16:44:56 -0400

    Update test_target_data_use_device_ptr.c
    Separating mapping and use_device_pointer so that the array is in device data environment before using use_device_pointer.

--- tests/4.5/target_data/test_target_data_use_device_ptr.c
+++ tests/4.5/target_data/test_target_data_use_device_ptr.c
@@ -4,7 +4,7 @@
 
 // Test for OpenMP 4.5 target data with use_device_ptr
 int main() {
-  int errors = 0, len = 10000, isHost = 0;
+  int errors = 0, len = 10000, isHost = 0, map_dummy;
   int *array_device = NULL;
   int *array_host = NULL;
 
@@ -14,17 +14,19 @@ int main() {
   for (int i = 0; i < len; ++i)
     array_host[i] = i;
 
-#pragma omp target data map(tofrom: array_device[0:len])   \
-    use_device_ptr(array_device)
+#pragma omp target data map(tofrom: array_device[0:len])
   {
-#pragma omp target is_device_ptr(array_device) map(tofrom: array_host[0:len]) map(tofrom: isHost)
+#pragma omp target data map(map_dummy) use_device_ptr(array_device)
     {
-      isHost = omp_is_initial_device();
-      for (int i = 0; i < len; ++i) {
-        array_device[i] = i;
-        array_host[i] += array_device[i];
-      } 
-    } // end target
+#pragma omp target is_device_ptr(array_device) map(tofrom: array_host[0:len]) map(tofrom: isHost)
+      {
+        isHost = omp_is_initial_device();
+        for (int i = 0; i < len; ++i) {
+          array_device[i] = i;
+          array_host[i] += array_device[i];
+        } 
+      } // end target
+    } // end target data
   } // end target data
 
   // checking results
