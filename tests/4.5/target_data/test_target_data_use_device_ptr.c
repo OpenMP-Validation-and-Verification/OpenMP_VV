80cde3b8535762bfa5753833effe302c4ac7e0c4
Swaroop Pophale <swaroop.pophale23@gmail.com>
2019-10-31 15:41:34 -0400

    Update test_target_data_use_device_ptr.c
    Minor edits to standardize.

--- tests/4.5/target_data/test_target_data_use_device_ptr.c
+++ tests/4.5/target_data/test_target_data_use_device_ptr.c
@@ -3,7 +3,7 @@
 // OpenMP API Version 4.5 Nov 2015
 // 
 // This file is a test for the use_device_ptr when used with the map
-// clause. This test uses arrays of size len which values are modified on the 
+// clause. This test uses arrays of size N which values are modified on the 
 // device and tested in the host. Once the array has been mapped to the device,
 // the use_device_ptr should be able to be used with the ptr to the array and 
 // subsequent modify values on the device. 
@@ -13,16 +13,17 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ompvv.h>
+#include "ompvv.h"
 
+#define N 1000
 
 int main() {
-  int errors = 0, len = 10000, isHost = 0, map_dummy;
+  int errors = 0, isHost = 0, map_dummy;
   int *array_device = NULL;
   int *array_host = NULL;
 
-  array_device = (int *)malloc(len*sizeof(int));
-  array_host = (int *)malloc(len*sizeof(int));
+  array_device = (int *)malloc(N*sizeof(int));
+  array_host = (int *)malloc(N*sizeof(int));
 
   for (int i = 0; i < len; ++i)
     array_host[i] = i;
@@ -36,7 +37,7 @@ int main() {
 #pragma omp target is_device_ptr(array_device) map(tofrom: array_host[0:len]) map(tofrom: isHost)
       {
         isHost = omp_is_initial_device();
-        for (int i = 0; i < len; ++i) {
+        for (int i = 0; i < N; ++i) {
           array_device[i] = i;
           array_host[i] += array_device[i];
         } 
@@ -45,7 +46,7 @@ int main() {
   } // end target data
 
   // checking results
-  for (int i = 0; i < len; ++i) {
+  for (int i = 0; i < N; ++i) {
     OMPVV_TEST_AND_SET(errors, array_host[i] != 2*i);
   }
 
