0bb8792e019baffcde0afa81fa3306de15a5e43b
Josh Davis <jhdavis@udel.edu>
2019-11-12 15:24:36 -0500

    Formatting fixes, missing initialization in F90, for lastprivate

--- tests/4.5/target_teams_distribute/test_target_teams_distribute_lastprivate.F90
+++ tests/4.5/target_teams_distribute/test_target_teams_distribute_lastprivate.F90
@@ -14,82 +14,84 @@
 
 #define N 1024
 
-      PROGRAM test_target_teams_distribute_lastprivate
-        USE iso_fortran_env
-        USE ompvv_lib
-        USE omp_lib
-        implicit none
-        INTEGER :: errors
-        OMPVV_TEST_OFFLOADING()
-        errors = 0
+PROGRAM test_target_teams_distribute_lastprivate
+  USE iso_fortran_env
+  USE ompvv_lib
+  USE omp_lib
+  implicit none
+  INTEGER :: errors
+  OMPVV_TEST_OFFLOADING
+  errors = 0
 
-        OMPVV_TEST_VERBOSE(test_scalar() .ne. 0)
-        OMPVV_TEST_VERBOSE(test_array() .ne. 0)
+  OMPVV_TEST_VERBOSE(test_scalar() .ne. 0)
+  OMPVV_TEST_VERBOSE(test_array() .ne. 0)
 
-        OMPVV_REPORT_AND_RETURN()
-      CONTAINS
-        INTEGER FUNCTION test_scalar()
-          INTEGER:: x, errors, privatized
-          INTEGER,DIMENSION(N):: a, b, c
+  OMPVV_REPORT_AND_RETURN()
+CONTAINS
+  INTEGER FUNCTION test_scalar()
+    INTEGER:: x, errors, privatized
+    INTEGER,DIMENSION(N):: a, b, c
 
-          DO x = 1, N
-            a(x) = 1
-            b(x) = x
-            c(x) = 0
-          END DO
+    DO x = 1, N
+       a(x) = 1
+       b(x) = x
+       c(x) = 0
+    END DO
 
-          errors = 0
+    errors = 0
 
-          !$omp target data map(to: a(1:N), b(1:N)) map(tofrom: c(1:N))
-            !$omp target teams distribute lastprivate(privatized) map(alloc: &
-            !$omp& a(1:N), b(1:N), c(1:N))
-            DO x = 1, N
-              privatized = a(x) - b(x)
-              c(x) = privatized + b(x)
-            END DO
-          !$omp end target data
+    !$omp target data map(to: a(1:N), b(1:N)) map(tofrom: c(1:N))
+    !$omp target teams distribute lastprivate(privatized) map(alloc: &
+    !$omp& a(1:N), b(1:N), c(1:N)) defaultmap(tofrom:scalar)
+    DO x = 1, N
+       privatized = a(x) - b(x)
+       c(x) = privatized + b(x)
+    END DO
+    !$omp end target data
 
-          DO x = 1, N
-            IF (c(x) .ne. a(x)) THEN
-              errors = errors + 1
-            END IF
-          END DO
+    DO x = 1, N
+       IF (c(x) .ne. a(x)) THEN
+          errors = errors + 1
+       END IF
+    END DO
 
-          IF (privatized .ne. (a(N) - b(N))) THEN
-            errors = errors + 1
-          END IF
+    IF (privatized .ne. (a(N) - b(N))) THEN
+       errors = errors + 1
+    END IF
 
-          test_scalar = errors
-        END FUNCTION test_scalar
+    test_scalar = errors
+  END FUNCTION test_scalar
 
-        INTEGER FUNCTION test_array()
-          INTEGER:: x, errors
-          INTEGER,DIMENSION(N):: a, b, c
-          INTEGER,DIMENSION(2):: privatized
+  INTEGER FUNCTION test_array()
+    INTEGER:: x, errors
+    INTEGER,DIMENSION(N):: a, b, c
+    INTEGER,DIMENSION(2):: privatized
 
-          DO x = 1, N
-            a(x) = 1
-            b(x) = x
-            c(x) = MOD(x, 10)
-          END DO
+    DO x = 1, N
+       a(x) = 1
+       b(x) = x
+       c(x) = MOD(x, 10)
+    END DO
 
-          !$omp target data map(to: a(1:N), b(1:N), c(1:N)) map(tofrom: &
-          !$omp& privatized(1:2))
-            !$omp target teams distribute lastprivate(privatized) map(alloc: &
-            !$omp& a(1:N), b(1:N), c(1:N))
-            DO x = 1, N
-              privatized(1) = a(x) + b(x) + c(x)
-              privatized(2) = (a(x) + b(x)) * c(x)
-            END DO
-          !$omp end target data
+    errors = 0
 
-          IF (privatized(1) .ne. a(N) + b(N) + c(N)) THEN
-            errors = errors + 1
-          END IF
-          IF (privatized(2) .ne. (a(N) + b(N)) * c(N)) THEN
-            errors = errors + 1
-          END IF
+    !$omp target data map(to: a(1:N), b(1:N), c(1:N)) map(tofrom: &
+    !$omp& privatized(1:2))
+    !$omp target teams distribute lastprivate(privatized) map(alloc: &
+    !$omp& a(1:N), b(1:N), c(1:N))
+    DO x = 1, N
+       privatized(1) = a(x) + b(x) + c(x)
+       privatized(2) = (a(x) + b(x)) * c(x)
+    END DO
+    !$omp end target data
 
-          test_array = errors
-        END FUNCTION test_array
-      END PROGRAM test_target_teams_distribute_lastprivate
+    IF (privatized(1) .ne. a(N) + b(N) + c(N)) THEN
+       errors = errors + 1
+    END IF
+    IF (privatized(2) .ne. (a(N) + b(N)) * c(N)) THEN
+       errors = errors + 1
+    END IF
+
+    test_array = errors
+  END FUNCTION test_array
+END PROGRAM test_target_teams_distribute_lastprivate
