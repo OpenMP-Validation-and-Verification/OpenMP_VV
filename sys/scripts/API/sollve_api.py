28525b037f9493a9129726d4399f13750831b09c
Jose Manuel Monsalve Diaz <josem@udel.edu>
2019-12-02 03:53:29 -0600

    Finishing the curl implementation

--- sys/scripts/API/sollve_api.py
+++ sys/scripts/API/sollve_api.py
@@ -3,6 +3,7 @@ import sys
 import json
 import traceback
 import os
+import subprocess
 
 ### SOLLVE_API_RELATED
 class SOLLVE_API_01(metaclass=abc.ABCMeta):
@@ -58,25 +59,104 @@ class SOLLVE_API_01(metaclass=abc.ABCMeta):
 
 ## This class if requests is not installed in the system
 class SOLLVE_API_01_curl(SOLLVE_API_01):
+  def __init__(self):
+    super().__init__()
+    self.print_log("Using the CURL Back end", 1)
+    #check if curl exists
+    stream = os.popen('which curl')
+    if (len(stream.read()) != 0):
+      print("We are using CURL because we could not find the `requests` package")
+      print("Error handling is limted. Please consider installing `requests` through")
+      print("    pip install requests")
+    else:
+      raise Exception("""
+            We could not find curl or the 'requests' python module in your system.
+            To obtain requests use pip install requests
+            """)
   def create_tag(self):
     self.print_log("create_tag curl")
+    stream = subprocess.Popen(['curl', '-s', '--show-error', self.CREATE_TAG_URL], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    errors = stream.stderr.read()
+    if len(errors) != 0:
+      print(f"Error creating tag {errors}", file=sys.stderr)
+    else:
+      return str(stream.stdout.read().decode('utf-8'))
 
   def obtain_result(self, tag):
     self.print_log("obtain_result curl")
+    stream = subprocess.Popen(['curl', '-s', '--show-error', self.OBTAIN_RESULT_URL+tag], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    errors = stream.stderr.read()
+    if len(errors) != 0:
+      print(f"Error creating tag {errors}", file=sys.stderr)
+    else:
+      # This method return scaped double quotes and other characters
+      val = stream.stdout.read().decode('unicode_escape')
+      # We need to remove any leading or ending spaces
+      val = val.lstrip().rstrip()
+      # Sometimes text comes with double quotes. Remove them
+      val = val[1:] if val[0] == '"' else val
+      val = val[:-1] if val[-1] == '"' else val
+      return val
 
   def update_result(self, tag, jsonLog):
     self.print_log("update_result curl")
+    command = ['curl', '-s', '--show-error', '-X POST', '-H "Content-Type: application/json"', f'-d @{jsonLog}', self.UPDATE_RESULT_URL+tag]
+    command = " ".join(command)
+    stream = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE,)
+    errors = stream.stderr.read()
+    if len(errors) != 0:
+      print(f"Error creating tag {errors}", file=sys.stderr)
+    else:
+      # This method return scaped double quotes and other characters
+      val = stream.stdout.read().decode('unicode_escape')
+      # We need to remove any leading or ending spaces
+      val = val.lstrip().rstrip()
+      if "true" in val:
+        return True
+      else:
+        return False
 
   def append_result(self, tag, jsonLog):
     self.print_log("append_result curl")
+    command = ['curl', '-s', '--show-error', '-X POST', '-H "Content-Type: application/json"', f'-d @{jsonLog}', self.APPEND_RESULT_URL+tag]
+    command = " ".join(command)
+    stream = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE,)
+    errors = stream.stderr.read()
+    if len(errors) != 0:
+      print(f"Error creating tag {errors}", file=sys.stderr)
+    else:
+      # This method return scaped double quotes and other characters
+      val = stream.stdout.read().decode('unicode_escape')
+      # We need to remove any leading or ending spaces
+      val = val.lstrip().rstrip()
+      if "true" in val:
+        return True
+      else:
+        return False
 
   def delete_result(self,tag):
     self.print_log("delete_result curl")
+    command = ['curl', '-s', '--show-error', '-X DELETE', self.DELETE_RESULT_URL+tag]
+    command = " ".join(command)
+    stream = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE,)
+    errors = stream.stderr.read()
+    if len(errors) != 0:
+      print(f"Error creating tag {errors}", file=sys.stderr)
+    else:
+      # This method return scaped double quotes and other characters
+      val = stream.stdout.read().decode('unicode_escape')
+      # We need to remove any leading or ending spaces
+      val = val.lstrip().rstrip()
+      if "true" in val:
+        return True
+      else:
+        return False
 
 
 ## This class if request is installed in the system
 class SOLLVE_API_01_requests(SOLLVE_API_01):
   def create_tag(self):
+    self.print_log("Using the requests module back end", 1)
     self.print_log("create_tag requests")
     try:
       response = requests.get(self.CREATE_TAG_URL)
@@ -110,10 +190,10 @@ class SOLLVE_API_01_requests(SOLLVE_API_01):
           print(f"Error reading json file {jsonLog}. [{type(err).__name__}] {err}")
         response = requests.post(self.UPDATE_RESULT_URL+tag, headers={'Content-Type': 'application/json'}, json=json_data)
         if (response.status_code == 200):
-          if ("false" in response.text):
-            return False
-          else:
+          if ("true" in response.text):
             return True
+          else:
+            return False
         else:
           raise requests.exceptions.HTTPError(f"Status code not as expected {response.status_code}")
     except Exception as err: 
@@ -131,10 +211,10 @@ class SOLLVE_API_01_requests(SOLLVE_API_01):
           print(f"Error reading json file {jsonLog}. [{type(err).__name__}] {err}")
         response = requests.post(self.APPEND_RESULT_URL+tag, headers={'Content-Type': 'application/json'}, json=json_data)
         if (response.status_code == 200):
-          if ("false" in response.text):
-            return False
-          else:
+          if ("true" in response.text):
             return True
+          else:
+            return False
         else:
           raise requests.exceptions.HTTPError(f"Status code not as expected {response.status_code}")
     except Exception as err: 
@@ -147,17 +227,17 @@ class SOLLVE_API_01_requests(SOLLVE_API_01):
     try:
       response = requests.delete(self.DELETE_RESULT_URL+tag)
       if (response.status_code == 200):
-        if ("false" in response.text):
-          return False
-        else:
+        if ("true" in response.text):
           return True
+        else:
+          return False
       else:
         raise requests.exceptions.HTTPError(f"Status code not as expected {response.status_code}")
     except Exception as err: 
       print(f"Error obtaining tag [{type(err).__name__}]: {err}",file=sys.stderr)
 
-try:
-  import requests
-  api = SOLLVE_API_01_requests()
-except:
-  api = SOLLVE_API_01_curl()
+# try:
+#   import requests
+#   api = SOLLVE_API_01_requests()
+# except:
+api = SOLLVE_API_01_curl()
