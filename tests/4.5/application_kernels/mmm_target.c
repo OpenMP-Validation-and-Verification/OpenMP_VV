2507f789945109ef5e90f0c29301a982d48be4a7
Jose Manuel Monsalve Diaz <josem@udel.edu>
2019-12-25 15:38:43 -0500

    Minor coding standard issues in tests/4.5/application_kernels/mmm_target.c

--- tests/4.5/application_kernels/mmm_target.c
+++ tests/4.5/application_kernels/mmm_target.c
@@ -24,38 +24,38 @@ int main (int argc, char *argv[])
 {
   OMPVV_TEST_OFFLOADING;
   int tid, nthreads, i, j, k;
-  int	*a = (int*)malloc(sizeof(int) * rowA * colA);           // matrix A to be multiplied
-  int	*b = (int*)malloc(sizeof(int) * colA * colB);           // matrix B to be multiplied 
-  int	*c = (int*)malloc(sizeof(int) * rowA * colB);           // result matrix C 
+  int	*a = (int*) malloc(sizeof(int) * rowA * colA);           // matrix A to be multiplied
+  int	*b = (int*) malloc(sizeof(int) * colA * colB);           // matrix B to be multiplied 
+  int	*c = (int*) malloc(sizeof(int) * rowA * colB);           // result matrix C 
 
   // Initialize matrices
-  for (i=0; i<rowA; i++)
-    for (j=0; j<colA; j++)
-      a[i*rowA+j]= 10; // i+j;
-  for (i=0; i<colA; i++)
-    for (j=0; j<colB; j++)
-      b[i*colA+j]= 50; //i*j;
-  for (i=0; i<rowA; i++)
-    for (j=0; j<colB; j++)
-      c[i*rowA+j]= 0;
+  for (i = 0; i < rowA; i++)
+    for (j = 0; j < colA; j++)
+      a[i*rowA+j] = 10; // i+j;
+  for (i = 0; i < colA; i++)
+    for (j = 0; j < colB; j++)
+      b[i*colA+j] = 50; //i*j;
+  for (i = 0; i < rowA; i++)
+    for (j = 0; j < colB; j++)
+      c[i*rowA+j] = 0;
 
-  int DimA=rowA*colA;
-  int DimB=colB*colA;
-  int DimC=rowA*colA;
+  int DimA = rowA*colA;
+  int DimB = colB*colA;
+  int DimC = rowA*colA;
 
 #pragma omp target map(to: a[0:DimA],b[0:DimB]) map(from: c[0:DimC])
   {
-    for (i=0; i<rowA; i++)
-      for(j=0; j<colB; j++)
-        for(k=0; k<colA; k++)
+    for (i = 0; i < rowA; i++)
+      for(j = 0; j < colB; j++)
+        for(k = 0; k < colA; k++)
           c[i*rowA+j] = a[i*rowA+j] * b[k*colA+j];
   }//end-target
 
   // Check results
-  int error=0;
-  for (i=0; i<rowA; i++)
+  int error = 0;
+  for (i = 0; i < rowA; i++)
   {
-    for (j=0; j<colB; j++) {
+    for (j = 0; j < colB; j++) {
       OMPVV_TEST_AND_SET(error, 500 != c[i*rowA+j]);
       OMPVV_ERROR_IF(500 != c[i*rowA+j], "Error: [%d][%d] should be 500 is %d",i,j,c[i*rowA+j]);
     }
