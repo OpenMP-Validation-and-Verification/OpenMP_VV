bc172822064c94bc9ed8d0bbf21c4fa9f9077bc2
Josh Davis <jhdavis@udel.edu>
2020-01-08 11:05:11 -0500

    Fix bad array bound

--- tests/4.5/target_teams_distribute/test_target_teams_distribute_depend_disjoint_section.c
+++ tests/4.5/target_teams_distribute/test_target_teams_distribute_depend_disjoint_section.c
@@ -36,7 +36,7 @@ int test_target_teams_distribute_depend_disjoint_section() {
     for (int x = 0; x < N; ++x) {
       c[x] += a[x] + b[x];
     }
-#pragma omp target teams distribute nowait depend(out: c[N/2:N/2]) map(alloc: a[0:N], b[0:N], c[0:N])
+#pragma omp target teams distribute nowait depend(out: c[N/2:N]) map(alloc: a[0:N], b[0:N], c[0:N])
     for (int x = 0; x < N; ++x) {
       c[x] += 2 * (a[x] + b[x]);
     }
