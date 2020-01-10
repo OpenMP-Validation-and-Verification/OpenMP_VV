239fd4de0aac2648238f209c44cc1ea2af8fbbb6
Joshua Davis <jhdavis@udel.edu>
2020-01-10 14:46:11 -0500

    add fortran template

---
+++ tests/4.5/target_teams_distribute/test_target_teams_distribute_map.F90
@@ -0,0 +1,12 @@
+!===--- test_target_teams_distribute_map.c----------------------------------===//
+!
+! OpenMP API Version 4.5 Nov 2015
+!
+! This test uses the map clause on a target teams distribute clause to test that
+! the indicated data is maped on the target in the fashion that is indicated
+! with the map-type.  Each map-type that is valid in this situation (to, from,
+! alloc, and tofrom) are each used and tested.
+!
+!//===----------------------------------------------------------------------===//
+
+  
