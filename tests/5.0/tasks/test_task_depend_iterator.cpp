6581f6764bf32a5040943be1900b5ae02c3c67a1
fmorac <fabian.mcg@crpl.udel.edu>
2020-02-05 19:35:41 -0500

    Revised version of the task depend iterator test

--- tests/5.0/tasks/test_task_depend_iterator.cpp
+++ tests/5.0/tasks/test_task_depend_iterator.cpp
@@ -20,42 +20,37 @@
 
 int test_task_depend_iterator() {
   int ptr[] = {0, 4, 5, 6, 7, 8, 9, 10, 11}, cols[] = {1, 2, 3, 4, 5, 5, 6, 6, 7, 7, 8};
-  std::vector<int> threadOrder, threadAssignment;
-  bool threadOrderError=false, threadAssignmentWarning=false;
+  std::vector<int> threadOrder;
+  bool threadOrderError = false;
 #pragma omp parallel num_threads(4)
   {
 #pragma omp single
     {
       for (int i = 0; i < 8; ++i) {
-        int pos = ptr[i], size = ptr[i+1] - ptr[i];
-#pragma omp task depend(iterator(it = 0 : size), in : ptr[cols[pos+it]]) depend(out : ptr[i])
+        int pos = ptr[i], size = ptr[i + 1] - ptr[i];
+#pragma omp task depend(iterator(it = 0 : size), in : ptr[cols[pos + it]]) depend(out : ptr[i])
         {
 #pragma omp critical
           {
             threadOrder.push_back(i);
-            threadAssignment.push_back(omp_get_thread_num());
           } // end critical section
           std::this_thread::sleep_for(std::chrono::milliseconds(100));
         } // end task depend
       }
     } // end single
   } // end parallel
-  std::sort(threadOrder.begin()+1,threadOrder.begin()+5);
-  std::sort(threadOrder.begin()+5,threadOrder.begin()+7);
-  std::sort(threadAssignment.begin()+1,threadAssignment.begin()+5);
+  std::sort(threadOrder.begin() + 1, threadOrder.begin() + 5);
+  std::sort(threadOrder.begin() + 5, threadOrder.begin() + 7);
   for(int i = 0; i < 8; ++i)
     threadOrderError = (threadOrder[i] != i) || threadOrderError;
-  for(int i = 0; i < 4; ++i)
-    threadAssignmentWarning = (threadAssignment[i+1] != i) || threadAssignmentWarning;
-  threadAssignmentWarning = (threadAssignment[5] == threadAssignment[6]) || threadAssignmentWarning;
-  OMPVV_WARNING_IF(threadAssignmentWarning, "The thread id assigned to each task may be erroneous.");
   OMPVV_ERROR_IF(threadOrderError, "The dependencies between tasks were not enforced in the correct order.");
   return threadOrderError ? 1 : 0;
 }
 
 
 int main() {
+  OMPVV_TEST_OFFLOADING;
   int errors = 0;
   OMPVV_TEST_AND_SET_VERBOSE(errors, test_task_depend_iterator());
-  OMPVV_RETURN(errors);
+  OMPVV_REPORT_AND_RETURN(errors);
 }
