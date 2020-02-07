6662046b96f9835b0f753c0555a2a71941d706a7
Josh Davis <jhdavis@udel.edu>
2020-02-07 16:06:02 -0500

    Start changing formatting of test

--- tests/4.5/target_enter_exit_data/test_target_enter_exit_data_map_malloced_array.c
+++ tests/4.5/target_enter_exit_data/test_target_enter_exit_data_map_malloced_array.c
@@ -1,48 +1,44 @@
+//===--- test_target_enter_exit_data_map_global_array.c ---------------------===//
+//
+// OpenMP API Version 4.5 Nov 2015
+//
+//
+////===----------------------------------------------------------------------===//
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include "ompvv.h"
 
-// Test for OpenMP 4.5 target enter and target exit data with allocated arrays.
+#define N 10
 
-int n=10;
 int *x;
 
-void init(int **A){
- *A = (int *)malloc(n*sizeof(int));
- if(NULL == *A) exit(-1);
- x=*A;
- #pragma omp target enter data map(to:x[:n])//Note:Mapping *A[:n] is incorrect as OpenMP doesn't support arbitrary expressions
-}
+int main() {
+  int i, errors = 0;
+  int *A;
 
-void finalize(){
- #pragma omp target exit data map(from:x[:n])
-}
+  A = (int *) malloc(n*sizeof(int));
+  if (NULL == A) {
+    exit(-1);
+  }
 
-int main (){
-int isHost=-1,i,errors=0;
-int *A;
+  x = A;
 
-init( &A);
+#pragma omp target enter data map(to: x) // Note: Mapping *A[:n] is incorrect as OpenMP doesn't support arbitrary expressions
 
-#pragma omp target map(tofrom: isHost) map(to: n) 
-{
- /*Record where the computation was executed*/
- isHost = omp_is_initial_device();
+#pragma omp target map(tofrom: isHost)
+  {
+    for (i = 0; i < N; i++) {
+      A[i] = N;
+    }
+  }
 
- for(i=0;i< n; i++)
-   A[i] = 10;
-}
+#pragma omp target exit data map(from: x)
 
- finalize();
- for(i=0; i<n; i++)
-   if(A[i] != 10){
-     errors += 1;
-   }
+  for (i = 0; i < N; i++) {
+    OMPVV_TEST_AND_SET_VERBOSE(errors, A[i] != N);
+  }
 
-  if (!errors)
-    printf("Test passed on %s\n", (isHost ? "host" : "device"));
-  else
-    printf("Test failed on %s\n", (isHost ? "host" : "device"));
-
-  return errors;
+  OMPVV_REPORT_AND_RETURN(errors);
 }
