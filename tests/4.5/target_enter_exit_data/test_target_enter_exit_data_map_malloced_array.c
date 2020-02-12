c34b7d0a19f75bf35e6a0b9ae69e0d0265356649
Josh Davis <jhdavis@udel.edu>
2020-02-12 13:41:54 -0500

    More changes to malloced array

--- tests/4.5/target_enter_exit_data/test_target_enter_exit_data_map_malloced_array.c
+++ tests/4.5/target_enter_exit_data/test_target_enter_exit_data_map_malloced_array.c
@@ -13,21 +13,27 @@
 #define N 10
 
 int *x;
+int *y;
 
-int main() {
+int test_tofrom() {
   int i, errors = 0;
   int *A;
 
-  A = (int *) malloc(n*sizeof(int));
+  A = (int *) malloc(N*sizeof(int));
   if (NULL == A) {
+    OMPVV_ERROR("Malloc returned NULL.");
     exit(-1);
+  }
+
+  for (i = 0; i < N; ++i) {
+    A[i] = 0;
   }
 
   x = A;
 
-#pragma omp target enter data map(to: x) // Note: Mapping *A[:n] is incorrect as OpenMP doesn't support arbitrary expressions
+#pragma omp target enter data map(to: A)
 
-#pragma omp target map(tofrom: isHost)
+#pragma omp target
   {
     for (i = 0; i < N; i++) {
       A[i] = N;
@@ -39,6 +45,58 @@ int main() {
   for (i = 0; i < N; i++) {
     OMPVV_TEST_AND_SET_VERBOSE(errors, A[i] != N);
   }
+
+  return errors;
+}
+
+int test_delete() {
+  int i, errors = 0;
+  int *A, *B;
+
+  A = (int *) malloc(N*sizeof(int));
+  B = (int *) malloc(N*sizeof(int));
+  if (NULL == A || NULL == B) {
+    OMPVV_ERROR("Malloc returned NULL.");
+    exit(-1);
+  }
+
+  for (i = 0; i < N; ++i) {
+    A[i] = N;
+  }
+
+  x = A;
+  y = B;
+
+#pragma omp target data map(tofrom: x) map(from: y)
+  {
+#pragma omp target exit data map(delete: x)
+    for (i = 0; i < N; ++i) {
+      x[i] = 0;
+    }
+#pragma omp target map(to: x)
+    {
+      for (i = 0; i < N; i++) {
+        y[i] = x[i];
+      }
+    }
+  }
+
+  for (i = 0; i < N; i++) {
+    OMPVV_TEST_AND_SET_VERBOSE(errors, B[i] != 0);
+  }
+
+  return errors;
+}
+
+int main() {
+  int errors = 0;
+
+  OMPVV_TEST_OFFLOADING;
+
+  OMPVV_TEST_SHARED_ENVIRONMENT;
+
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_tofrom());
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_delete());
 
   OMPVV_REPORT_AND_RETURN(errors);
 }
