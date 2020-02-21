c41304b30d5afcc58ce7c018d26a243221d24537
tmh97 <thuber@udel.edu>
2020-02-20 21:46:40 -0500

    Fixing macro usage and adding additional test function

--- tests/4.5/target/test_target_map_scalar_default.c
+++ tests/4.5/target/test_target_map_scalar_default.c
@@ -1,9 +1,9 @@
-//===--- test_target_map_escalar_default.c - testing default map to scalar --===//
+//===--- test_target_map_scalar_default.c -----------------------------------===//
 // 
 // OpenMP API Version 4.5 Nov 2015
 // 
 // When no map-type-modifier (e.g. to, from and tofrom) are not specified, the 
-// default behavior should be tofrom. This test check if this is sattisfied with
+// default behavior should be tofrom. This test checks if this is sattisfied with
 // a simple integer value. An array is created an initialized to zero in the host
 // then changed in the device with a scalar value.
 //
@@ -11,23 +11,22 @@
 
 #include <omp.h>
 #include <stdio.h>
+#include "ompvv.h" 
 
 #define N 1000
 
-int main() {
+int test_scalar_to() {
+
   int compute_array[N];
-  int asclr = 12, sum = 0, result = 0;
-  int i, isHost = -1;
+  int asclr = 12, sum = 0, result = 0, errors = 0;
+  int i;
 
   // Array initialization
   for (i = 0; i < N; i++) 
     compute_array[i] = 0;
 
-#pragma omp target map(from: compute_array) map(tofrom: isHost) map(asclr)
+#pragma omp target map(from: compute_array) map(asclr)
   {
-  // Record where the computation was executed
-  isHost = omp_is_initial_device();
-
   for (i = 0; i < N; i++)
     compute_array[i] = i + asclr;
  
@@ -39,13 +38,40 @@ int main() {
   for (i = 0; i < N; i++)
     result += i + asclr;
 
-  if (result != sum) {
-    printf("Test failed on %s\n",isHost ? "host":"device");
-    return 1;
-  }
-  else {
-    printf("Test passed on %s\n", isHost ? "host":"device");
-    return 0;
-  }
+  OMPVV_TEST_AND_SET_VERBOSE(errors, result!= sum);  
 
+  return errors;
+}
+
+int test_scalar_from() {
+
+  int new_scalar = 25;
+  int errors = 0;
+
+#pragma omp target map(new_scalar)
+  {
+  // Test that asclr is default mapped 'to' the device 
+  OMPVV_TEST_AND_SET_VERBOSE(errors, new_scalar != 25);
+  //new_scalar = 15;
+
+  }
+  
+  // Test that asclr is default mapped 'from' back to host
+  //OMPVV_TEST_AND_SET_VERBOSE(errors, new_scalar!=25);
+  
+  return errors;	
+}
+
+int main() {
+  
+  int errors = 0;
+
+  //Test Offloading
+  int is_offloading;
+  OMPVV_TEST_AND_SET_OFFLOADING(is_offloading);
+  OMPVV_WARNING_IF(!is_offloading, "This test in running on host, asclr is not copied over to the device");
+
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_scalar_to());
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_scalar_from());  
+  OMPVV_REPORT_AND_RETURN(errors);
 }
