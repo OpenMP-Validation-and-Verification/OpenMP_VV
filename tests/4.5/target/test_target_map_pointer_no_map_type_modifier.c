ede96d44e35ee49bb8b1f68fe29ba32212d1b5d5
Thomas Huber <thuber@udel.edu>
2020-02-26 20:25:52 -0500

    Update test_target_map_pointer_no_map_type_modifier.c

--- tests/4.5/target/test_target_map_pointer_no_map_type_modifier.c
+++ tests/4.5/target/test_target_map_pointer_no_map_type_modifier.c
@@ -1,4 +1,4 @@
-//===--- test_target_map_pointer_default.c -------------------------------------===//
+//===--- test_target_map_pointer_no_map_type_modifier.c -----------------------===//
 // 
 // OpenMP API Version 4.5 Nov 2015
 // 
