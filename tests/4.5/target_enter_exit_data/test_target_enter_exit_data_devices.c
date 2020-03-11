56655b44df02e8635821a4424a836721f00c04ec
tmh97 <thuber@udel.edu>
2020-03-11 18:39:59 -0400

    Modifiying test_target_enter_exit_data_devices.c

--- tests/4.5/target_enter_exit_data/test_target_enter_exit_data_devices.c
+++ tests/4.5/target_enter_exit_data/test_target_enter_exit_data_devices.c
@@ -2,12 +2,13 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "ompvv.h"
 
 #define N 1000
 
 int test_set_default_dev() {
 
-  printf("test_set_default_dev\n");
+  OMPVV_INFOMSG("test_set_default_dev");
 
   // Get number of devices
   int num_dev = omp_get_num_devices();
@@ -17,29 +18,23 @@ int test_set_default_dev() {
   printf("initial device: %d\n", omp_get_initial_device());
   printf("default device: %d\n", def_dev);
 
-  int sum[num_dev], errors = 0, isHost = 0;
+  int sum[num_dev], errors = 0;
   int h_matrix[num_dev][N];
 
   for (int dev = 0; dev < num_dev; ++dev) {
     omp_set_default_device(dev);
-    // unstructured mapping
-    {
+
 #pragma omp target enter data map(alloc: h_matrix[dev][0 : N])
-        printf(""); // forcing the compiler to not moving out of the scope
-    }
-    // operation
+    
     // assert(dev == omp_get_default_device() && "message");
-#pragma omp target map(alloc: h_matrix[dev][0 : N]) map(tofrom: isHost) // map(alloc: ) to avoid target to map the entire matrix h_matrix[dev][:]
+#pragma omp target map(alloc: h_matrix[dev][0 : N]) // map(alloc: ) to avoid target to map the entire matrix h_matrix[dev][:]
     {
-      isHost = omp_is_initial_device();
       for (int i = 0; i < N; ++i)
         h_matrix[dev][i] = dev;
     }
     // unstructured exit
-    {
+
 #pragma omp target exit data map(from: h_matrix[dev][0 : N])
-        printf("");
-    }
   }
 
   // checking results
@@ -48,13 +43,8 @@ int test_set_default_dev() {
     sum[dev] = h_matrix[dev][0];
     for (int i = 1; i < N; ++i)
       sum[dev] += h_matrix[dev][i];
-    errors |= (dev * N != sum[dev]);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, (dev * N != sum[dev]));
   }
-
-  if (!errors)
-    printf("Test passed on %s: num_devices = %d\n", (isHost ? "host" : "device"), num_dev);
-  else
-    printf("Test failed on %s: num_devices = %d\n", (isHost ? "host" : "device"), num_dev);
 
   omp_set_default_device(def_dev);
 
@@ -63,37 +53,31 @@ int test_set_default_dev() {
 
 int test_device() {
 
-  printf("test_device\n");
+  OMPVV_INFOMSG("test_device");
 
   // Get number of devices
   int num_dev = omp_get_num_devices();
-  printf("num_devices: %d\n", num_dev);
+  OMPVV_INFOMSG("num_devices: %d", num_dev);
 
-  printf("initial device: %d\n", omp_get_initial_device());
-  printf("default device: %d\n", omp_get_default_device());
+  OMPVV_INFOMSG("initial device: %d", omp_get_initial_device());
+  OMPVV_INFOMSG("default device: %d", omp_get_default_device());
 
   int sum[num_dev], errors = 0, isHost = 0;
   int h_matrix[num_dev][N];
 
   for (int dev = 0; dev < num_dev; ++dev) {
-    // unstructured mapping
-    {
+
 #pragma omp target enter data map(alloc: h_matrix[dev][0 : N]) device(dev) 
-        printf("");
-    }
-    // operation
-#pragma omp target map(alloc: h_matrix[dev][0 : N]) map(tofrom: isHost) device(dev)
+
+#pragma omp target map(alloc: h_matrix[dev][0 : N]) device(dev)
     {
-      isHost = omp_is_initial_device();
       for (int i = 0; i < N; ++i)
         h_matrix[dev][i] = dev;
     }
-    // unstructured exit
-    {
+
 #pragma omp target exit data map(from: h_matrix[dev][0 : N]) device(dev)
-        printf("");
-    }
-  }
+  
+}
 
   // checking results
   errors = 0;
@@ -101,103 +85,22 @@ int test_device() {
     sum[dev] = h_matrix[dev][0];
     for (int i = 1; i < N; ++i)
       sum[dev] += h_matrix[dev][i];
-    errors |= (dev * N != sum[dev]);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, (dev * N != sum[dev]));
   }
-
-  if (!errors)
-    printf("Test passed on %s: num_devices = %d\n", (isHost ? "host" : "device"), num_dev);
-  else
-    printf("Test failed on %s: num_devices = %d\n", (isHost ? "host" : "device"), num_dev);
-
 
   return errors;
 }
 
-
-// We are commenting this code out because passing more devices that available
-// is not supported by the specifications. This test is passing on clang/20170629,
-// xl/20170727-beta and gcc/7.1.1-20170802 but produces a side effect runtime error
-// 1587-160 on xl and clang. Gcc produces no error
-
-//int test_more_devices() {
-//
-//  printf("test_more_devices\n");
-//
-//  // Get number of devices
-//  int real_num_dev = omp_get_num_devices();
-//  int num_dev = real_num_dev + 2;
-//  printf("num_devices: %d, real_num_devices: %d\n", num_dev, real_num_dev);
-//
-//  int def_dev = omp_get_default_device();
-//  printf("initial device: %d\n", omp_get_initial_device());
-//  printf("default device: %d\n", def_dev);
-//
-//  int sum[num_dev], errors = 0, extra_on_host = 0, isHost = 0;
-//  int h_matrix[num_dev][N];
-//
-//  // omp_set_default_device is implementation dependent
-//  for (int dev = 0; dev < num_dev; ++dev) {
-//    omp_set_default_device(dev);
-//    int dev_tmp = omp_get_default_device();
-//    // unstructured mapping
-//    {
-//#pragma omp target enter data map(alloc: h_matrix[dev][0 : N])
-//        printf("");
-//    }
-//    // operation
-//    // assert(dev == dev_tmp);
-//#pragma omp target map(alloc: h_matrix[dev][0 : N]) map(tofrom: isHost) map(tofrom : extra_on_host)
-//    {
-//      isHost = omp_is_initial_device();
-//      for (int i = 0; i < N; ++i)
-//        h_matrix[dev][i] = dev;
-//      if (dev >= real_num_dev)
-//          extra_on_host = 1;
-//    }
-//    // unstructured exit
-//    {
-//#pragma omp target exit data map(from: h_matrix[dev][0 : N])
-//        printf("");
-//    }
-//  }
-//
-//  // checking results
-//  errors = 0;
-//  for (int dev = 0; dev < num_dev; ++dev) {
-//    sum[dev] = h_matrix[dev][0];
-//    for (int i = 1; i < N; ++i)
-//      sum[dev] += h_matrix[dev][i];
-//    errors |= (dev * N != sum[dev]);
-//    if (errors)
-//      printf("Error at dev=%d\n", dev);
-//  }
-//
-//  if (!errors)
-//    printf("Test passed: num_devices = %d, real_num_dev = %d, extra_on_host = "
-//           "%d\n",
-//           num_dev, real_num_dev, extra_on_host);
-//  else
-//    printf("Test failed: num_devices = %d, real_num_dev = %d, extra_on_host = "
-//           "%d\n",
-//           num_dev, real_num_dev, extra_on_host);
-//
-//  if (!errors)
-//    printf("Test passed on %s: num_devices = %d, real_num_dev = %d, extra_on_host = %d\n", (isHost ? "host" : "device"), num_dev, real_num_dev, extra_on_host);
-//  else
-//    printf("Test failed on %s: num_devices = %d, real_num_dev = %d, extra_on_host = %d\n", (isHost ? "host" : "device"), num_dev, real_num_dev, extra_on_host);
-//
-//  omp_set_default_device(def_dev);
-//
-//  return errors;
-//}
-
 int main() {
 
   int errors = 0;
+  
+  //Test for offloading
+  int is_offloading;
+  OMPVV_TEST_AND_SET_OFFLOADING(is_offloading);
 
-  errors += test_set_default_dev();
-  errors += test_device();
-  //errors += test_more_devices();
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_set_default_dev());
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_device());
 
-  return errors;
+  OMPVV_REPORT_AND_RETURN(errors);
 }
