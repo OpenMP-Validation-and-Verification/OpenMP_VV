f74a9441ac4a4c8c97a10dac520d65fef7b1739a
tmh97 <thuber@udel.edu>
2020-03-18 21:24:01 -0400

    Making recommended changes

--- tests/4.5/target_enter_data/test_target_enter_data_malloced_array.c
+++ tests/4.5/target_enter_data/test_target_enter_data_malloced_array.c
@@ -17,7 +17,6 @@
 #include <omp.h>
 #include "ompvv.h"
 
-// Test for OpenMP 4.5 target enter and target exit data with allocated arrays.
 
 // global variables
 int n=10, B[10];
@@ -25,15 +24,19 @@ int *x;
 
 
 void init(int **A) {
- int i;
- *A = (int *) malloc(n*sizeof(int));
- if (NULL == *A) exit(-1);
- x = *A;
- for (i = 0; i < n; i++){
-   x[i] = 10;
-   B[i] = 0;
- }
+  int i;
+  *A = (int *) malloc(n*sizeof(int));
+  if (NULL == *A){ 
+    OMPVV_ERROR("This Test Has Failed, disregard other messages, array A is not properly allocated");
+    exit(-1);
+  }
+  x = *A;
+  for (i = 0; i < n; i++){
+    x[i] = 10;
+    B[i] = 0;
+}
  
+
 #pragma omp target enter data map(to:x[:n])//Note:Mapping *A[:n] is incorrect as OpenMP doesn't support arbitrary expressions
 }
 
@@ -44,18 +47,17 @@ int main () {
   OMPVV_TEST_AND_SET_OFFLOADING(is_offloading);
   int i, errors = 0;
   int *A;
-
   init(&A);
 
 #pragma omp target map(to: n) map(tofrom: B)
 {
- for (i = 0; i < n; i++)
-   B[i] = x[i];
+  for (i = 0; i < n; i++)
+    B[i] = x[i];
 }
 
 // finalize();
- for (i = 0; i < n; i++)
-   if (B[i] != 10) {
+  for (i = 0; i < n; i++)
+    if (B[i] != 10) {
      errors += 1;
    }
  
