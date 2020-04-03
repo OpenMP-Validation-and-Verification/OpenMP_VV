32913dd45b03359dbe9cc1aa9130fe914071965c
Josh Davis <jhdavis@udel.edu>
2020-04-03 14:01:24 -0400

    Add general static lib for ompvv suite

---
+++ ompvv/libompvv.c
@@ -0,0 +1,18 @@
+//===------ libompvv.c --------------- OMPVV STATIC LIBRARY ---------------===//
+//
+// Static lib file for OMP Validation and verification test suite.
+//
+//===----------------------------------------------------------------------===//
+
+#include "omp.h"
+
+int offload_test() {
+  int a = 0;
+
+#pragma omp target map(tofrom: a)
+  {
+    a = 1;
+  }
+
+  return a;
+}
---
+++ ompvv/libompvv.h
@@ -0,0 +1,7 @@
+//===------ libompvv.h --------------- OMPVV STATIC LIBRARY HEADER --------===//
+//
+// Static lib header file for OMP Validation and verification test suite.
+//
+//===----------------------------------------------------------------------===//
+
+int offload_test(void);
