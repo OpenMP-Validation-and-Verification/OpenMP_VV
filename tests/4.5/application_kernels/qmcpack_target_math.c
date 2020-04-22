ad6a7c580ebdd07087bae11bc4fd6a02238d7f25
Swaroop Pophale <pophaless@ornl.gov>
2020-04-21 20:27:16 -0400

    Adding dummy line as Ye Luo suggested

--- tests/4.5/application_kernels/qmcpack_target_math.c
+++ tests/4.5/application_kernels/qmcpack_target_math.c
@@ -26,9 +26,11 @@ int test_math_lib_inside_target() {
   
   // Array initialization
   for (int i = 0; i < N; ++i) {
-    array[i] = FP_ZERO;
+    array[i] = 0.99;
   }
-
+  // This is intentional
+  int c99_zero = FP_ZERO;
+  
 #pragma omp target map(tofrom: array[0:N]) 
   for (int i = 0; i < N; ++i) {
     array[i] = pow((double)i,2.0);
