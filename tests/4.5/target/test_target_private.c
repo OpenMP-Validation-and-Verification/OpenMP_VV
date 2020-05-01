b662cfcf8a50a73ca7a5122161802d34842773ad
Swaroop Pophale <swaroop.pophale23@gmail.com>
2020-05-01 13:02:26 -0400

    Update test_target_private.c
    Addressing @tob2 's and @jhdavis8 's comments.

--- tests/4.5/target/test_target_private.c
+++ tests/4.5/target/test_target_private.c
@@ -5,7 +5,7 @@
 // Testing private clause with target directive. The test begins by initializing
 // and filling a 2-D array with all zeros and generating four threads. In a parallel
 // region inside the target region, the mapped 2-D array is assigned the integer 
-// value equal to the thread number  assigned to the private variable.
+// value equal to the thread number assigned to the private variable.
 // Finally, back on the host, we check that array is properly filled.
 //
 ////===--------------------------------------------------------------------------===//
@@ -33,14 +33,13 @@ int main() {
       compute_array[i][j] = 0;
 
 
-    
-
-#pragma omp target map(tofrom:compute_array, real_num_threads) private(p_val)
+#pragma omp target map(tofrom: compute_array) (from: real_num_threads) private(p_val)
 {
 #pragma omp parallel private(p_val,i) num_threads(4)
   {
     p_val = omp_get_thread_num();
     real_num_threads = omp_get_num_threads();
+    OMPVV_WARNING_IF(real_num_threads == 1, "The number of threads creatable was 1. This is not a specifications error but we could not confirm privatization.");
     for (i = 0; i < N; i++)
       compute_array[p_val][i] += p_val;
   } // end parallel
@@ -49,7 +48,6 @@ int main() {
   for (i = 0; i < real_num_threads; i++){ 
     for (j = 0; j < N; j++){
       OMPVV_TEST_AND_SET_VERBOSE(errors, (compute_array[i][j] != i));
-      OMPVV_ERROR_IF(compute_array[i][j] != 100, "compute_array[%d][%d] = %d\n",i,j,compute_array[i][j]);
     }
   }//end for
   
