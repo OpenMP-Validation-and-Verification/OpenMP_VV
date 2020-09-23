7c19295b19de0a58551c4639e462fcbb7e32d0d7
Jacob Weightman <jacobdweightman@gmail.com>
2020-09-23 13:10:48 -0500

    test_declare_target_nested.c globals must be declared for target (#201)
    * test_declare_target_nested.c globals must be declared for target
    
    * Update test_declare_target_nested.c
    
    Adding update from for arrays a,b,c, allowing us to check for correct values on host.
    
    Co-authored-by: Jacob Weightman <weightma@node015.cm.cluster>
    Co-authored-by: Thomas Huber <thuber@udel.edu>

--- tests/5.0/declare_target/test_declare_target_nested.c
+++ tests/5.0/declare_target/test_declare_target_nested.c
@@ -21,13 +21,13 @@
 
 #pragma omp declare target
 int a[N], b[N], c[N]; // implicit map 3 variables 
+int errors = 0;
+int i = 0;
   #pragma omp declare target
      int test_target();
   #pragma omp end declare target
 #pragma omp end declare target
 
-int errors = 0;
-int i = 0;
 int test_target() { //function in declare target statement 
 
 //change values on device
@@ -38,7 +38,6 @@ int test_target() { //function in declare target statement
     c[i] = 15;
   }
 
-  //confirm updated values on host
   for (i = 0; i < N; i++) {
     if ( a[i] != 5 || b[i] != 10 || c[i] != 15) {
       errors++;  
@@ -52,6 +51,13 @@ int test_wrapper() { //wrapper for declare target function
   #pragma omp target 
   {
     test_target();
+  }
+  #pragma omp target update from(errors, a, b, c)
+  
+  for (i = 0; i < N; i++) { //check array values on host
+    if ( a[i] != 5 || b[i] != 10 || c[i] != 15) {
+      errors++;  
+    } 
   }
   return errors;
 }
