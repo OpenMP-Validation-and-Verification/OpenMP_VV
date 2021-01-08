67f05fa43a38368d1b8cf973d7988e6e70a074bb
Josh Davis <jhdavis@udel.edu>
2021-01-08 17:22:57 -0500

    Change bind test to use nested loops, fix header comment, add version of test w no bind clause

--- tests/5.0/loop/test_loop_bind.c
+++ tests/5.0/loop/test_loop_bind.c
@@ -2,12 +2,11 @@
 //
 // OpenMP API Version 5.0 Nov 2018
 //
-// This test checks the loop directive with the order(concurrent) clause. The
-// order(concurrent) clause is assumed to be present if it is not present, so
-// this test covers the standalone loop directive as well. The test creates a
-// parallel region with a loop construct nested within, and performs simple
-// operations on an int array which are then checked for correctness. Note
-// that the number of
+// This test checks the loop directive with the bind(binding) clause. The bind
+// clause indicates that the loop construct should apply in the context of the
+// given binding, one of teams, parallel, or thread. Each of these bindings
+// is tested in an appropriate context and the correctness of results of
+// array operations in the nested loop is checked.
 //
 ////===----------------------------------------------------------------------===//
 #include <assert.h>
@@ -21,13 +20,15 @@
 int test_loop_bind_teams() {
   OMPVV_INFOMSG("test_loop_bind_teams");
   int errors = 0;
-  int x[N];
+  int x[N][N];
   int y[N];
   int z[N];
   int num_teams = -1;
 
   for (int i = 0; i < N; i++) {
-    x[i] = 1;
+    for (int j = 0; j < N; j++) {
+      x[i][j] = 1;
+    }
     y[i] = i;
     z[i] = 2*i;
   }
@@ -36,16 +37,21 @@ int test_loop_bind_teams() {
   {
 #pragma omp loop bind(teams)
     for (int i = 0; i < N; i++) {
-      x[i] += y[i]*z[i];
+      for (int j = 0; j < N; j++) {
+        x[i][j] += y[i]*z[i];
+      }
     }
-    if (omp_get_team_num() == 0) {
+    if (omp_get_thread_num() == 0 && omp_get_team_num() == 0) {
       num_teams = omp_get_num_teams();
     }
   }
 
   for (int i = 0; i < N; i++) {
-    OMPVV_TEST_AND_SET_VERBOSE(errors, x[i] != 1 + (y[i]*z[i]));
+    for (int j = 0; j < N; j++) {
+      OMPVV_TEST_AND_SET_VERBOSE(errors, x[i][j] != 1 + (y[i]*z[i]));
+    }
   }
+
 
   OMPVV_WARNING_IF(num_teams == 1, "Test ran with one team, so parallelism of loop construct with bind(teams) can't be guaranteed.");
   OMPVV_TEST_AND_SET_VERBOSE(errors, num_teams < 1);
@@ -70,17 +76,16 @@ int test_loop_bind_parallel() {
     z[i] = 2*i;
   }
 
-#pragma omp teams distribute num_teams(OMPVV_NUM_TEAMS_DEVICE) thread_limit(OMPVV_NUM_THREADS_HOST)
-  for (int i = 0; i < N; i++) {
 #pragma omp parallel num_threads(OMPVV_NUM_THREADS_HOST)
-    {
+  {
 #pragma omp loop bind(parallel)
+    for (int i = 0; i < N; i++) {
       for (int j = 0; j < N; j++) {
         x[i][j] += y[i]*z[i];
       }
-      if (omp_get_thread_num() == 0 && omp_get_team_num() == 0) {
-        num_threads = omp_get_num_threads();
-      }
+    }
+    if (omp_get_thread_num() == 0 && omp_get_team_num() == 0) {
+      num_threads = omp_get_num_threads();
     }
   }
 
@@ -97,8 +102,50 @@ int test_loop_bind_parallel() {
   return errors;
 }
 
-int test_loop_bind_thread() {
-  OMPVV_INFOMSG("test_loop_bind_thread");
+int test_loop_bind_thread_teams() {
+  OMPVV_INFOMSG("test_loop_bind_thread_teams");
+  int errors = 0;
+  int x[N][N];
+  int y[N];
+  int z[N];
+  int num_teams = -1;
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      x[i][j] = 1;
+    }
+    y[i] = i;
+    z[i] = 2*i;
+  }
+
+#pragma omp teams num_teams(OMPVV_NUM_TEAMS_DEVICE) thread_limit(OMPVV_NUM_THREADS_HOST)
+  {
+#pragma omp loop bind(thread)
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        x[i][j] += y[i]*z[i];
+      }
+    }
+    if (omp_get_thread_num() == 0 && omp_get_team_num() == 0) {
+      num_teams = omp_get_num_teams();
+    }
+  }
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      OMPVV_TEST_AND_SET_VERBOSE(errors, x[i][j] != (1 + num_teams*(y[i]*z[i])));
+    }
+  }
+
+  OMPVV_WARNING_IF(num_teams == 1, "Test ran with one team, so parallelism of loop construct can't be guaranteed.");
+  OMPVV_TEST_AND_SET_VERBOSE(errors, num_teams < 1);
+  OMPVV_ERROR_IF(num_teams < 1, "omp_get_num_teams() returned an invalid number of teams.");
+
+  return errors;
+}
+
+int test_loop_bind_thread_parallel() {
+  OMPVV_INFOMSG("test_loop_bind_thread_parallel");
   int errors = 0;
   int x[N][N];
   int y[N];
@@ -113,17 +160,16 @@ int test_loop_bind_thread() {
     z[i] = 2*i;
   }
 
-#pragma omp teams distribute num_teams(OMPVV_NUM_TEAMS_DEVICE) thread_limit(OMPVV_NUM_THREADS_HOST)
-  for (int i = 0; i < N; i++) {
 #pragma omp parallel num_threads(OMPVV_NUM_THREADS_HOST)
-    {
+  {
 #pragma omp loop bind(thread)
+    for (int i = 0; i < N; i++) {
       for (int j = 0; j < N; j++) {
         x[i][j] += y[i]*z[i];
       }
-      if (omp_get_thread_num() == 0 && omp_get_team_num() == 0) {
-        num_threads = omp_get_num_threads();
-      }
+    }
+    if (omp_get_thread_num() == 0 && omp_get_team_num() == 0) {
+      num_threads = omp_get_num_threads();
     }
   }
 
@@ -145,7 +191,8 @@ int main() {
 
   OMPVV_TEST_AND_SET_VERBOSE(errors, test_loop_bind_teams());
   OMPVV_TEST_AND_SET_VERBOSE(errors, test_loop_bind_parallel());
-  OMPVV_TEST_AND_SET_VERBOSE(errors, test_loop_bind_thread());
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_loop_bind_thread_teams());
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_loop_bind_thread_parallel());
 
   OMPVV_REPORT_AND_RETURN(errors);
 }
