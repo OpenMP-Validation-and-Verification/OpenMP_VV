de6edcb73ddefa35f3d3e0c8168ac5aaa4e01c28
Josh Davis <jhdavis@udel.edu>
2021-01-12 15:07:40 -0500

    Add check to make sure all threads wait before leaving loop region

--- tests/5.0/loop/test_loop_order_concurrent.c
+++ tests/5.0/loop/test_loop_order_concurrent.c
@@ -23,6 +23,7 @@
 int test_loop_order_concurrent() {
   OMPVV_INFOMSG("test_loop_order_concurrent");
   int errors = 0;
+  int total_wait_errors = 0;
   int x[N];
   int y[N];
   int z[N];
@@ -30,16 +31,24 @@ int test_loop_order_concurrent() {
 
   for (int i = 0; i < N; i++) {
     x[i] = 1;
-    y[i] = i;
-    z[i] = 2*i;
+    y[i] = i + 1;
+    z[i] = 2*(i + 1);
   }
 
-#pragma omp parallel num_threads(OMPVV_NUM_THREADS_HOST)
+#pragma omp parallel num_threads(OMPVV_NUM_THREADS_HOST) shared(total_wait_errors, x, y, z, num_threads)
   {
+    int wait_errors = 0;
 #pragma omp loop
     for (int i = 0; i < N; i++) {
       x[i] += y[i]*z[i];
     }
+    for (int i = 0; i < N; i++) {
+      if (x[i] == 1) {
+        wait_errors++;
+      }
+    }
+#pragma omp atomic update
+    total_wait_errors += wait_errors;
     if (omp_get_thread_num() == 0) {
       num_threads = omp_get_num_threads();
     }
@@ -52,8 +61,9 @@ int test_loop_order_concurrent() {
   OMPVV_WARNING_IF(num_threads == 1, "Test ran with one thread, so parallelism of loop construct can't be guaranteed.");
   OMPVV_TEST_AND_SET_VERBOSE(errors, num_threads < 1);
   OMPVV_ERROR_IF(num_threads < 1, "omp_get_num_threads() returned an invalid number of threads.");
+  OMPVV_ERROR_IF(total_wait_errors, "Threads in parallel region did not wait for loop region to finish before proceeding.");
 
-  return errors;
+  return errors + total_wait_errors;
 }
 
 
