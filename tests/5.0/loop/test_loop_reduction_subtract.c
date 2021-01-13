b7674f530111522fea71bd585199a1f36d32a098
Joshua Hoke Davis <32313271+jhdavis8@users.noreply.github.com>
2021-01-13 16:14:06 -0500

    Update test_loop_reduction_subtract.c

--- tests/5.0/loop/test_loop_reduction_subtract.c
+++ tests/5.0/loop/test_loop_reduction_subtract.c
@@ -47,13 +47,13 @@ int test_subtraction() {
   }
 
   for (int x = 1; x < N; ++x) {
-    OMPVV_WARNING_IF(num_threads[x - 1] != num_threads[x], "Kernel reported differing numbers of threads.  Validity of testing of reduction clause cannot be guaranteed.");
+    OMPVV_WARNING_IF(num_threads[x - 1] != num_threads[x], "Test reported differing numbers of threads.  Validity of testing of reduction clause cannot be guaranteed.");
   }
   OMPVV_WARNING_IF(num_threads[0] == 1, "Test operated with one thread.  Reduction clause cannot be tested.");
   OMPVV_WARNING_IF(num_threads[0] <= 0, "Test reported invalid number of threads.  Validity of testing of reduction clause cannot be guaranteed.");
 
   OMPVV_TEST_AND_SET_VERBOSE(errors, host_total != total);
-  OMPVV_ERROR_IF(host_total != total, "Total on device is %d but expected total from host is %d.", total, host_total);
+  OMPVV_ERROR_IF(host_total != total, "Total from loop directive is %d but expected total is %d.", total, host_total);
 
   return errors;
 }
