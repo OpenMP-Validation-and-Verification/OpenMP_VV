658a869c22aae30be11b27d8d759503b0a156e01
Josh Davis <jhdavis@udel.edu>
2021-01-25 12:29:58 -0500

    Add proper error messages for failed alloc

--- tests/5.0/parallel_for/test_parallel_for_allocate.c
+++ tests/5.0/parallel_for/test_parallel_for_allocate.c
@@ -24,9 +24,17 @@ int test_parallel_for_allocate() {
   int errors = 0;
   int* x;
   int result[N][N];
+  int successful_alloc = 0;
+
   omp_memspace_handle_t x_memspace = omp_default_mem_space;
   omp_alloctrait_t x_traits[1] = {omp_atk_alignment, 64};
   omp_allocator_handle_t x_alloc = omp_init_allocator(x_memspace, 1, x_traits);
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      result[i][j] = -1;
+    }
+  }
 
 #pragma omp parallel for allocate(x_alloc: x) private(x) shared(result) num_threads(OMPVV_NUM_THREADS_HOST)
   for (int i = 0; i < N; i++) {
@@ -40,8 +48,12 @@ int test_parallel_for_allocate() {
         result[i][j] = x[j];
       }
       free(x);
+      successful_alloc++;
     }
   }
+
+  OMPVV_ERROR_IF(successful_alloc < 1, "Failed to allocate x");
+  OMPVV_TEST_AND_SET_VERBOSE(errors, successful_alloc < 1);
 
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
