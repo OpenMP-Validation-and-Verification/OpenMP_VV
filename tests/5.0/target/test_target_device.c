0d4a539fb4eb04dcc73ad88e1d27c4fa64d66508
tmh97 <thuber@udel.edu>
2021-02-22 16:31:48 -0500

    New 5.0 test for target construct with device clause

---
+++ tests/5.0/target/test_target_device.c
@@ -0,0 +1,81 @@
+//===--- test_target_device.c ----------------------------------------------===//
+//
+// OpenMP API Version 5.0 Nov 2018
+//
+// This test checks the target construct with device clause where device-
+// modifier is either ancestor or device_num. If no device_modifier is 
+// present, the behavior is the same as if device_num were present.
+//
+////===---------------------------------------------------------------------===//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <omp.h>
+#include "ompvv.h"
+
+#define N 1028
+
+int test_target_device_ancestor() {
+
+    int i, which_device;
+    int a[N];
+    int errors = 0; 
+
+    for (int i = 0; i < N; i++) {
+        a[i] = i;
+    }
+
+    #pragma omp target device(ancestor: 1) map(tofrom: a, which_device) 
+    {
+        for (int i = 0; i < N; i++) {
+            a[i] = a[i] + 2;
+        }
+ 
+        which_device = omp_is_initial_device();
+    }
+
+    OMPVV_TEST_AND_SET_VERBOSE(errors, which_device != 1);
+    OMPVV_WARNING_IF(which_device != 1, "Target region was executed on device. Due to ancestor device-modifier,"
+                                         "this region should execute on host");
+
+    return errors;
+
+}
+
+int test_target_device_device_num() {
+    
+    int i, which_device;
+    int b[N];
+    int errors = 0; 
+
+    for (int i = 0; i < N; i++) {
+        b[i] = i;
+    }
+
+    #pragma omp target device(device_num: 1) map(tofrom: b, which_device) 
+    {
+        for (int i = 0; i < N; i++) {
+            b[i] = b[i] + 2;
+        }
+ 
+        which_device = omp_is_initial_device();
+    }
+
+    OMPVV_TEST_AND_SET_VERBOSE(errors, which_device != 0);
+    OMPVV_WARNING_IF(which_device != 0, "Target region was executed on host. Due to device num device-modifier,"
+                                         "this region should execute on specified target device");    
+
+    return errors;
+
+}
+
+int main() {
+
+    int errors = 0;
+   
+    OMPVV_TEST_AND_SET_VERBOSE(errors, test_target_device_ancestor());
+    
+    OMPVV_TEST_AND_SET_VERBOSE(errors, test_target_device_device_num());
+
+    OMPVV_REPORT_AND_RETURN(errors);
+}
