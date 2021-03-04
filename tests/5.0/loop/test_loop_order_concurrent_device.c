66880460f21d1a8496c0741152c033b52bd6e2b3
Joshua Hoke Davis <32313271+jhdavis8@users.noreply.github.com>
2021-03-04 11:06:12 -0500

    Add 5.0 test of loop with order concurrent on device (#258)
    * Add 5.0 test of loop with order concurrent on device
    
    * Update test_loop_order_concurrent_device.c
    
    * Update tests to check for barrier errors at only one other random index

--- tests/5.0/loop/test_loop_order_concurrent_device.c
+++ tests/5.0/loop/test_loop_order_concurrent_device.c
@@ -1,17 +1,17 @@
-//===--- test_loop_order_concurrent.c ---------------------------------------===//
+//===--- test_loop_order_concurrent_device.c --------------------------------===//
 //
 // OpenMP API Version 5.0 Nov 2018
 //
-// This test checks the loop directive with the order(concurrent) clause.
-// The order(concurrent) clause is assumed to be present if it is not
-// present, so this test covers the standalone loop directive as well. The
-// test creates a parallel region with a loop construct nested within, and
-// performs simple operations on an int array which are then checked for
-// correctness. Additionally, since loop binds to a parallel region, the test
-// checks randomly that other threads wait before proceeding out of the loop
-// region. The number of threads is checked in the parallel region but after
-// the loop construct because runtime API calls are not permitted in loop
-// directive regions.
+// This test checks the loop directive with the order(concurrent) clause, in
+// the context of a target construct. The order(concurrent) clause is assumed
+// to be present if it is not present, so this test covers the standalone
+// loop directive as well. The test creates a parallel region with a loop
+// construct nested within, and performs simple operations on an int array
+// which are then checked for correctness. Additionally, since loop binds to
+// a parallel region, the test checks randomly that other threads wait before
+// proceeding out of the loop region. The number of threads is checked in the
+// parallel region but after the loop construct because runtime API calls are
+// not permitted in loop directive regions.
 //
 ////===----------------------------------------------------------------------===//
 #include <assert.h>
@@ -23,15 +23,15 @@
 
 #define N 1024
 
-int test_loop_order_concurrent() {
-  OMPVV_INFOMSG("test_loop_order_concurrent");
+int test_loop_order_concurrent_device() {
+  OMPVV_INFOMSG("test_loop_order_concurrent_device");
   int errors = 0;
   int total_wait_errors = 0;
   int x[N];
   int y[N];
   int z[N];
   int num_threads = -1;
-  int rand_indexes[OMPVV_NUM_THREADS_HOST];
+  int rand_indexes[OMPVV_NUM_THREADS_DEVICE];
 
   for (int i = 0; i < N; i++) {
     x[i] = 1;
@@ -39,11 +39,11 @@ int test_loop_order_concurrent() {
     z[i] = 2*(i + 1);
   }
 
-  for (int i = 0; i < OMPVV_NUM_THREADS_HOST; i++) {
+  for (int i = 0; i < OMPVV_NUM_THREADS_DEVICE; i++) {
     rand_indexes[i] = rand()%(N + 1);
   }
 
-#pragma omp parallel num_threads(OMPVV_NUM_THREADS_HOST)
+#pragma omp target parallel num_threads(OMPVV_NUM_THREADS_DEVICE) map(tofrom: x[0:N], num_threads, total_wait_errors) map(to: y[0:N], z[0:N])
   {
 #pragma omp loop order(concurrent)
     for (int i = 0; i < N; i++) {
@@ -72,11 +72,13 @@ int test_loop_order_concurrent() {
 
 
 int main() {
+  OMPVV_TEST_OFFLOADING;
+
   int errors = 0;
 
   srand(time(0));
 
-  OMPVV_TEST_AND_SET_VERBOSE(errors, test_loop_order_concurrent());
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_loop_order_concurrent_device());
 
   OMPVV_REPORT_AND_RETURN(errors);
 }
