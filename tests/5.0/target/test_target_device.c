850149ef92553cb91dea57502f2994e9bea47bc9
tmh97 <thuber@udel.edu>
2021-03-27 19:17:18 -0400

    Making recommended changes

--- tests/5.0/target/test_target_device.c
+++ tests/5.0/target/test_target_device.c
@@ -25,16 +25,23 @@ int test_target_device_ancestor() {
         a[i] = i;
     }
 
-    #pragma omp target device(ancestor: 1) map(tofrom: a, which_device) 
-    {
-        for (int i = 0; i < N; i++) {
-            a[i] = a[i] + 2;
-        }
- 
-        which_device = omp_is_initial_device();
+    OMPVV_TEST_AND_SET(errors, omp_get_num_devices() <= 0);
+    OMPVV_ERROR_IF(omp_get_num_devices() <= 0, "There are no target devices available, cannot properly "
+                                                 "test fallback to host");
+
+    if (omp_get_num_devices() > 0) {
+
+        #pragma omp target device(ancestor: 1) map(tofrom: a, which_device) 
+	{
+	    for (int i = 0; i < N; i++) {
+                a[i] = a[i] + 2;
+	    }
+	
+	    which_device = omp_is_initial_device();
+	}
     }
 
-    OMPVV_TEST_AND_SET_VERBOSE(errors, which_device != 1);
+    OMPVV_TEST_AND_SET(errors, which_device != 1);
     OMPVV_ERROR_IF(which_device != 1, "Target region was executed on device. Due to ancestor device-modifier,"
                                          "this region should execute on host");
 
@@ -44,7 +51,7 @@ int test_target_device_ancestor() {
 
 int test_target_device_device_num() {
     
-    int i, which_device, host_device_num, first_device_num;
+    int i, target_device_num, host_device_num, first_device_num;
     int b[N];
     int errors = 0; 
 
@@ -55,26 +62,25 @@ int test_target_device_device_num() {
     host_device_num = omp_get_device_num(); 
 
     
-    OMPVV_TEST_AND_SET_VERBOSE(errors, omp_get_num_devices <= 0);
-    OMPVV_ERROR_IF(omp_get_num_devices <= 0, "Test fails, there are no target devices available");         
+    OMPVV_TEST_AND_SET(errors, omp_get_num_devices() <= 0);
+    OMPVV_ERROR_IF(omp_get_num_devices() <= 0, "There are no target devices available, cannot properly test");         
 
     if (omp_get_num_devices() > 0) {
          
-        first_device_num = host_device_num + 1;
-   
-        #pragma omp target device(device_num: first_device_num) map(tofrom: b, which_device) 
+        first_device_num = omp_get_num_devices() - 1;
+        #pragma omp target device(device_num: first_device_num) map(tofrom: b, target_device_num) 
         {
             for (int i = 0; i < N; i++) {
                 b[i] = b[i] + 2;
             }
  
-            which_device = omp_is_initial_device();
+            target_device_num = omp_get_device_num();
         }
     }
 
-    OMPVV_TEST_AND_SET_VERBOSE(errors, which_device != 0);
-    OMPVV_ERROR_IF(which_device != 0, "Target region was executed on host. Due to device num device-modifier,"
-                                         "this region should execute on specified target device");   
+    OMPVV_TEST_AND_SET(errors, target_device_num == host_device_num);
+    OMPVV_ERROR_IF(target_device_num == host_device_num, "Target region was executed on host," 
+                   "this region should execute on specified target device number");   
 
     return errors;
 
