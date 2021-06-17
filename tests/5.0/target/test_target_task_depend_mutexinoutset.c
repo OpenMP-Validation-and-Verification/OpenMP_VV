d65c63131768119e9b6f95474aabe90e154424d1
Swaroop Pophale <swaroop.pophale23@gmail.com>
2021-06-17 09:18:36 -0400

    Update test_target_task_depend_mutexinoutset.c
    Adding device check.

--- tests/5.0/target/test_target_task_depend_mutexinoutset.c
+++ tests/5.0/target/test_target_task_depend_mutexinoutset.c
@@ -51,6 +51,8 @@ int test_target_task_depend_mutexinoutset() {
 int main() {
   
   int errors = 0;
+  
+  OMPVV_TEST_OFFLOADING;
   OMPVV_TEST_AND_SET_VERBOSE(errors, test_target_task_depend_mutexinoutset());
 
   OMPVV_REPORT_AND_RETURN(errors);
