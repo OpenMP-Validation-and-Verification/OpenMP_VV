e9cfe7f2f899833548b0c0c4ad62b5a847dfb0a2
Swaroop Pophale <swaroop.pophale23@gmail.com>
2021-09-30 15:42:21 -0400

    Update test_declare_target_device_type_nohost.c (#304)
    * Update test_declare_target_device_type_nohost.c
    
    NOTE: Functions declared as nohost require additional protection against fall back mechanism that may require it to execute on host.
    
    * Update test_declare_target_device_type_nohost.c
    
    * WIP
    
    * Added declare variant directive to protect device only function.
    
    * Pending change not oushed.
    
    * Update test_declare_target_device_type_nohost.c
    
    Thanks @jacobdweightman !
    
    Co-authored-by: Swaroop Pophale <pophaless@ornl.gov>

--- tests/5.0/declare_target/test_declare_target_device_type_nohost.c
+++ tests/5.0/declare_target/test_declare_target_device_type_nohost.c
@@ -7,49 +7,59 @@
 // clause is present on the contained declare target directive, then its argument 
 // determines which versions are made available. If device_type(nohost) is present 
 // only a device version of the procedure is made available. 
+// The device version of the function is specified via declare variant.
 // 
 //===-------------------------------------------------------------------------------===//
 
 #include <omp.h>
 #include <stdio.h>
+#include <assert.h>
 #include <stdlib.h>
 #include "ompvv.h"
 
-#define N 1024
+#define N 10
 int errors = 0;
+
+void target_function();
 
 #pragma omp declare target
 int a[N], b[N], c[N];  
 int i = 0;
 #pragma omp end declare target
 
-void update() { 
+#pragma omp declare variant(target_function) match(device={kind(nohost)})
+void update() {
+  for (i = 0; i < N; i++) {
+    a[i] += 5;
+    b[i] += 5;
+    c[i] += 5;
+  }
+}
+
+#pragma omp declare target device_type(nohost)
+void target_function(){
   for (i = 0; i < N; i++) {
     a[i] += 1;
     b[i] += 2;
     c[i] += 3;
   }
-}
-
-#pragma omp declare target to(update) device_type(nohost)
+} 
 
 int test_declare_target_device_type_nohost() { 
 
+  update();
+
   #pragma omp target update to(a,b,c)
+
   #pragma omp target  
   {
     update();
-
-    for (i = 0; i < N; i++) {
-      a[i] += i;
-      b[i] += 2 * i;
-      c[i] += 3 * i;
-    }
   }
+
   #pragma omp target update from (a,b,c)
   
   for (i = 0; i < N; i++) { //check array values on host
-    if ( a[i] != 2 * i + 1 || b[i] != 4 * i + 2 || c[i] != 6 * i + 3 ) {
+    if ( a[i] != 6 || b[i] != 7 || c[i] != 8 ) {
       errors++;
     }
   }
@@ -58,12 +68,14 @@ int test_declare_target_device_type_nohost() {
 }
 
 int main () {
+
+  OMPVV_TEST_OFFLOADING;
   
   //initalize arrays on host
   for (i = 0; i < N; i++) {
-    a[i] = i;
-    b[i] = 2 * i;
-    c[i] = 3 * i;
+    a[i] = 0;
+    b[i] = 0;
+    c[i] = 0;
   }
 
   OMPVV_TEST_AND_SET_VERBOSE(errors, test_declare_target_device_type_nohost());
