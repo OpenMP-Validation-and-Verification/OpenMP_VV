45cb157664fab999602811b2dba4df7774bfa06f
tmh97 <thuber@udel.edu>
2021-10-26 16:16:20 -0400

    Removing data map alloc, adding locator-list alongside present motion-modifier

--- tests/5.1/motion/test_motion_present.c
+++ tests/5.1/motion/test_motion_present.c
@@ -17,35 +17,37 @@
 
 #define N 1024
 
-int errors, i;
+int errors;
+int i;
 
 int test_motion_present() {
    
+   errors = 0;
+
    struct test_struct {
      int s; 
      int S[N]; 
    }; 
 
-   int scalar; // scalar 
+   int scalar_var; // scalar 
    int A[N]; // aggregate 
    struct test_struct new_struct; // aggregate
    int *ptr; // scalar, pointer 
 
-   scalar = 1; 
+   scalar_var = 1; 
    A[0] = 0; A[50] = 50;
    new_struct.s = 10; new_struct.S[0] = 10; new_struct.S[1] = 10;
    ptr = &A[0]; 
    ptr[50] = 50; ptr[51] = 51;
 
-   #pragma omp target enter data map(alloc:scalar) map(alloc: A[N]) map(alloc: new_struct) //map(alloc: *ptr)
-   #pragma omp target update to(present) // Present means values should remain as if already declared in map as alloc
-   #pragma omp target map(tofrom: errors)
+   #pragma omp target update to(present: scalar_var, new_struct) // Only looking to test lines 5-6 on page 207
+   #pragma omp target map(tofrom: errors) defaultmap(none) //map(tofrom: errors) map(from: scalar_var, A, new_struct) 
    {     
-        if(scalar != 1){errors++;}
+        if(scalar_var != 1){errors++;}
         if(A[0] != 0 || A[50] != 50){errors++;}
         if(A[50] != 50 || A[51] != 51){errors++;}
-        if(new_struct.s != 10){errors++;}
-        if(new_struct.S[0] != 10){errors++;}
+        if(new_struct.s == 10){errors++;}
+        if(new_struct.S[0] == 10){errors++;}
    }
 
   return errors;
