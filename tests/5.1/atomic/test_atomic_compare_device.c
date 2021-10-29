2b4988c908fd769f41be6e4496200234a80b70eb
Nolan Baker <61514629+nolanbaker31@users.noreply.github.com>
2021-10-29 10:10:54 -0400

    Atomic Compare Device (#399)
    * Created device version of atomic compare test
    
    * removed a semicolon
    
    * Update test_atomic_compare_device.c
    
    * Update test_atomic_compare_device.c
    
    Few syntax fixes.
    
    GCC gives error "error: expected 'read', 'write', 'update', 'capture', 'seq_cst', 'acq_rel', 'release', 'relaxed' or 'hint' clause" on line 35 for atomic compare.
    
    LLVM passes with 0 errors.
    
    * Update test_atomic_compare_device.c
    
    Style changes
    
    * Update test_atomic_compare_device.c
    
    adjusting header length (should be ~ 82 chars long)
    
    * Update test_atomic_compare_device.c
    
    based on @tob2 recommended changes.
    Tested w/ llvm/14.0.0-20210927 w/ 0 errors.
    
    Co-authored-by: Thomas Huber <thuber@udel.edu>

---
+++ tests/5.1/atomic/test_atomic_compare_device.c
@@ -0,0 +1,50 @@
+//===--- test_atomic_compare_device.c ---------------------------------------===//
+//
+// OpenMP API Version 5.1 Nov 2020
+//
+// Adapted from OpenMP example video https://www.youtube.com/watch?v=iS6IG7nzCSo
+// Creates an array with random numbers, and uses atomic compare on the device
+// environment to find the max, testing against non-parallel maximum.
+//
+////===----------------------------------------------------------------------===//
+
+#include <assert.h>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "ompvv.h"
+
+#define N 100
+
+int test_atomic_compare() {
+  OMPVV_INFOMSG("test_atomic_compare");
+
+  int arr[N];
+  int errors = 0;
+  int max = 0, pmax = 0;
+
+   for(int i = 0; i < N; i++){
+      arr[i] = rand()%1000;
+   }
+   for(int i = 0; i < N; i++){ // Sets max through non-parallel methods
+      if(arr[i] > max){
+         max = arr[i];
+      }
+   }
+   #pragma omp target parallel for map(pmax) shared(pmax)// Sets max using parallel for loop, using atomic to ensure max is correct
+   for(int i = 0; i < N; i++){
+      #pragma omp atomic compare
+      if(arr[i] > pmax){
+         pmax = arr[i];
+      }
+   }
+   OMPVV_TEST_AND_SET(errors, pmax != max);
+   return errors;
+}
+
+
+int main() {
+  int errors = 0;
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_atomic_compare());
+  OMPVV_REPORT_AND_RETURN(errors);
+}
