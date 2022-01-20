11e29815366cefd69fafb6cad5ffe3ba6050401d
Swaroop Pophale <pophaless@ornl.gov>
2022-01-19 19:35:16 -0500

    Adding test for allocate directive without clause

---
+++ tests/5.0/allocate/test_allocate.c
@@ -0,0 +1,47 @@
+//===----------------- test_allocate.c ------------------------------------===//
+//
+// OpenMP API Version 5.0 Nov 2018
+//
+// Tests the allocate directive. A variable 'x' is allocated using 
+// the omp_alloc call. If no clause is specified then the memory allocator s
+// pecified by the def-allocator-var ICV will be used. The tests 
+// checks that the values were written correctly, and then frees the memory.
+//
+//===----------------------------------------------------------------------===//
+
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "ompvv.h"
+
+#define N 1024
+
+int test_allocate() {
+  int errors = 0;
+  int* x;
+
+#pragma omp allocate(x) 
+  x = (int *) omp_alloc(N*sizeof(int), omp_null_allocator);
+
+#pragma omp parallel for 
+  for (int i = 0; i < N; i++) {
+    x[i] = i;
+  }
+
+  for (int i = 0; i < N; i++) {
+    OMPVV_TEST_AND_SET_VERBOSE(errors, x[i] != i);
+  }
+
+  omp_free(x,omp_default_mem_alloc);
+
+  return errors;
+}
+
+int main() {
+
+  int errors = 0;
+
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_allocate() != 0);
+
+  OMPVV_REPORT_AND_RETURN(errors);
+}
