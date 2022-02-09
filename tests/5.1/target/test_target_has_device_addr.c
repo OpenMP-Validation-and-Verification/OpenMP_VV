f3c03e99a5ba18f8b05cdd5c10963f4f929218bd
Nolan Baker <61514629+nolanbaker31@users.noreply.github.com>
2022-02-09 11:24:33 -0500

    Update tests/5.1/target/test_target_has_device_addr.c
    Co-authored-by: Tobias Burnus <tobias@codesourcery.com>

--- tests/5.1/target/test_target_has_device_addr.c
+++ tests/5.1/target/test_target_has_device_addr.c
@@ -28,16 +28,25 @@ int test_target_has_device_addr() {
   int *first_arr_device_addr;
   int *second_scalar_device_addr;
   int *second_arr_device_addr;
-  #pragma omp target map(from: first_scalar_device_addr, first_arr_device_addr)
+  #pragma omp target enter data map(to: x, arr)
+  #pragma omp target map(from: first_scalar_device_addr, first_arr_device_addr) map(to: x, arr)
   {
     first_scalar_device_addr = &x;
     first_arr_device_addr = &arr[0];
   }
-  #pragma omp target map(from:second_scalar_device_addr, second_arr_device_addr) has_device_addr(x, arr[N]) 
+  int *data_x, *data_arr;
+  #pragma omp target data use_device_addr(x,arr)
+  {
+  data_x = &x; data_arr = &arr[0];
+  #pragma omp target map(from:second_scalar_device_addr, second_arr_device_addr) has_device_addr(x, arr) 
   {
     second_scalar_device_addr = &x;
     second_arr_device_addr = &arr[0];
   }
+  }
+ #pragma omp target exit data map(release: x, arr)
+ OMPVV_TEST_AND_SET(errors, first_scalar_device_addr != data_x);
+ OMPVV_TEST_AND_SET(errors, first_arr_device_addr != data_arr);
   OMPVV_TEST_AND_SET(errors, first_scalar_device_addr != second_scalar_device_addr);
   OMPVV_TEST_AND_SET(errors, first_arr_device_addr != second_arr_device_addr)
   return errors;
