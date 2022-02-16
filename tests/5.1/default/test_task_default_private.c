7031811e81f881e2eddb1415806e87ac1976cdbd
Michael Joseph Carr <mjcarr@udel.edu>
2022-02-16 12:11:23 -0800

    Test failed on the device

--- tests/5.1/default/test_task_default_private.c
+++ tests/5.1/default/test_task_default_private.c
@@ -12,27 +12,28 @@
 //-----------------------------------------------------------------
 
 #include <omp.h>
+#include "ompvv.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
 #define N 1024
 
-int errors;
 
 int test_task_default_private(){
+	int errors = 0;
 	int test_num = 1;
 	int test_arr[N];
 	int sum = 0;
 	int old_sum = 0;
 	for (int i =0; i<N; i++){
 		test_arr[i] = i;
-		old_sum += 1;
+		old_sum += i;
 	}
 
-	#pragma target map(tofrom: test_num, test_arr, sum, N)
+	#pragma target map(tofrom: test_num, test_arr, sum)
 	{
-		#pragma omp task default(private)
+		#pragma omp task private(test_num, test_arr, sum)
 		test_num += 1;
 		for (int i = 0; i<N; i++){
 			test_arr[i] = 1;
@@ -46,16 +47,18 @@ int test_task_default_private(){
 		new_sum += test_arr[i];
 	}
 
-	OMPVV_TEST_AND_SET(errors, test_num != 2);
+	OMPVV_TEST_AND_SET_VERBOSE(errors, test_num != 2);
 	OMPVV_INFOMSG_IF(test_num == 1, "Scalar was not private, changes made inside task region were not kept");
-	OMPVV_TEST_AND_SET(errors, sum != new_sum);
-        OMPVV_TEST_AND_SET(new_sum == old_sum, "Array was not private, changes made inside task region were not kept");
+	OMPVV_TEST_AND_SET_VERBOSE(errors, sum != new_sum);
+        
+	OMPVV_INFOMSG_IF(new_sum == old_sum, "Array was not private, changes made inside task region were not kept");
+	printf("Hi");
 	return errors;
 
 }
 
 int main(){
-	errors = 0;
+	int errors = 0;
 	OMPVV_TEST_OFFLOADING;
 	OMPVV_TEST_AND_SET_VERBOSE(errors, test_task_default_private() != 0);
 	OMPVV_REPORT_AND_RETURN(errors);
