4899a6431e21ee1ff9b56d80a8c13bd6dbaef88d
Seyong Lee <lees2@ornl.gov>
2022-02-15 23:57:35 -0500

    Add test_target_map_with_close_modifier.F90

---
+++ tests/5.0/target/test_target_map_with_close_modifier.F90
@@ -0,0 +1,66 @@
+!===--- test_target_map_with_close_modifier.F90 -------------------------------===//
+!
+! OpenMP API Version 5.0 Nov 2018
+!
+! This test checks for support of the close map-type-modifier on a map clause. The test maps several
+! different data types to device with tofrom map-type and then checks for expected updated values on
+! the host.
+! 
+!//===----------------------------------------------------------------------===//
+#include "ompvv.F90"
+
+#define N 1024
+
+PROGRAM test_target_map_with_close_modifier
+  USE iso_fortran_env
+  USE ompvv_lib
+  USE omp_lib
+  implicit none
+  OMPVV_TEST_OFFLOADING
+
+  OMPVV_TEST_VERBOSE(test_close_modifier() .ne. 0)
+
+  OMPVV_REPORT_AND_RETURN()
+
+CONTAINS
+  INTEGER FUNCTION test_close_modifier()
+    INTEGER, DIMENSION(N) :: a
+    INTEGER :: errors, i, scalar
+
+    TYPE memberT
+    INTEGER :: var
+    INTEGER, DIMENSION(N) :: b
+    ENDTYPE memberT
+
+    TYPE(memberT) :: member
+
+    scalar = 19
+    member%var = 1
+
+    errors = 0
+
+    DO i = 1, N
+       a(i) = i
+       member%b(i) = i
+    END DO
+
+    !$omp target map(close,tofrom : scalar, a, member)
+    scalar = 25
+    member%var = 16
+    DO i = 1, N
+       a(i) = i * 2
+       member%b(i) = i * 2
+    END DO
+    !$omp end target
+
+    DO i = 1, N
+       OMPVV_TEST_AND_SET(errors, a(i) .ne. i * 2)
+       OMPVV_TEST_AND_SET(errors, member%b(i) .ne. i * 2)
+    END DO
+
+    OMPVV_TEST_AND_SET(errors, scalar .ne. 25)
+    OMPVV_TEST_AND_SET(errors, member%var .ne. 16)
+
+    test_close_modifier = errors
+  END FUNCTION test_close_modifier
+END PROGRAM test_target_map_with_close_modifier
