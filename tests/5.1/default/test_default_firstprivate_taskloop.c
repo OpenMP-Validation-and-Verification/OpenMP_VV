6c36853580dcf34bc801f98ebbc0d6eb4e5b33df
Kristina Holsapple <kris@udel.edu>
2022-02-16 04:50:19 -0800

    Remove target version

--- tests/5.1/default/test_default_firstprivate_taskloop.c
+++ tests/5.1/default/test_default_firstprivate_taskloop.c
@@ -26,24 +26,24 @@ int test_default_firstprivate_taskloop() {
   for (int i=0; i<N; i++){
         arr[i] = i;
         sum += arr[i];
- }
+	 }
 #pragma omp parallel num_threads(OMPVV_NUM_THREADS_HOST)
-  {
+    {	
 #pragma omp single 
-	  {	  
+	{  
 #pragma omp taskloop default(firstprivate)
   for (int i = 0; i < N; i++) {
 		arr[i] = i + 3;
 		scalar_var += 7;
- }
-  }
-  }
+ 		}	
+  	}	
+     }
   int new_sum =0;
   int wrong_sum =0;
   for (int i = 0; i < N; i++) {
 	new_sum += arr[i];
 	wrong_sum += i + 3;
-  }
+  	}
   OMPVV_TEST_AND_SET(errors, scalar_var != 6);
   OMPVV_INFOMSG_IF(scalar_var == 0, "Scalar was not initialized in parallel region & not updated");
   OMPVV_INFOMSG_IF(scalar_var >  6, "Scalar was not firstprivate, changes made in parallel affected original copy");
