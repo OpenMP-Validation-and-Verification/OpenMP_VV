c35ca8ed70e07c1b9403aaf75fdd23b5cc065f82
Jaydon Reap <jaydonreap@gmail.com>
2022-02-20 14:29:42 -0800

    Stipped down test to demonstrate bug

--- tests/5.1/teams/test_target_teams_thread_limit.c
+++ tests/5.1/teams/test_target_teams_thread_limit.c
@@ -1,66 +1,24 @@
-//===---------------- test_target_teams_thread_limit.c -----------------------===//
-//
-// OpenMP API Version 5.1 Nov 2020
-//
-// This test uses the thread_limit clause on the teams construct. Specifically
-// testing if a thread_limit from a above target construct properly carries
-// down to the nested teams construct, as if it were directly on the construct
-// as defined in the spec. The test validates that only the specified 
-// threads are created by summing a shared variable across all threads 
-// (and teams). If the threads are correctedly limited this should produce the 
-// expected value. Additional warnings are sent if specific issues occur.
-//
-////===----------------------------------------------------------------------===//
-
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "ompvv.h"
 
-#define N 1024
+/*
+ * Stripped down test to demostrate the teams thread limit bug (target or on host)
+ */
 
 int main() {
-	int errors = 0;
-	int shared = 0;
-	int num_teams = 0;
-
-	int testing_thread_limit = OMPVV_NUM_THREADS_DEVICE/OMPVV_NUM_TEAMS_DEVICE;
-
-	OMPVV_TEST_OFFLOADING;
-
-	#pragma omp target map(tofrom:num_teams,shared,errors) thread_limit(testing_thread_limit)
+	#pragma omp target
 	{
-		#pragma omp teams num_teams(OMPVV_NUM_TEAMS_DEVICE)
+		#pragma omp teams thread_limit(16) num_teams(8)
 		{
 
-			#pragma omp parallel num_threads(testing_thread_limit)
+			#pragma omp parallel num_threads(16)
 			{
-				if (omp_get_team_num() == 0) {
-					num_teams = omp_get_num_teams();
-				}
-
-				for (int i = 0; i < omp_get_num_teams(); i++) {
-					if (omp_get_team_num() == i) {
-						if (omp_get_num_threads() > testing_thread_limit) {
-							#pragma omp atomic
-							errors++;
-						}
-					}
-				}
-
-				for (int i = 0; i < omp_get_num_threads(); i++) {
-					#pragma omp atomic
-					shared++;
+				if (omp_get_thread_num() === 0) {
+					printf("Number Threads Detected: %d\n", omp_get_num_threads());
 				}
 			}
 		}
 	
 	}
-
-	OMPVV_WARNING_IF(num_teams != OMPVV_NUM_TEAMS_DEVICE, "The number of teams was unexpected, the test results are likely inconcuslive")
-	OMPVV_WARNING_IF(shared > (num_teams * testing_thread_limit), "The sum was higher than expected. This likely means thread_limit isn't capping the maximum threads created");
-	OMPVV_WARNING_IF(testing_thread_limit == 1, "Only one thread was allocated to each team, the test results are likely inconclusive");
-	OMPVV_TEST_AND_SET(errors, (shared != (num_teams * testing_thread_limit)));
-
-	OMPVV_REPORT_AND_RETURN(errors);
 }
