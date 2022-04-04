c32244dddd38c4395efdfff7737e9a8b42f7ea92
Jaydon Reap <jaydonreap@gmail.com>
2022-04-04 11:49:19 -0400

    Improved and removed unneeded parallel

--- tests/5.1/teams/test_target_teams_thread_limit.c
+++ tests/5.1/teams/test_target_teams_thread_limit.c
@@ -30,39 +30,31 @@ int main() {
 
 	#pragma omp target map(tofrom:num_teams,shared,errors) thread_limit(testing_thread_limit)
 	{
-		#pragma omp teams distribute num_teams(OMPVV_NUM_TEAMS_DEVICE)
+		#pragma omp teams num_teams(OMPVV_NUM_TEAMS_DEVICE)
 		{
-			#pragma omp parallel
-			{
 
-				if (omp_get_team_num() == 0 && omp_get_thread_num() == 0) {
-					num_teams = omp_get_num_teams();
-				}
+			if (omp_get_team_num() == 0 && omp_get_thread_num() == 0) {
+				num_teams = omp_get_num_teams();
+			}
 
-
-				if (omp_get_thread_num() == 0) {
-				
+			if (omp_get_thread_num() == 0) {
 				for (int i = 0; i < omp_get_num_teams(); i++) {
 					if (omp_get_team_num() == i) {
 						if (omp_get_num_threads() > testing_thread_limit) {
-							#pragma omp atomic
 							errors++;
 						}
 					}
 				}
 
-				}
-
 				for (int i = 0; i < omp_get_num_threads(); i++) {
-					#pragma omp atomic
 					shared++;
 				}
-
 			}
 
 		}
-	
+
 	}
+	
 
 	OMPVV_WARNING_IF(num_teams != OMPVV_NUM_TEAMS_DEVICE, "The number of teams was unexpected, the test results are likely inconcuslive")
 	OMPVV_WARNING_IF(shared > (num_teams * testing_thread_limit), "The sum was higher than expected. This likely means thread_limit isn't capping the maximum threads created");
