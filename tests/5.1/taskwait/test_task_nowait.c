2b2e52336a146e38abaf9af1e9027823b06a4584
Michael Joseph Carr <mjcarr@udel.edu>
2022-04-08 13:03:55 -0700

    finishing nowait test

--- tests/5.1/taskwait/test_task_nowait.c
+++ tests/5.1/taskwait/test_task_nowait.c
@@ -22,29 +22,29 @@ int test_task_nowait(){
 	int test_arr[N];
 	int sum = 0;
 	for (int i =0; i<N; i++){
-		test_arr[i] = i;
+		test_arr[i] = 1;
 		sum += i;
 	}
 	#pragma omp task private(test_scaler)
 	{
 		test_scaler += 1;
 	}
+	#pragma omp taskwait
 	#pragma omp task private(test_arr)
 	{
 		for (int i=0; i<N; i++){
 			test_arr[i] = i;
 		}
 	}
-	#pragma omp taskwait
 	int new_sum;
 	for (int i = 0; i < N; i++){
 		new_sum += test_arr[i];
 	}
-	OMPVV_TEST_AND_SET(errors, test_scaler != 2);
-	OMPVV_INFOMSG_IF(test_scaler == 1, "Scalar taskwait region was not ran");
+	OMPVV_TEST_AND_SET(errors, test_scaler != 1);
+	OMPVV_INFOMSG_IF(test_scaler == 2, "Scalar test was not private");
 	OMPVV_INFOMSG_IF(test_scaler > 2, "Scalar taskwait region was ran more than once");
-	OMPVV_TEST_AND_SET(errors, sum == new_sum);
-	OMPVV_INFOMSG_IF(sum == new_sum, "Array taskwait region was not ran");
+	OMPVV_TEST_AND_SET(errors, new_sum != 1024);
+	OMPVV_INFOMSG_IF(sum == new_sum, "Array taskwait region was not private");
 	return errors;
 
 }
