0b72e1251498d8d9c8c04c4e80c46939307c8cd4
Nolan Baker <61514629+nolanbaker31@users.noreply.github.com>
2022-04-28 13:40:36 -0400

    removed scalar variable, only using array

--- tests/5.1/target/test_target_memcpy_async_depobj.c
+++ tests/5.1/target/test_target_memcpy_async_depobj.c
@@ -22,14 +22,14 @@ int errors, i;
 
 int test_target_memcpy_async_depobj() {
 
-    int h, t, i, x;
+    int h, t, i;
     errors = 0;
     int *mem;
     int *mem_dev_cpy;
     h = omp_get_initial_device();
     t = omp_get_default_device();
 
-    OMPVV_TEST_AND_SET_VERBOSE(errors, omp_get_num_devices() < 1 || t < 0);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, omp_get_num_devices() < 1 || t < 0); //errors if device not available
 
     mem = (double *)malloc( sizeof(double)*N);
     mem_dev_cpy = (double *)omp_target_alloc( sizeof(double)*N, t);
@@ -38,23 +38,22 @@ int test_target_memcpy_async_depobj() {
 
     omp_depend_t obj;
 
-    #pragma omp depobj(obj) depend(inout: x)
-
-    /* dst src */
+    #pragma omp depobj(obj) depend(inout: mem_dev_cpy)
+    
+    /* copy to device memory */
     omp_target_memcpy_async(mem_dev_cpy, mem, sizeof(double)*N,
                                 0,          0,
                                 t,          h,
                                 1,          &obj);
 
     #pragma omp taskwait depend(depobj: obj)
-    #pragma omp target is_device_ptr(mem_dev_cpy) map(tofrom: x) device(t) depend(depobj: *obj)
+    #pragma omp target is_device_ptr(mem_dev_cpy) device(t) depend(depobj: obj)
     #pragma omp teams distribute parallel for
-    for(i = 0;i<N;i++){
+    for(i = 0; i < N; i++){
         mem_dev_cpy[i] = i*2; // initialize data
-        x = 5;
     }
-    OMPVV_TEST_AND_SET(errors, x!=5);
-    /* dst src */
+    
+    /* copy to host memory */
     omp_target_memcpy_async(mem, mem_dev_cpy, sizeof(double)*N,
                                 0,          0,
                                 h,          t,
@@ -64,8 +63,9 @@ int test_target_memcpy_async_depobj() {
     for(int i=0;i<N;i++){
         OMPVV_TEST_AND_SET(errors, mem_dev_cpy[i]!=i*2);
     }
+    // free resources
     omp_target_free(mem_dev_cpy, t);
-    #pragma omp depobj(obj) destroy // frees resource
+    #pragma omp depobj(obj) destroy 
     return errors;
 }
 
