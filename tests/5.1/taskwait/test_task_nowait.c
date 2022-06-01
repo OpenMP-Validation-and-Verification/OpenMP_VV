9639c047f6fca45e4074986971b1af02011b17e6
Michael Joseph Carr <mjcarr@udel.edu>
2022-05-31 17:24:50 -0700

    adding nowait depend clause

--- tests/5.1/taskwait/test_task_nowait.c
+++ tests/5.1/taskwait/test_task_nowait.c
@@ -29,7 +29,7 @@ int test_task_nowait(){
 	{
 		test_scaler += 1;
 	}
-	#pragma omp taskwait wait depend(out:test_scalar)
+	#pragma omp taskwait nowait depend(out:test_scalar)
 	#pragma omp task private(test_arr)
 	{
 		for (int i=0; i<N; i++){
