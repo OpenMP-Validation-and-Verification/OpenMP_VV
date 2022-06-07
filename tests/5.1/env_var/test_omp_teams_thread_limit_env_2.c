84f98637adea4e5014f27eabfa394d6b37ab8f7f
Michael Joseph Carr <mjcarr@udel.edu>
2022-06-07 12:43:26 -0700

    adding comment

--- tests/5.1/env_var/test_omp_teams_thread_limit_env_2.c
+++ tests/5.1/env_var/test_omp_teams_thread_limit_env_2.c
@@ -1,3 +1,17 @@
+//--------------- test_omp_team_thread_limit_env_2.c---------------------//
+//
+// OpenMP API Version 5.1 Aug 2021
+//
+// The objective of this test is to check that the team_thread_limit env
+// var is set properly. It is important to note that the name of the test
+// is very important to the env var being set. It follows a convention set
+// up for testing env variables in run.sh. This test itself simply checks
+// that the threadlimit is set to the appropriate variable value specified
+// in the test name. 
+//-----------------------------------------------------------------------//
+
+
+
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
