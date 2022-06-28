8a267154dc35257647c793f595180c301af4c15c
Michael Joseph Carr <mjcarr@udel.edu>
2022-06-28 07:29:38 -0700

    adapting scope test to test reduction

--- tests/5.1/scope/test_scope_construct.c
+++ tests/5.1/scope/test_scope_construct.c
@@ -18,10 +18,10 @@
 #define N 1024
 
 int test_scope(int n, int a[], int s){
-	#pragma omp parallel
+	int errors;
+	#pragma omp parallel firstprivate(s)
 	{
-		float loc_s = 0.0f;
-		static int nthrs;
+		int loc_s = 0;
 		#pragma omp for
 		{
 			for (int i = 0; i < n; i++)
@@ -29,26 +29,30 @@ int test_scope(int n, int a[], int s){
 		}
 		#pragma omp single
 		{
-			s += loc_s;
-			nthrs++;
+			s = 0;
 		}
-		#pragma omp scope reduction(+:s,nthrs)
+		#pragma omp scope reduction(+:s)
 		{
 			s += loc_s;
-			nthrs++;
 		}
 	}
-}
+	OMPVV_TEST_AND_SET_VERBOSE(errors, s != 1024);
+	OMPVV_INFOMSG_IF(s == 0, "sum was not set");
+	return errors;	
+}	
 
 int main(){
 	int a[N];
 	int s = 0;
+	int errors = 0;
 	for (int i; i < N; i++){
 		a[i] = 1;
 	}
+	OMPVV_TEST_OFFLOADING;
+	OMPVV_TEST_AND_SET_VERBOSE(errors, test_scope() != 0);
+	OMPVV_REPORT_AND_RETURN(errors);
 	test_scope(N,a,s);
 	return s;
-
 }
 
 
