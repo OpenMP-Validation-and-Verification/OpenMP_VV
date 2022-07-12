86614969c93fe91b90c7e1d2f3f965e184373103
Michael Joseph Carr <mjcarr@udel.edu>
2022-07-12 12:25:03 -0700

    updating test based on new comments

--- tests/5.1/target/test_target_is_accessible.c
+++ tests/5.1/target/test_target_is_accessible.c
@@ -17,18 +17,33 @@
 
 int check_device(){
 	int errors = 0;
+	int isSharedMemory = 0;
 	int check_test = 2;
 	const int N = 100;
 	const int buf_size = sizeof(int) * N;
 	const int dev = omp_get_default_device();
+	
+	/*Assumes taht one shared-memory systems, no copy is done*/
+	#pragma omp target map(to: isSharedMemory)
+		isSharedMemory = 1;
 
 	int *ptr = (int *) omp_target_malloc(buf_size,dev);
 
 	check_test = omp_target_is_accessible(ptr, buf_size, dev);
 	
-	omp_target_free(ptr, dev);
-	OMPVV_TEST_AND_SET_VERBOSE(errors, check_test != 0);
+	if(check_test)
+		{
+		#pragma omp target firstprivate(ptr)
+			for (int i=0; i<N; i++)
+				ptr[i] = 5*i
+		for (int i = 0; i < N; i++)
+			OMPVV_TEST_AND_SET(errors, ptr[i] != 5*i);
+		}
+
+	free(ptr);
+	OMPVV_TEST_AND_SET_VERBOSE(errors, check_test != isSharedMemory);
 	OMPVV_INFOMSG_IF(check_test == 1, "Omp_target_is_accessible is 1");
+	OMPVV_INFOMSG_IF(check_test == 0, "Omp_target_is_accessible is 0");
 	OMPVV_ERROR_IF(check_test == 2, "omp_target_is_accessible did not return true or false");
 	return errors;
 }
