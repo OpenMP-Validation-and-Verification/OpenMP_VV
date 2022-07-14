2cbeef4824ca899f3cde0df528aa9d8b7ce0bace
Swaroop Pophale <pophaless@ornl.gov>
2022-07-14 16:31:25 -0400

    Adding additional checks.

--- tests/5.1/requires/test_target_is_accessible_with_usm.c
+++ tests/5.1/requires/test_target_is_accessible_with_usm.c
@@ -2,7 +2,7 @@
 //
 // OpenMP API Version 5.1 Aug 2021
 //
-// This test checks that the omp_target_is_accessible device routine.
+// This test checks the omp_target_is_accessible device routine.
 // In this test the output of the target_is_accessible call should return
 // true because the requires unified shared memory is used.
 //-----------------------------------------------------------------------//
@@ -18,32 +18,51 @@
 #pragma omp requires unified_shared_memory
 
 int check_device(){
-	int errors = 0;
-	int check_test = -57;
-	const int buf_size = sizeof(int) * N;
-	const int dev = omp_get_default_device();
+  int errors = 0;
+  int check_test = -57;
+  const int buf_size = sizeof(int) * N;
+  const int dev = omp_get_default_device();
 
-	int *ptr = (int *) malloc(buf_size);
+  int *ptr = (int *) malloc(buf_size);
 
-	check_test = omp_target_is_accessible(ptr, buf_size, dev);
-	
-	free(ptr);
+  if(ptr == NULL){
+    OMPVV_INFOMSG("Memory allocation on host failed");
+    return 1;
+  }
+  for(int i = 0;i < N; i++)
+    ptr[i] = i;
 
-	OMPVV_TEST_AND_SET_VERBOSE(errors, check_test == 0);
+  check_test = omp_target_is_accessible(ptr, buf_size, dev);
 
-	OMPVV_ERROR_IF(check_test == -57, "Call to omp_target_is_accessible did not return true or false");
-	OMPVV_INFOMSG_IF((check_test != 0 && check_test != -57), "The host memory is accessible from the default device.");
+  OMPVV_TEST_AND_SET_VERBOSE(errors, check_test == 0);
 
-	return errors;
+  OMPVV_ERROR_IF(check_test == -57, "Call to omp_target_is_accessible did not return true or false");
+  OMPVV_INFOMSG_IF((check_test != 0 && check_test != -57), "The host memory is accessible from the default device.");
+
+  if(check_test){
+  #pragma omp target is_device_ptr(ptr) device(dev)
+  {
+    for(int i = 0;i < N; i++)
+      ptr[i] = ptr[i] * i;
+  }
+
+  for(int i = 0;i < N; i++)
+   OMPVV_ERROR_IF(ptr[i] != i * i, "Operations on the host memory that was reported as accessible produced incorrect results");
+
+}
+
+  free(ptr);
+
+  return errors;
 }
 
 int main(){
-	int errors = 0;
+int errors = 0;
 
-	OMPVV_TEST_OFFLOADING;
+OMPVV_TEST_OFFLOADING;
 
-	OMPVV_TEST_AND_SET_VERBOSE(errors, check_device() != 0);
+OMPVV_TEST_AND_SET_VERBOSE(errors, check_device() != 0);
 
-	OMPVV_REPORT_AND_RETURN(errors);
+OMPVV_REPORT_AND_RETURN(errors);
 	
 }
