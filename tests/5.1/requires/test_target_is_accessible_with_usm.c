405e2a9d66157db3f9f3ac4516fac35c4b67ec39
Swaroop Pophale <swaroop.pophale23@gmail.com>
2022-07-19 14:24:09 -0400

    Update test_target_is_accessible_with_usm.c
    Changing initial value of test_check.

--- tests/5.1/requires/test_target_is_accessible_with_usm.c
+++ tests/5.1/requires/test_target_is_accessible_with_usm.c
@@ -19,7 +19,7 @@
 
 int check_device(){
   int errors = 0;
-  int check_test = -57;
+  int check_test = 0;
   const int buf_size = sizeof(int) * N;
   const int dev = omp_get_default_device();
 
@@ -46,9 +46,11 @@ int check_device(){
   }
 
   for(int i = 0;i < N; i++)
-   OMPVV_ERROR_IF(ptr[i] != i * i, "Operations on the host memory that was reported as accessible produced incorrect results");
+   OMPVV_ERROR_IF(ptr[i] != i * i, "Operations on the host memory that was reported as accessible produced incorrect results.");
 
-}
+  }
+  else
+  OMPVV_ERROR ("Host memory was reported inaccessible despite of 'requires unified_shared_memory' specified.");
 
   free(ptr);
 
