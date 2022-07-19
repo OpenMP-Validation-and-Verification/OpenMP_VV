5f2b506944b1f11c392f29905553c8596284215c
Michael Joseph Carr <mjcarr@udel.edu>
2022-07-19 12:44:29 -0700

    Scope nowait test

---
+++ tests/5.1/scope/test_scope_nowait_construct.c
@@ -0,0 +1,43 @@
+//-------------test_scope_nowait_construct.c-------------//
+//
+//OpenMP API Version 5.1 Aug 2021
+//
+//Tests the behavior of the scope construct with nowait
+//specified. This test should remove the implied barrier
+//creatd by the scope construct when nowait is specified.
+//-------------------------------------------------------//
+
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "ompvv.h"
+#include <math.h>
+#include <unistd.h>
+
+#define N 1024
+
+int test_scope_nowait(){
+	int errors = 0;
+	int test_scalar = 1;
+	for (int i=0; i<N; i++){
+		test_arr[i] = 1;
+		sum += 1;
+	}
+	#pragma omp task shared(test_int)
+	{
+		#pragma omp scope privatee(test_int) nowait
+		{
+			test_int += 1;
+		}
+	}
+	OMPVV_TEST_AND_SET_VERBOSE(errors, test_int != 1);
+        OMPVV_INFOMSG_IF(test_int == 2, "test was not private");
+	return errors;
+}
+
+int main(){
+	int errors = 0;
+	OMPVV_TEST_OFFLOADING;
+	OMPVV_TEST_AND_SET_VERBOSE(errors, test_scope_nowait() != 0);
+	OMPVV_REPORT_AND_RETURN(errors);
+}
