f1aa40eb854da441d055f23f06f8ce9f66b9cc2a
Nolan Baker <nolanb@udel.edu>
2022-08-17 12:54:33 -0400

    created test

---
+++ tests/5.1/dispatch/test_dispatch_depend.c
@@ -0,0 +1,67 @@
+//===---- test_dispatch_depend.c -----------------------------------------===//
+// 
+// OpenMP API Version 5.1
+//
+// Uses dispatch construct as context for variant directive. Uses depend clause
+// which adds depend property to the interoperability requirement set.
+//
+// Inspired by "OpenMP 5.1 Features: The Dispatch Construct" video:
+// https://www.youtube.com/watch?v=ruugaX95gIs
+// 
+//===-------------------------------------------------------------------------------===//
+
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "ompvv.h"
+
+#define N 1024
+
+int arr[N]; // implicit map array 
+int errors;
+int i = 0;
+int a;
+
+void add_two(int *arr);
+
+#pragma omp declare variant(add_two) match(construct={dispatch})
+void add(int *arr){
+    for (int i = 0; i < N; i++){ // Base function adds 1 to array values
+        arr[i] = i+1;
+    }
+    #pragma omp task depend(out: a)
+        a = 3;
+}
+
+void add_two(int *arr){
+    OMPVV_TEST_AND_SET_VERBOSE(errors, a != 3);
+    OMPVV_ERROR_IF(errors > 0, "Depend clause on dispatch directive not working properly");
+    for (int i = 0; i < N; i++){
+        arr[i] = i+2; // Variant function adds 2 to array values
+    }
+}
+
+int test_wrapper() { 
+    errors = 0;
+    add(arr);
+    for(i = 0; i < N; i++){
+        OMPVV_TEST_AND_SET_VERBOSE(errors, arr[i] != i+1);
+    } 
+    OMPVV_ERROR_IF(errors > 0, "Base function is not working properly");
+
+    #pragma omp dispatch depend(in: a)
+        add(arr);
+
+    for(i = 0; i < N; i++){
+        OMPVV_TEST_AND_SET_VERBOSE(errors, arr[i] != i+2);
+    }
+    OMPVV_ERROR_IF(errors > 0, "Dispatch w/ depend is not working properly");
+    return errors;
+}
+
+int main () {
+    OMPVV_TEST_OFFLOADING;
+    OMPVV_TEST_AND_SET_VERBOSE(errors, test_wrapper());
+    OMPVV_REPORT_AND_RETURN(errors);
+}  
