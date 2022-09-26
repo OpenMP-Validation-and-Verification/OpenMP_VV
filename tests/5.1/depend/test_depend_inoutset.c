9e6243c1049eb7bd935c6a96bd767dca9107eb84
Nolan Baker <nolanb@udel.edu>
2022-09-26 11:05:53 -0400

    Testing using arrays

--- tests/5.1/depend/test_depend_inoutset.c
+++ tests/5.1/depend/test_depend_inoutset.c
@@ -13,32 +13,36 @@
 #include "ompvv.h"
 #include <math.h>
 
-int errors;
+#define N 1024
 
+int errors;
+int arr[N];
 int depend_inoutset(){
-    #pragma omp parallel
-#pragma omp single
+  #pragma omp parallel
+  #pragma omp single
   {
-#pragma omp task depend(out: c)
-   c = 1; /* Task T1 */
- #pragma omp task depend(out: a)
-   a = 2; /* Task T2 */
- #pragma omp task depend(out: b)
-   b = 3; /* Task T3 */
- #pragma omp task depend(in: a) depend(inoutset: c)
-   c += a; /* Task T4 */
- #pragma omp task depend(in: b) depend(inoutset: c)
-   c += b; /* Task T5 */
- #pragma omp task depend(in: c)
-   d = c; /* Task T6 */
+    for(int i = 0; i < N; i++){
+      #pragma omp task depend(out: arr[i])
+      arr[i] = i + 1;
+    }
+    for(int i = 0; i < N; i++){
+      #pragma omp task depend(inoutset: arr[i])
+      arr[i] = arr[i] + 2;
+    }
+    for(int i = 0; i < N; i++){
+      #pragma omp task depend(inoutset: arr[i])
+      arr[i] = arr[i] + 3;
+    }
   }
-  OMPVV_TEST_AND_SET_VERBOSE(errors, d != 6);
+  for(int i = 0; i < N; i++){
+    OMPVV_TEST_AND_SET_VERBOSE(errors, arr[i] != i + 6);
+  }
   return errors;
 }
 
 int main() {
    errors = 0;
    OMPVV_TEST_OFFLOADING;
-   OMPVV_TEST_AND_SET_VERBOSE(errors, test_depend_intouset() != 0);
+   OMPVV_TEST_AND_SET_VERBOSE(errors, depend_inoutset() != 0);
    OMPVV_REPORT_AND_RETURN(errors);
 }
