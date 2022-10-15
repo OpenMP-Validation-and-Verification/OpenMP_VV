c23fb7f72354d177b5a5ae944434783106f7f46a
Jaydon Reap <jaydonreap@gmail.com>
2022-10-15 18:30:06 -0400

    Tweaked some errors and fixed spacing

--- tests/5.1/depend/test_target_depend_out_omp_all_memory.c
+++ tests/5.1/depend/test_target_depend_out_omp_all_memory.c
@@ -27,23 +27,24 @@ int main() {
 
 	OMPVV_TEST_OFFLOADING;
 
-	#pragma omp target map(tofrom: x, y, errors)
+	#pragma omp target map(tofrom: errors) map(to: x, y)
 	{
 		#pragma omp parallel
     #pragma omp single
     {
-      #pragma omp task shared(x, y) depend(out: x, y)
+      #pragma omp task shared(x, y) depend(out: omp_all_memory)
       {
         for(int i = 0; i < N; i++) {
           x[i] += y;   
         }
       }
-      #pragma omp task shared(x, y, errors) depend(out: omp_all_memory)
+      #pragma omp task shared(x, y, errors) depend(in: x, y)
       {
-        for (int i 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
           OMPVV_TEST_AND_SET(errors, x[i] != i + y);
         }
       }
+      #pragma omp taskwait
     }
 	}
 
