8150a04e63c3b1eb672c52631bc3f1f04123c563
Jaydon Reap <jaydonreap@gmail.com>
2022-10-15 18:11:58 -0400

    Added initial description and file

---
+++ tests/5.1/depend/test_target_depend_out_omp_all_memory.c
@@ -0,0 +1,42 @@
+//===------------ test_target_depend_out_omp_all_memory.c --------------------===//
+//
+// OpenMP API Version 5.1 Nov 2020
+//
+// This test checks that the special omp_all_memory locator keyword works
+// as intended. The test checks this by having two variables passed into the 
+// target parallel region, depending on both values. If both variables are
+// properly passed through the sections then it passes. If not, the test fails.
+//
+////===----------------------------------------------------------------------===//
+
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "ompvv.h"
+
+#define N 1024
+
+int main() {
+	int errors = 0;
+	int x[N];
+
+	for (int i = 0; i < N; i++) {
+		x[i] = i;
+	}
+
+	OMPVV_TEST_OFFLOADING;
+
+	#pragma omp target map(tofrom: x)
+	{
+		#pragma omp parallel for order(unconstrained:concurrent)
+		for (int i = 0; i < N; i++) {
+			x[i] = x[i] + 2;	
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+    		OMPVV_TEST_AND_SET(errors, x[i] != i + 2);
+	}
+
+	OMPVV_REPORT_AND_RETURN(errors);
+}
