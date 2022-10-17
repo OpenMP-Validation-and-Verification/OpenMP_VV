3e614a95dca480a27193d721bf43bf58d0bfb3fd
Seyong Lee <lees2@ornl.gov>
2022-10-17 12:33:36 -0400

    Add new_test/test_requires_unified_shared_memory_stack_is_device_ptr.F90 (#581)
    * Add new_test/test_requires_unified_shared_memory_stack_is_device_ptr.F90
            - C test passed with NVHPC 22.5
            - C test passed with LLVM 15.0.0-latest
            - C test failed with GCC 11.2.0
            - C test passed with XL 16.1.1-10 but on the host
            - Fortran test passed with NVHPC 22.5
            - Fortran test failed with GCC 11.2.0
            - Fortran test failed with XL 16.1.1-10
    
    * - Update the Fortran test so that dummy argument is used in the is_device_ptr clause.
    - NVHPC 22.5: Both C and Fortran tests passed.
    - LLVM 15.0.0: C test passed.
    - GCC 12.1.1: C test passed but Fortran test failed (Segmentation fault - invalid memory reference.).
    - XL 16.1.1-10: C test passed but ran on host. Fortran test failed.

---
+++ tests/5.0/requires/test_requires_unified_shared_memory_stack_is_device_ptr.F90
@@ -0,0 +1,89 @@
+!===--- test_requires_unified_shared_memory_stack_is_device_ptr.F90 --------------------------===//
+!
+! OpenMP API Version 5.0 Nov 2018
+!
+! Testing memory access from host and device to a variable allocated in the stack
+! The variable is accessed through a pointer to guarantee that there is no default
+! mapping tofrom: of the stack array
+!
+! We use the is_device_ptr to guarantee using the host pointer is used in the device
+!
+!===------------------------------------------------------------------------------===//
+
+#define OMPVV_MODULE_REQUIRES_LINE !$omp requires unified_shared_memory
+#include "ompvv.F90"
+
+#define N 1024
+
+PROGRAM test_requires_unified_shared_memory_stack_is_device_ptr
+   USE iso_fortran_env
+   USE ompvv_lib
+   USE omp_lib
+   implicit none
+   LOGICAL:: isOffloading
+
+!$omp requires unified_shared_memory
+
+  OMPVV_TEST_AND_SET_OFFLOADING(isOffloading)
+
+  OMPVV_WARNING_IF(.NOT. isOffloading, "With no offloading, unified shared memory is guaranteed due to host execution")
+
+  OMPVV_TEST_VERBOSE(unified_shared_memory_stack_is_device_ptr() .NE. 0)
+
+  OMPVV_REPORT_AND_RETURN()
+
+CONTAINS
+  SUBROUTINE test_is_device_ptr(anArrayDummy, anArrayCopyDummy)
+    INTEGER, DIMENSION(N):: anArrayDummy
+    INTEGER, DIMENSION(N):: anArrayCopyDummy
+    INTEGER:: i
+
+    ! Modify in the device
+    !$omp target is_device_ptr(anArrayDummy)
+    DO i = 1, N
+      anArrayDummy(i) = anArrayDummy(i) + 10
+    END DO
+    !$omp end target
+
+    ! Modify again on the host
+    DO i = 1, N
+      anArrayDummy(i) = anArrayDummy(i) + 10
+    END DO
+
+    ! Get the value the device is seeing
+    !$omp target is_device_ptr(anArrayDummy)
+    DO i = 1, N
+      anArrayCopyDummy(i) = anArrayDummy(i)
+    END DO
+    !$omp end target
+  END SUBROUTINE test_is_device_ptr
+
+  INTEGER FUNCTION unified_shared_memory_stack_is_device_ptr()
+    INTEGER:: errors, i
+    INTEGER, TARGET, DIMENSION(N):: anArray
+    INTEGER, DIMENSION(N):: anArrayCopy
+
+    OMPVV_INFOMSG("Unified shared memory testing - Array on stack")
+
+    errors = 0
+
+    DO i = 1, N
+      anArray(i) = i
+      anArrayCopy(i) = 0
+    END DO
+
+    CALL test_is_device_ptr(anArray, anArrayCopy)
+
+    DO i = 1, N
+      OMPVV_TEST_AND_SET_VERBOSE(errors, anArray(i) .NE. (i + 20))
+      OMPVV_TEST_AND_SET_VERBOSE(errors, anArrayCopy(i) .NE. (i + 20))
+      IF (errors .NE. 0) THEN
+        exit
+      END IF
+    END DO
+    
+    unified_shared_memory_stack_is_device_ptr = errors
+  END FUNCTION unified_shared_memory_stack_is_device_ptr
+END PROGRAM test_requires_unified_shared_memory_stack_is_device_ptr
+      
+   
