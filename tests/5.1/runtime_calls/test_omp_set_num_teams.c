61c784bff4821eb562c2374a29d5830cfdd53002
fel-cab <cabarcas@udel.edu>
2022-11-04 10:47:14 -0400

    Changed text in headder. Added test for num_teams[0] > OMPVV_NUM_TEAMS_DEVICE, fixed problem with errors variable in main. OMPVV_TEST_OFFLOADING is now working on main

--- tests/5.1/runtime_calls/test_omp_set_num_teams.c
+++ tests/5.1/runtime_calls/test_omp_set_num_teams.c
@@ -2,9 +2,6 @@
 //
 // OpenMP API Version 5.1 Aug 2021
 //
-// Section 3.4.3 from
-// https://www.openmp.org/wp-content/uploads/OpenMP-API-Specification-5-1.pdf
-//
 // Test for omp_set_num_teams. This routine sets number of threads to be used
 // in teams regions that do not specify a num_teams clause
 // based on the 5.0/teams/test_teams.c test
@@ -36,6 +33,10 @@ int test_omp_set_num_teams()
     OMPVV_WARNING("Test operated with 1 team. num_teams requested were %d.", OMPVV_NUM_TEAMS_DEVICE);
   } else if (num_teams[0] < 1) {
     OMPVV_ERROR("omp_get_num_teams() reported a value less than one.");
+    errors++;
+  } else if (num_teams[0] > OMPVV_NUM_TEAMS_DEVICE) {
+    OMPVV_ERROR("omp_get_num_teams() reported a value greater than set.");
+    errors++;
   }
 
   for (int x = 1; x < num_teams[0]; ++x) {
@@ -46,18 +47,18 @@ int test_omp_set_num_teams()
 
   OMPVV_INFOMSG_IF(!(errors), "Test passed with %d teams.", num_teams[0]);
 
-  OMPVV_REPORT_AND_RETURN(errors);
+  return errors;
 }
 
 
 int main() {
 
-  //OMPVV_TEST_OFFLOADING;
+  OMPVV_TEST_OFFLOADING;
 
   int errors = 0;
-  //OMPVV_TEST_AND_SET_VERBOSE(errors, test_omp_set_num_teams() != 0);
-  test_omp_set_num_teams();
 
-  //OMPVV_REPORT_AND_RETURN(errors);
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_omp_set_num_teams());
+
+  OMPVV_REPORT_AND_RETURN(errors);
 }
 
