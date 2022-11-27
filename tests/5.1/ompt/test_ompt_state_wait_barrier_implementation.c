34931de49731cc5cd6d7858871bdc00f3bde7a66
Michael Joseph Carr <mjcarr@udel.edu>
2022-11-27 14:04:45 -0800

    in progress test update

--- tests/5.1/ompt/test_ompt_state_wait_barrier_implementation.c
+++ tests/5.1/ompt/test_ompt_state_wait_barrier_implementation.c
@@ -14,13 +14,27 @@
 #include <stdlib.h>
 #include <omp-tools.h>
 #include "ompvv.h"
+#include <unistd.h>
 
 int test_case(){
+	int error_check = 0;
 	int errors = 0;
 	int test_val = 0;
-	test_val = ompt_state_wait_barrier_implementation;
-	OMPVV_TEST_AND_SET_VERBOSE(errors, test_val != 21);
-	OMPVV_INFOMSG_IF(test_val == 0, "local test variable was not overwritten");
+	int enum_num = 21;
+	//test_val = ompt_state_wait_barrier_implementation;
+	//OMPVV_TEST_AND_SET_VERBOSE(errors, test_val != 21);
+	//OMPVV_INFOMSG_IF(test_val == 0, "local test variable was not overwritten");
+
+	#pragma omp teams shared(enum_num, error_check)
+	{
+		int state_val = ompt_get_state();
+		if (state_val != enum_num){
+			errors += 1;
+		}
+		usleep(10);
+	}
+	OMPVV_TEST_AND_SET_VERBOSE(errors, error_check != 0);
+	OMPVV_INFOMSG_IF(error_check != 0, "a thread state was not teams wait");
 	return errors;
 }
 
