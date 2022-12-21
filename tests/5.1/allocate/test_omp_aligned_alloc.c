a6b43d58dcac23b339e7b2b128e312b2039a0554
Jaydon Reap <jaydonreap@gmail.com>
2022-12-21 15:53:53 -0500

    Added initial code framework and updated desc

--- tests/5.1/allocate/test_omp_aligned_alloc.c
+++ tests/5.1/allocate/test_omp_aligned_alloc.c
@@ -2,12 +2,10 @@
 //
 // OpenMP API Version 5.1 Nov 2020
 //
-// Tests the allocate directive with allocator and align clause.
-// The declarative allocator statement uses the omp_default_mem_alloc handle
-// for default memory allocation for "x", aligned to 64-byte alignment via the 
-// align clause. Parallel region checks that 64-byte alignment is correct 
-// and that the memory can be written to in and the values
-// were written correctly, and then frees the memory.
+// Tests the omp_aligned_alloc routine properly requests a memory allocation
+// aligned to the passed byte parameter. Starts by initializing some memory
+// in the omp_default_mem_space and then checks if this memory is aligned
+// and can be properly written to and read from.
 //
 //===----------------------------------------------------------------------===//
 
@@ -17,5 +15,33 @@
 #include "ompvv.h"
 
 #define N 1024
+
+int test_omp_aligned_alloc() {
+  int errors = 0;
+  float *x, *y;
+
+
+  #pragma omp target map(from:x[:N])
+  {
+    #pragma omp parallel for simd simdlen(16) aligned(x: 64)
+    for (int i = 0; i < N; i++) {
+      x[i] = i;
+    }
+  }
+
+  for (int i = 0; i < N; i++) {
+    OMPVV_TEST_AND_SET_VERBOSE(errors, x[i] != i);
+  }
+
+  return errors;
+}
+
+int main() {
+  OMPVV_TEST_OFFLOADING;
+  int errors = 0;
+
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_omp_aligned_alloc() != 0);
+  OMPVV_REPORT_AND_RETURN(errors);
+}
 
 
