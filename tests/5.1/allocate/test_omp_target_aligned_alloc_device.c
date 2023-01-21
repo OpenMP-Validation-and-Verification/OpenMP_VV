ffe53f22d9ca9bf8f6e316aa1a8ed2529248e00d
Jaydon Reap <jaydonreap@gmail.com>
2023-01-20 20:14:00 -0500

    Updated documentation and changed function names

--- tests/5.1/allocate/test_omp_target_aligned_alloc_device.c
+++ tests/5.1/allocate/test_omp_target_aligned_alloc_device.c
@@ -1,11 +1,11 @@
-//===------ test_omp_aligned_alloc.c ------------------------------------===//
+//===------ test_omp_target_aligned_alloc_device.c ------------------------------------===//
 //
 // OpenMP API Version 5.1 Nov 2020
 //
 // Tests the omp_aligned_alloc routine properly requests a memory allocation
 // aligned to the passed byte parameter. Starts by initializing some memory
 // in the omp_default_mem_space and then checks if this memory is aligned
-// and can be properly written to and read from.
+// and can be properly written to and read from. Checks on device.
 //
 //===----------------------------------------------------------------------===//
 
@@ -17,34 +17,38 @@
 
 #define N 1024
 
-int test_omp_aligned_alloc_on_host() {
+int test_omp_aligned_alloc_on_device() {
   int errors = 0;
-  int *x, *y;
+  
+  #pragma omp target map(tofrom: errors) 
+  {
+    int *x, *y;
 
-  omp_memspace_handle_t  memspace = omp_default_mem_space;
-  omp_alloctrait_t       traits[1] = {{omp_atk_alignment, 64}};
-  omp_allocator_handle_t alloc = omp_init_allocator(memspace,1,traits);
+    omp_memspace_handle_t  memspace = omp_default_mem_space;
+    omp_alloctrait_t       traits[1] = {{omp_atk_alignment, 64}};
+    omp_allocator_handle_t alloc = omp_init_allocator(memspace,1,traits);
 
-  x = (int *)omp_aligned_alloc(64, N*sizeof(int), alloc);
-  y = (int *)omp_aligned_alloc(64, N*sizeof(int), alloc);
+    x = (int *)omp_aligned_alloc(64, N*sizeof(int), alloc);
+    y = (int *)omp_aligned_alloc(64, N*sizeof(int), alloc);
     
-  OMPVV_TEST_AND_SET_VERBOSE(errors, ((intptr_t)(y))%64 != 0 || ((intptr_t)(x))%64 != 0);
+    OMPVV_TEST_AND_SET_VERBOSE(errors, ((intptr_t)(y))%64 != 0 || ((intptr_t)(x))%64 != 0);
 
-  #pragma omp parallel for simd simdlen(16) aligned(x,y: 64)
-  for (int i = 0; i < N; i++) {
-    x[i] = i;
-    y[i] = i+1;
+    #pragma omp parallel for simd simdlen(16) aligned(x,y: 64)
+    for (int i = 0; i < N; i++) {
+      x[i] = i;
+      y[i] = i+1;
+    }
+
+    #pragma omp parallel for simd simdlen(16) aligned(x,y: 64)
+    for (int i = 0; i < N; i++) {
+      OMPVV_TEST_AND_SET_VERBOSE(errors, x[i] != i);
+      OMPVV_TEST_AND_SET_VERBOSE(errors, y[i] != i+1);
+    }
+
+    omp_free(x, alloc);
+    omp_free(y, alloc);
+    omp_destroy_allocator(alloc);
   }
-
-  #pragma omp parallel for simd simdlen(16) aligned(x,y: 64)
-  for (int i = 0; i < N; i++) {
-    OMPVV_TEST_AND_SET_VERBOSE(errors, x[i] != i);
-    OMPVV_TEST_AND_SET_VERBOSE(errors, y[i] != i+1);
-  }
-
-  omp_free(x, alloc);
-  omp_free(y, alloc);
-  omp_destroy_allocator(alloc);
 
   return errors;
 }
@@ -53,7 +57,7 @@ int main() {
   OMPVV_TEST_OFFLOADING;
   int errors = 0;
 
-  OMPVV_TEST_AND_SET_VERBOSE(errors, test_omp_aligned_alloc_on_host() != 0);
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_omp_aligned_alloc_on_device() != 0);
   OMPVV_REPORT_AND_RETURN(errors);
 }
 
