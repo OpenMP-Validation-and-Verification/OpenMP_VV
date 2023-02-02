c5b28e96165a890ba79d91f3dc396d1e2fd78a7e
Nolan Baker <61514629+nolanbaker31@users.noreply.github.com>
2023-02-02 14:11:00 -0500

    Fix warnings on nested_declare_target test (#663)
    * updated test
    
    * fix newline error

--- tests/5.0/declare_target/test_nested_declare_target.c
+++ tests/5.0/declare_target/test_nested_declare_target.c
@@ -1,11 +1,11 @@
 //===---- test_nested_declare_target.c -------------------------------------===//
-// 
-// OpenMP API Version 5.0 
+//
+// OpenMP API Version 5.0
 //
 // The declare target directive specifies that variables, functions(C,C++ and Fortran),
-// and subroutines (Fortran) are mapped to a device. The declaration-definition-seq 
-// defined by a declare target directive and an end declare target directive may contain 
-// declare target directives. 
+// and subroutines (Fortran) are mapped to a device. The declaration-definition-seq
+// defined by a declare target directive and an end declare target directive may contain
+// declare target directives.
 //===-------------------------------------------------------------------------------===//
 
 #include <omp.h>
@@ -14,54 +14,56 @@
 #include "ompvv.h"
 
 #define N 1024
+
 int errors = 0;
-int a[N], b[N], c[N];  
-int i = 0;
+int a[N], b[N], c[N];
 
+#pragma omp declare target
+#pragma omp declare target link(a,b,c)
+#pragma omp end declare target
 
-void update() { 
-  for (i = 0; i < N; i++) {
+void update() {
+  for (int i = 0; i < N; i++) {
     a[i] += 1;
     b[i] += 2;
     c[i] += 3;
   }
 }
 
-#pragma omp declare target 
-#pragma omp declare target link(a,b,c,i)  
-#pragma omp declare target to(update)  
+#pragma omp declare target
+#pragma omp declare target to(update)
 #pragma omp end declare target
 
-int test_declare_target_device_type_any() { 
-  
-  #pragma omp target map(to: i) map(tofrom: a, b, c) 
+int test_declare_target_device_type_any() {
+
+  #pragma omp target map(tofrom: a, b, c)
   {
     update();
   }
-  
 
-  for (i = 0; i < N; i++) { //check array values on host
+
+  for (int i = 0; i < N; i++) { //check array values on host
     if ( a[i] != i + 1 || b[i] != i + 3 || c[i] != i + 5) {
-      errors++;  
-    } 
+      errors++;
+    }
   }
 
   //on host
   update();
 
-  for (i = 0; i < N; i++) { //check array values on host
+  for (int i = 0; i < N; i++) { //check array values on host
     if ( a[i] != i + 2 || b[i] != i + 5 || c[i] != i + 8) {
       errors++;
     }
   }
-  
+
   return errors;
 }
 
 int main () {
-  
+
   //initalize arrays on host
-  for (i = 0; i < N; i++) {
+  for (int i = 0; i < N; i++) {
     a[i] = i;
     b[i] = i + 1;
     c[i] = i + 2;
@@ -70,4 +72,4 @@ int main () {
   OMPVV_TEST_AND_SET_VERBOSE(errors, test_declare_target_device_type_any());
 
   OMPVV_REPORT_AND_RETURN(errors);
-}  
+}
