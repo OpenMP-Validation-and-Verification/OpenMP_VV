2a44f4b9c2b19e8904bda80f611e141c71611aa8
Seyong Lee <lees2@ornl.gov>
2023-02-16 13:51:19 -0500

    Add new_test/test_requires_atomic_default_mem_order_acq_rel.F90 (#677)
    * Add new_test/test_requires_atomic_default_mem_order_acq_rel.F90
    
    * Update test_requires_atomic_default_mem_order_acq_rel.F90
    
    Setting dynamic readjustment of threads to false.
    
    * Update test_requires_atomic_default_mem_order_acq_rel.c
    
    Setting dynamic adjustment to false.
    
    ---------
    
    Co-authored-by: Swaroop Pophale <swaroop.pophale23@gmail.com>

--- tests/5.0/requires/test_requires_atomic_default_mem_order_acq_rel.c
+++ tests/5.0/requires/test_requires_atomic_default_mem_order_acq_rel.c
@@ -15,8 +15,6 @@
 #include <stdlib.h>
 #include "ompvv.h"
 
-#define N 1024
-
 #pragma omp requires atomic_default_mem_order(acq_rel)
 
 int test_requires_atomic_acq_rel() {
@@ -24,8 +22,11 @@ int test_requires_atomic_acq_rel() {
 
   int x = 0, y = 0;
   int errors = 0;
+  
+  omp_set_dynamic(0);
+  omp_set_num_threads(2);
 
-#pragma omp parallel num_threads(2)
+#pragma omp parallel
    {
       int thrd = omp_get_thread_num();
        if (thrd == 0) {
