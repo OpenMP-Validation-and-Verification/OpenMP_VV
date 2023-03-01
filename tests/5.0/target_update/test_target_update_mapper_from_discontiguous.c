ba131e321d8c704611fa13242b1311a84c5240ad
Swaroop Pophale <swaroop.pophale23@gmail.com>
2023-03-01 10:54:51 -0500

    Update test_target_update_mapper_from_discontiguous.c
    Test now uses update with stride.

--- tests/5.0/target_update/test_target_update_mapper_from_discontiguous.c
+++ tests/5.0/target_update/test_target_update_mapper_from_discontiguous.c
@@ -7,7 +7,6 @@
 // checks a new addition to target update in OpenMP 5.0 that states "List items in the
 // to or from clauses may include array sections with stride expressions." 
 //
-// Adopted from OpenMP 5.0 Example target_mapper.1.c
 //===------------------------------------------------------------------------===//
 
 #include <omp.h>
@@ -17,40 +16,58 @@
 
 #define N 1024
 
-typedef struct newvec {
+typedef struct{
   size_t len;
   double *data;
-} newvec_t;
+} T;
 
-size_t i;
+int i;
 int errors = 0;
 
-#pragma omp declare mapper(newvec_t v) map(v, v.data[0:v.len])
+#pragma omp declare mapper(S: T v) map(v, v.len, v.data[0:v.len])
 
 int target_update_from_mapper() {
   
-  newvec_t s; 
-
-  s.data = (double *)calloc(N,sizeof(double));
+  T s;
+  
   s.len = N;
-  for (i = 0; i< s.len; i++){ //initalize the struct
-    s.data[i] = 0;
-  }
-  #pragma omp target
-  {
-    for (i = 0; i < s.len; i += 2) {
+  s.data = (double *)calloc(N,sizeof(double));
+  
+  
+  #pragma omp target  
+  { 
+    for (int i = 0; i < s.len; i++) {
       s.data[i] = i;
     }
   }//end target
-  #pragma omp target update from(s)
-  for (i =0; i < s.len; i += 2) {
-    OMPVV_TEST_AND_SET(errors, s.data[i] != i);
+  
+  #pragma omp target data map(mapper(S), to:s) 
+  { 
+    #pragma omp target defaultmap(none) map(to:s.len)
+    for (int i = 0; i < s.len; i+=2) {
+      s.data[i] += i ;
+    }
+  
+  #pragma omp target update from(s.data[0:s.len:2])
   } 
+  
+
+  for (i =0; i < s.len; i++) {
+    if(i%2){
+      OMPVV_TEST_AND_SET(errors, s.data[i] != i);
+    }
+    else{
+      OMPVV_TEST_AND_SET(errors, s.data[i] != 2 * i);
+    }
+  }
+
   return errors;
 }
 
 int main() {
+
   OMPVV_TEST_OFFLOADING;
   OMPVV_TEST_AND_SET_VERBOSE(errors, target_update_from_mapper());
+
   OMPVV_REPORT_AND_RETURN(errors);
 }
