6bd9d7c7f6325fe7d14cbc98a5dd3ebb490c731b
Swaroop Pophale <swaroop.pophale23@gmail.com>
2023-03-10 13:50:47 -0500

    Update test_target_defaultmap_present_aggregate.c
    Fixing errors.

--- tests/5.1/target/test_target_defaultmap_present_aggregate.c
+++ tests/5.1/target/test_target_defaultmap_present_aggregate.c
@@ -26,39 +26,41 @@ int test_defaultmap_present_aggregate() {
    int a[N];
    struct test_struct {
      int s; 
-     int S[N]; 
-   }; 
-
+     int S[2];
+   };
+   
    //Initialize aggregate array
    for (i = 0; i < N; i++) {
       a[i] = i;
    }
-
+   
    //Initialize struct and associated members
    struct test_struct new_struct;
    new_struct.s = 10; new_struct.S[0] = 100; new_struct.S[1] = 1000;
-
-
-   #pragma omp target enter data map(to: a, new_struct)
-   #pragma omp target map(tofrom: errors) defaultmap(present: aggregate)
+   
+   
+   #pragma omp target data map(tofrom: a, new_struct)
    {
+   #pragma omp target map(tofrom: errors) defaultmap(present: aggregate)
+   {  
       for (i = 0; i < N; i++) {
          if(a[i] != i){errors++;}
-         a[i] = 2+i;
+         a[i] += 2;
       }
       
-      if(new_struct.s != 10 || new_struct.S[0] != 100 || new_struct.S[1] != 1000) {errors++;}
+      if(new_struct.s != 10 && new_struct.S[0] != 100 && new_struct.S[1] != 1000) {errors++;}
       new_struct.s = 7; new_struct.S[0] = 70; new_struct.S[1] = 700;
    }
-
+   }
+     
    OMPVV_ERROR_IF(errors > 0, "Values were not mapped to the device properly");
 
    for (i = 0; i < N; i++) {
-      OMPVV_TEST_AND_SET_VERBOSE(errors, a[i] == 2+i);
+      OMPVV_TEST_AND_SET(errors, a[i] != 2+i);
    }
 
-   OMPVV_TEST_AND_SET_VERBOSE(errors, new_struct.s == 7 ||  new_struct.S[0] == 70 || new_struct.S[1] == 700);
-  
+   OMPVV_TEST_AND_SET(errors, new_struct.s != 7 &&  new_struct.S[0] != 70 && new_struct.S[1] != 700);
+ 
    return errors;
 }
 
@@ -68,4 +70,5 @@ int main() {
    OMPVV_TEST_AND_SET_VERBOSE(errors, test_defaultmap_present_aggregate() != 0);
    OMPVV_REPORT_AND_RETURN(errors);
 }
+
     
