ab3eeaa2f7e5dca1949dac0c1fbb7689a01f4d84
Nolan Baker <nolanb@udel.edu>
2023-03-14 15:12:31 -0400

    Added FORTRAN version

--- tests/4.5/target_teams_distribute/test_target_teams_distribute_thread_limit.F90
+++ tests/4.5/target_teams_distribute/test_target_teams_distribute_thread_limit.F90
@@ -1,17 +1,11 @@
-!===--- test_target_teams_distribute_num_threads.F90------------------------===//
+!===--- test_target_teams_distribute_thread_limit.F90------------------------===//
 !
 ! OpenMP API Version 4.5 Nov 2015
 !
-! This test uses the num_threads clause on a target teams distribute directive to
+! This test uses the thread_limit clause on a target teams distribute directive to
 ! indicate a requested number of threads to execute the teams distribute region.
 ! The specifications indicate that the number of threads that are given can be any
-! number that is equal to or less than the indicated value. We first run a
-! target teams distribute region without the clause to see what the default
-! number of threads is, and then we use a value that is less than that in the
-! test of the num_threads clause. If the region is run with more threads than
-! indicated, the test errors. If the region is run with less threads than
-! indicated, the test issues a warning since it is known that the device can
-! run with more threads than was actually given.
+! number that is equal to or less than the indicated value.
 !
 !//===----------------------------------------------------------------------===//
 
@@ -32,31 +26,22 @@ PROGRAM test_target_teams_distribute_device
   OMPVV_REPORT_AND_RETURN()
 CONTAINS
   INTEGER FUNCTION test_threads()
-    INTEGER:: num_threads, default_threads, errors, x
+    INTEGER:: num_threads, errors, x
     errors = 0
+    num_threads = -9
 
-    !$omp target teams distribute map(from: default_threads)
+    !$omp target teams distribute thread_limit(4) map(from: num_threads)
     DO x = 1, N
        IF (omp_get_team_num() .eq. 0) THEN
-          default_threads = omp_get_thread_limit()
+               !$omp parallel
+               IF (omp_get_thread_num() .eq. 0) THEN
+                       num_threads = omp_get_num_threads()
+               END IF
+               !$omp end parallel
        END IF
     END DO
 
-    OMPVV_WARNING_IF(default_threads .eq. 1, "Test operated with one thread. Cannot test thread_limit clause.")
-    OMPVV_TEST_AND_SET(errors, default_threads .lt. 1)
-
-    IF (default_threads .gt. 0) THEN
-       !$omp target teams distribute map(from: num_threads) &
-       !$omp& thread_limit(default_threads / 2)
-       DO x = 1, N
-          IF (omp_get_team_num() .eq. 0) THEN
-             num_threads = omp_get_thread_limit()
-          END IF
-       END DO
-
-       OMPVV_TEST_AND_SET(errors, num_threads .gt. default_threads / 2)
-       OMPVV_WARNING_IF(num_threads .lt. default_threads / 2, "Test provided fewer threads than clause indicated.")
-    END IF
+    OMPVV_TEST_AND_SET(errors, num_threads .gt. 4)
 
     test_threads = errors
   END FUNCTION test_threads
