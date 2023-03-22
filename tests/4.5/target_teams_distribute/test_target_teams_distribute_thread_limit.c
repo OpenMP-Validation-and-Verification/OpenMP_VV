bbc3909f69429435e82988373e43f997cfaa13ee
Nolan Baker <61514629+nolanbaker31@users.noreply.github.com>
2023-03-22 11:39:26 -0400

    target_teams_distribute_thread_limit fix (#681)
    * Updated test
    
    * Changed from array to variable
    
    * Simplified with swaroops changes
    
    * Added FORTRAN version

--- tests/4.5/target_teams_distribute/test_target_teams_distribute_thread_limit.c
+++ tests/4.5/target_teams_distribute/test_target_teams_distribute_thread_limit.c
@@ -1,17 +1,11 @@
-//===--- test_target_teams_distribute_num_threads.c--------------------------===//
+//===--- test_target_teams_distribute_thread_limit.c--------------------------===//
 //
 // OpenMP API Version 4.5 Nov 2015
 //
-// This test uses the num_threads clause on a target teams distribute directive to
+// This test uses the thread_limit clause on a target teams distribute directive to
 // indicate a requested number of threads to execute the teams distribute region.
 // The specifications indicate that the number of threads that are given can be any
-// number that is equal to or less than the indicated value. We first run a
-// target teams distribute region without the clause to see what the default
-// number of threads is, and then we use a value that is less than that in the
-// test of the num_threads clause. If the region is run with more threads than
-// indicated, the test errors. If the region is run with less threads than
-// indicated, the test issues a warning since it is known that the device can
-// run with more threads than was actually given.
+// number that is equal to or less than the indicated value. 
 //
 ////===----------------------------------------------------------------------===//
 
@@ -22,34 +16,32 @@
 
 #define N 1024
 
-int main() {
-  OMPVV_TEST_OFFLOADING;
-  int default_threads;
-  int num_threads;
+int test_target_teams_distribute_thread_limit(){
+  int num_threads = -9;
   int errors = 0;
-
-#pragma omp target teams distribute map(from: default_threads)
-  for (int x = 0; x < N; ++x) {
-    if (omp_get_team_num() == 0) {
-      default_threads = omp_get_thread_limit();
-    }
-  }
-
-  OMPVV_WARNING_IF(default_threads == 1, "Test operated with one thread. Cannot test thread_limit clause.");
-  OMPVV_TEST_AND_SET(errors, default_threads <= 0);
-
-  if (default_threads > 0) {
-#pragma omp target teams distribute thread_limit(default_threads / 2) map(from: num_threads)
+  
+  
+  #pragma omp target teams distribute thread_limit(4) map(from: num_threads)
     for (int x = 0; x < N; ++x) {
       if (omp_get_team_num() == 0) {
-        num_threads = omp_get_thread_limit();
+        #pragma omp parallel
+        {
+        if (omp_get_thread_num() == 0)
+          num_threads = omp_get_num_threads();
+        }
       }
     }
+    OMPVV_TEST_AND_SET_VERBOSE(errors, num_threads > 4);
 
-    OMPVV_TEST_AND_SET(errors, num_threads > default_threads / 2);
-    OMPVV_WARNING_IF(num_threads < default_threads / 2, "Test was provided fewer threads than the thread_limit clause indicated. Still spec-conformant.");
+  return errors;
 
-  }
+}
 
+int main(){
+  int errors = 0;
+  OMPVV_TEST_OFFLOADING;
+
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_target_teams_distribute_thread_limit() != 0);
   OMPVV_REPORT_AND_RETURN(errors);
 }
+
