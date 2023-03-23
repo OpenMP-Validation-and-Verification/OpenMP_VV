39c1e2d7abab6dbfc3d01c5a4820f40f6ea30339
mjcarr458 <54590896+mjcarr458@users.noreply.github.com>
2023-03-23 09:34:06 -0400

    indirect declare target test (#689)
    * funcptr test structure
    
    * Update test_target_declare_indirect.c
    
    Hope this helps.
    
    Have not compiled yet, please test.
    
    * Update test_target_declare_indirect.c
    
    Minor fixes.
    
    * Update tests/5.1/target/test_target_declare_indirect.c
    
    Co-authored-by: Tobias Burnus <tobias@codesourcery.com>
    
    * adding test description
    
    * Update test_target_declare_indirect.c
    
    "enter" is 5.2, reverting to "to"
    
    ---------
    
    Co-authored-by: Swaroop Pophale <swaroop.pophale23@gmail.com>
    Co-authored-by: Tobias Burnus <tobias@codesourcery.com>

---
+++ tests/5.1/target/test_target_declare_indirect.c
@@ -0,0 +1,70 @@
+//----------------test_target_declare_indirect.c-----------------
+//
+// OpenMP API Version 5.1 Mar 2023
+//
+// This test intends to test the functionality of an indirect
+// declare target region. A random int is chosen that correlates
+// to a function pointer. That function is then chosen and the 
+// test ensures it runs as expected.
+// --------------------------------------------------------------
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "ompvv.h"
+#include <math.h>
+
+typedef int(*funcptr)();
+
+int fun1(){
+	return 5;
+}
+
+int fun2(){
+	return 10;
+}
+
+int fun3(){
+	return 15;
+}
+#pragma omp declare target to(fun1, fun2, fun3) indirect
+
+int test_declare_target_indirect(){
+
+        funcptr fptr;
+	int errors = 0;
+        int fun_choice = rand() % 3; //will give either 0, 1 or 2
+	int ret_val, result;
+        
+	if(!fun_choice){
+	  fptr = &fun1;
+	  result = 5;
+	}
+	  
+	if(fun_choice == 1){
+	  fptr = &fun2;
+	  result = 10;
+	}
+	  
+	if(fun_choice == 3){
+	  fptr = &fun3;
+	  result = 15;
+	}	
+	  
+	#pragma omp target map(from: ret_val)
+	  ret_val = fptr();
+	  
+        OMPVV_TEST_AND_SET(errors, ret_val != result);
+	return errors;
+}
+
+
+
+int main(){
+
+  int errors = 0;
+  OMPVV_TEST_OFFLOADING;
+  
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_declare_target_indirect() != 0);
+  OMPVV_REPORT_AND_RETURN(errors);
+	
+}
