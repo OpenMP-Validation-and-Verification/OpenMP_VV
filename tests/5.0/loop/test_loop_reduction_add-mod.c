29f393afc63696992b119f70dc7d09cc7d1188a1
Andrew Kallai <andrewka@udel.edu>
2023-03-27 17:05:47 +0000

    added ompvv statements to reduction add clause test and removed comments

--- tests/5.0/loop/test_loop_reduction_add-mod.c
+++ tests/5.0/loop/test_loop_reduction_add-mod.c
@@ -3,8 +3,8 @@
 // OpenMP API Version 5.0 Nov 2018
 //
 // This test uses the reduction clause on a loop directive, testing that the
-// variable in the reduction clause is properly reduced using the add
-// operator.
+// variables in the reduction clause are properly reduced using the add
+// operator. Test written in reference to test_loop_reduction_add.c.
 //
 //===----------------------------------------------------------------------------===//
 
@@ -12,10 +12,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "ompvv.h"
-//#include <math.h>
-//#define N 1024
-int main() {
-  int n = -32; 
+
+int test_add() {
+  int n = -32, errors=0; 
   float f = -32.0;
   double d = -32.0;
   char c = '(';
@@ -23,68 +22,31 @@ int main() {
 
 #pragma omp parallel num_threads(OMPVV_NUM_THREADS_HOST)
   {
-    #pragma omp loop reduction(+:n,f,d,c) 
+  #pragma omp loop reduction(+:n,f,d,c) 
     for(int i=0;i<64;i++){
       n = n + 1;
       f = f + 1.0;  
       d = d + 1.0;  
       c = c + 1;
     }
-
-    #pragma omp for 
+  #pragma omp for 
     for(int i=0;i<64;i++){
       thread_counts[omp_get_thread_num()] += 1;
     }
   }
 
   for(int i = 0; i < 8; i++){
-    printf("%i,",thread_counts[i]);
+    OMPVV_WARNING_IF(8 != thread_counts[i], "THREADS NOT DISTRIBUTED EVENLY, TESTING IS INVALID");
   }
-  printf("\n%i,%f,%f,%c\n",n,f,d,c);
-  return 0;
+
+  OMPVV_TEST_AND_SET_VERBOSE(errors, (n != 32) || (f != 32.0) || (d != 32.0) || (c != 'h'));
+  OMPVV_ERROR_IF(errors, "\nn|EXPECTED:32, RECEIVED:%d\nf|EXPECTED:32.0, RECEIVED:%.1f\nd|EXPECTED:32.0, RECEIVED:%.1f\nc|EXPECTED:h, RECEIVED:%c\n",n,f,d,c); 
+
+  return errors;
 }
-//int test_add() {
-//  int a[N], b[N], num_threads[N];
-//  int total = 0, expect_total = 0, errors = 0;
-//
-//  for (int x = 0; x < N; ++x) {
-//    a[x] = 1;
-//    b[x] = x;
-//    num_threads[x] = -1;
-//  }
-//
-//#pragma omp parallel num_threads(OMPVV_NUM_THREADS_HOST)
-//  {
-//#pragma omp loop reduction(+:total)
-//    for (int x = 0; x < N; ++x) {
-//      total += a[x] + b[x];
-//    }
-//#pragma omp for
-//    for (int x = 0; x < N; ++x) {
-//      num_threads[x] = omp_get_num_threads();
-//    }
-//  }
-//
-//  for (int x = 0; x < N; ++x) {
-//    expect_total += a[x] + b[x];
-//  }
-//
-//  for (int x = 1; x < N; ++x) {
-//    OMPVV_WARNING_IF(num_threads[x - 1] != num_threads[x], "Test reported differing numbers of threads.  Validity of testing of reduction clause cannot be guaranteed.");
-//  }
-//  OMPVV_WARNING_IF(num_threads[0] == 1, "Test operated with one thread.  Reduction clause cannot be tested.");
-//  OMPVV_WARNING_IF(num_threads[0] <= 0, "Test reported invalid number of threads.  Validity of testing of reduction clause cannot be guaranteed.");
-//
-//  OMPVV_TEST_AND_SET_VERBOSE(errors, expect_total != total);
-//  OMPVV_ERROR_IF(expect_total != total, "Total from loop directive is %d but expected total is %d.", total, expect_total);
-//
-//  return errors;
-//}
-//
-//int main() {
-//  int total_errors = 0;
-//
-//  OMPVV_TEST_AND_SET_VERBOSE(total_errors, test_add() != 0);
-//
-//  OMPVV_REPORT_AND_RETURN(total_errors);
-//}
+int main() {
+  int total_errors = 0;
+  OMPVV_TEST_AND_SET_VERBOSE(total_errors, test_add() != 0);
+  OMPVV_REPORT_AND_RETURN(total_errors);
+  return total_errors;
+}
