ee1d384df9d8c15fabc1db38f0935e82968ceb48
Andrew Kallai <andrewka@udel.edu>
2023-03-29 20:27:34 +0000

    Removed lines which checked for thread distribution and included the target directive in the device test so the test runs on the device

--- tests/5.0/loop/test_loop_reduction_add_device-mod.c
+++ tests/5.0/loop/test_loop_reduction_add_device-mod.c
@@ -19,9 +19,8 @@ int test_add() {
   float f = -32.0;
   double d = -32.0;
   char c = '(';
-  int thread_counts[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-#pragma omp parallel num_threads(OMPVV_NUM_THREADS_HOST)
+#pragma omp target map(tofrom:n,f,d,c) 
   {
   #pragma omp loop reduction(+:n,f,d,c) 
     for(int i=0;i<64;i++){
@@ -30,14 +29,6 @@ int test_add() {
       d = d + 1.0;  
       c = c + 1;
     }
-  #pragma omp for 
-    for(int i=0;i<64;i++){
-      thread_counts[omp_get_thread_num()] += 1;
-    }
-  }
-
-  for(int i = 0; i < 8; i++){
-    OMPVV_WARNING_IF(8 != thread_counts[i], "THREADS NOT DISTRIBUTED EVENLY, TESTING IS INVALID");
   }
 
   OMPVV_TEST_AND_SET_VERBOSE(errors, (n != 32) || (f != 32.0) || (d != 32.0) || (c != 'h'));
