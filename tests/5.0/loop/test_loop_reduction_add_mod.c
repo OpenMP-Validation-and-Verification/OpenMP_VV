49a3eb73fdc99eb612d3fb31d7205e2a6ded7c51
andrewkallai <73764771+andrewkallai@users.noreply.github.com>
2023-04-17 22:16:58 -0400

    Update test_loop_reduction_add_mod.c

--- tests/5.0/loop/test_loop_reduction_add_mod.c
+++ tests/5.0/loop/test_loop_reduction_add_mod.c
@@ -1,40 +1,41 @@
-//===--- test_loop_reduction_add_device-mod.c ------------------------------------------===//
+//===--- test_loop_reduction_add_mod.c ------------------------------------===//
 //
 // OpenMP API Version 5.0 Nov 2018
 //
 // This test uses the reduction clause on a loop directive, testing that the
 // variables in the reduction clause are properly reduced using the add
-// operator. This test checks the above in a target context.
-// Test written in reference to test_loop_reduction_add_device.c.
+// operator. Standard types are reduced: int, float, double, char.
+// Test written in reference to test_loop_reduction_add.c.
 //
-//===----------------------------------------------------------------------------===//
+//===-----------------------------------------------------------------------===//
 
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "ompvv.h"
-#define A -32 
-#define B -32.0 
-#define L '(' 
+#define A -32
+#define B 64
+#define L 40
+#define X 1
 
 int test_add() {
   int n = A, errors=0; 
-  float f = B;
-  double d = B;
+  float f = A;
+  double d = A;
   char c = L;
-  int exp_n = A + 64;
-  float exp_f = B + 64.0;
-  double exp_d = B + 64.0;
-  char exp_c = L + 64;
+  int exp_n = A + B;
+  float exp_f = A + B;
+  double exp_d = A + B;
+  char exp_c = L + B;
 
 #pragma omp parallel 
   {
   #pragma omp loop reduction(+:n,f,d,c) 
-    for(int i=0;i<64;i++){
-      n = n + 1;
-      f = f + 1.0;  
-      d = d + 1.0;  
-      c = c + 1;
+    for(int i=0;i<B;i++){
+      n = n + X;
+      f = f + X;  
+      d = d + X;  
+      c = c + X;
     }
   }
 
