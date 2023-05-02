7eb44e454f4267a6cb29aa4d480493e03235511b
Andrew Kallai <kallai.andrew@gmail.com>
2023-05-02 14:32:28 -0700

    Removed unecessary info in test header

--- tests/5.2/scope/test_scope_firstprivate_construct_mod.c
+++ tests/5.2/scope/test_scope_firstprivate_construct_mod.c
@@ -1,12 +1,10 @@
 //--------------- test_scope_firstprivate_construct_mod.c -----------------------------//
 //
-// Code written in reference to OpenMP API Version 5.1 Nov 2020
-// Modified for OpenMP API Version 5.2 Nov 2021 
+// OpenMP API Version 5.2 Nov 2021 
 //
 // This test checks that the scope firstprivate construct clause is properly working.
 // The test itself passes a test integer into the scope pragma and ensures that
 // all changes made to it are not kept outside of the scope region.
-// Written in reference to 5.1/scope/test_scope_private_construct.c
 //----------------------------------------------------------------------------//
 
 
