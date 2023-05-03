8f0cf9dd7636c81892715fdeb919851c9362f35d
Jaydon Reap <jaydonreap@gmail.com>
2023-05-03 10:39:12 -0700

    Finalized test for set_num_teams

--- tests/5.1/teams/test_target_set_num_teams.c
+++ tests/5.1/teams/test_target_set_num_teams.c
@@ -2,7 +2,11 @@
 //
 // OpenMP API Version 5.1 Nov 2020
 //
-// 
+// This test uses the omp_set_num_teams utility to set the default number of
+// teams allocated for team clauses. The test runs teams and checks to see if
+// the number allocated is correct and reflective of the new default set. It
+// then checks to see if an explicitly stated num_teams clause overrides
+// the omp_set_num_teams properly.
 //
 //===------------------------------------------------------------------------===//
 
@@ -17,45 +21,30 @@ int main() {
 
 	OMPVV_TEST_OFFLOADING;
 
-	int errors = 0;
-	int not_shared = 5;
-	int num_teams = 0; 
+	int errors = 0; 
+	int num_teams = 0;
 
-	#pragma omp target teams map(tofrom:num_teams, errors) 
+	omp_set_num_teams(1);
+	
+	#pragma omp target teams map(tofrom: num_teams)
 	{
-		#pragma omp parallel
-		{
-			if (omp_get_team_num() == 0) {
-				num_teams = omp_get_num_teams();
-			}
+		if (omp_get_team_num() == 0) {
+			num_teams = omp_get_num_teams();
+		}
+	}               
+	
+	OMPVV_ERROR_IF(num_teams == 1, "Number of teams detected was not the amount set by omp_set_num_teams()");
+	OMPVV_TEST_AND_SET(errors, num_teams == 1);
+
+	#pragma omp target teams map(tofrom: num_teams) num_teams(OMPVV_NUM_TEAMS_DEVICE)
+	{
+		if (omp_get_team_num() == 0) {
+			num_teams = omp_get_num_teams();
 		}
 	}
 
-	//Set the number of teams to be one less what the default is
-	//omp_set_num_teams(num_teams - 1);
-		
-
-	#pragma omp target teams map(tofrom: num_teams, errors)
-	{
-		#pragma omp parallel
-		{
-			if (omp_get_team_num() == 0) {
-				OMPVV_ERROR_IF(omp_get_num_teams() == num_teams, "Team count did not change when the teams limit was set");
-				OMPVV_TEST_AND_SET(errors, omp_get_num_teams() == num_teams);
-			}
-		}
-	}
-
-	#pragma omp target teams num_teams(OMPVV_NUM_TEAMS_DEVICE)
-	{
-		#pragma omp parallel
-		{
-			if (omp_get_team_num() == 0) {
-				OMPVV_ERROR_IF(omp_get_num_teams() != OMPVV_NUM_TEAMS_DEVICE, "Team count was not properly overriden by the num_teams clause");
-				OMPVV_TEST_AND_SET(errors, omp_get_num_teams() != OMPVV_NUM_TEAMS_DEVICE);
-			}
-		}
-	}
+	OMPVV_ERROR_IF(num_teams != OMPVV_NUM_TEAMS_DEVICE, "Number of teams was not properly overriden by the num_teams clause");
+	OMPVV_TEST_AND_SET(errors, num_teams != OMPVV_NUM_TEAMS_DEVICE);
 
 	OMPVV_REPORT_AND_RETURN(errors);
 }
