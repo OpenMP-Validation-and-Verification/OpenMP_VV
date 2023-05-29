a1cf2046dc4d39677c12838b577f5a538c646ec1
Lakhan singh Thakur <Lakhan-singh.Thakur@amd.com>
2023-05-29 17:32:44 +0530

    Modified collapse related test case

--- tests/5.0/target/test_target_collapse.c
+++ tests/5.0/target/test_target_collapse.c
@@ -11,21 +11,21 @@
 #include "omp.h"
 #include "ompvv.h"
 
-#define N 100
-
+#define N 5
+#define ITERS 5
 int Runtst(int gpu) {
-  omp_set_num_threads(N*N);
+  omp_set_num_threads(N);
   int A[N], B[N] = {0};
   int errors = 0;
   for (int i = 0; i < N; ++i) {
     A[i] = 0;
-    B[i] = i;
+    B[i] = i + 1;
   }
-  int ThrdTrack[N] = {0};
+  int ThrdTrack[ITERS * ITERS + ITERS] = {0}; // an array of 30 elements
 #pragma omp target data map(tofrom: A, B, ThrdTrack) device(gpu)
 #pragma omp target parallel for collapse(2) shared(A, B, ThrdTrack) device(gpu)
-  for (int i = 0; i < N; ++i) {
-    for (int j = 0; j < N; ++j) {
+  for (int i = 0; i < ITERS; ++i) {
+    for (int j = 0; j < ITERS; ++j) {
       int ThrdId = omp_get_thread_num();
       ThrdTrack[ThrdId] = ThrdId;
 #pragma omp atomic
@@ -36,10 +36,17 @@ int Runtst(int gpu) {
   int count = 0;
   // verifying the result
   for (int i = 0; i < N; ++i) {
-    if (A[i] != N * (N - 1)/2) {
+    if (A[i] != N * (N + 1)/2) {
       errors++;
     }
     if (ThrdTrack[i] != i) {
+      errors++;
+    }
+  }
+  // since only 25 threads are expected to get launched the remaining 5 places
+  // of ThrdTrack[] array should still have zeros
+  for (int i = 25; i < 30; ++i) {
+    if (ThrdTrack[i] != 0) {
       errors++;
     }
   }
