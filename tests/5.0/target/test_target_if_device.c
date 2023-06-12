2a2edce2304633e41218835349c62181eaf6416f
lthakur <lakhan-singh.thakur@amd.com>
2023-06-12 17:09:48 +0530

    Update tests/5.0/target/test_target_if_device.c
    Co-authored-by: fel-cab <110852406+fel-cab@users.noreply.github.com>

--- tests/5.0/target/test_target_if_device.c
+++ tests/5.0/target/test_target_if_device.c
@@ -15,7 +15,7 @@
 int Runtst(int gpu, int Proc) { // Proc = 0(host), 1(gpu)
   int *A = malloc(sizeof(int) * N), Errors = 0;
   int *InitDev = malloc(sizeof(int));
-  for (int i = 0; i < THREADS; ++i) {
+  for (int i = 0; i < N; ++i) {
     A[i] = i;
   }
 
