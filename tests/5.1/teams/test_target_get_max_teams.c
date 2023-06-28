9748725497ffa6f8ff791bd62b3ae70309899e7f
Swaroop Pophale <swaroop.pophale23@gmail.com>
2023-06-28 12:53:05 -0400

    Update test_target_get_max_teams.c
    Separating host and device checks.

--- tests/5.1/teams/test_target_get_max_teams.c
+++ tests/5.1/teams/test_target_get_max_teams.c
@@ -17,24 +17,40 @@
 
 int main() {
 
-	OMPVV_TEST_OFFLOADING;
+        OMPVV_TEST_OFFLOADING;
 
-	int errors = 0; 
-	int MAX_TEAMS = omp_get_max_teams();
-	int num_teams = MAX_TEAMS + 1; //a value that is not possible
+        int errors = 0;
+        int MAX_TEAMS = omp_get_max_teams(); //on host
+        int num_teams = MAX_TEAMS + 1; //a value that is not possible
+        printf("max on host:%d\n",MAX_TEAMS);
+        #pragma omp teams
+        {
+                if (omp_get_team_num() == 0) {
+                        num_teams = omp_get_num_teams();
+                }
+        }       
 
-	#pragma omp target teams map(tofrom: num_teams)
-	{
-		if (omp_get_team_num() == 0) {
-			num_teams = omp_get_num_teams();
-		}
-	}               
+        OMPVV_ERROR_IF(MAX_TEAMS > 0 && num_teams > MAX_TEAMS, "Number of teams reported on host exceeded max number of teams (max no. > 0)");
+        OMPVV_ERROR_IF(num_teams <= 0, "Number of teams reported on host is 0 or negative");
+        OMPVV_TEST_AND_SET(errors, num_teams > MAX_TEAMS);
+        OMPVV_TEST_AND_SET(errors, num_teams <= 0);
+        
+        #pragma omp target map(tofrom:num_teams)
+        {
+           MAX_TEAMS = omp_get_max_teams(); //first-private
+          #pragma omp teams 
+          {
+             if (omp_get_team_num() == 0) {
+               printf("max on target:%d\n",MAX_TEAMS);
+               num_teams = MAX_TEAMS + 1; //a value that is not possible
+               num_teams = omp_get_num_teams();
+             }  
+          }
+        }
+        OMPVV_ERROR_IF(MAX_TEAMS > 0 && num_teams > MAX_TEAMS, "Number of teams reported on device exceeded max number of teams (max no. > 0)");
+        OMPVV_ERROR_IF(num_teams <= 0, "Number of teams reported on device is 0 or negative");
+        OMPVV_TEST_AND_SET(errors, num_teams > MAX_TEAMS);
+        OMPVV_TEST_AND_SET(errors, num_teams <= 0);
 
-        OMPVV_ERROR_IF(MAX_TEAMS > 0 && num_teams > MAX_TEAMS, "Number of teams reported exceeded max number of teams (max no. > 0)");
-	OMPVV_ERROR_IF(num_teams < 0, "Number of teams is negative");
-	
-	OMPVV_TEST_AND_SET(errors, num_teams < 0);
-	OMPVV_TEST_AND_SET(errors, num_teams > MAX_TEAMS);
-
-	OMPVV_REPORT_AND_RETURN(errors);
+        OMPVV_REPORT_AND_RETURN(errors);
 }
