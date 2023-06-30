afa1a282e2be2a97d325821a72eb29cacee24d09
Seyong Lee <lees2@ornl.gov>
2023-06-30 10:59:44 -0400

    Add new_test/5.1/test_target_map_iterators.F90 (#727)

---
+++ tests/5.1/target/test_target_map_iterators.F90
@@ -0,0 +1,55 @@
+!===--- test_target_map_iterator.F90 ----------------------------------===//
+!
+! OpenMP API Version 5.1 Nov 2020
+!
+! This test is designed to test the iterator map-type-modifier for the map
+! clause. The test should create a list of size N and then pass to the 
+! target region the length of the array 1:N and modify the values.
+! The test then checks to see if the appropriate range was modified.
+!
+!//===-----------------------------------------------------------------===//
+
+#include "ompvv.F90"
+
+#define N 1024
+
+PROGRAM test_target_map_iterator
+  USE iso_fortran_env
+  USE, INTRINSIC :: iso_c_binding
+  USE ompvv_lib
+  USE omp_lib
+  implicit none
+  OMPVV_TEST_OFFLOADING
+
+  OMPVV_TEST_VERBOSE(test_map_iterator() .NE. 0)
+
+  OMPVV_REPORT_AND_RETURN()
+
+CONTAINS
+  INTEGER FUNCTION test_map_iterator()
+    INTEGER :: errors, i, listsum
+    INTEGER, DIMENSION(N) :: test_lst
+
+    errors = 0
+    listsum = 0
+
+    DO i=1, N
+      test_lst(i) = 1
+    END DO
+
+    !$omp target map(iterator(it = 1:N), tofrom: test_lst(it))
+    DO i=1, N
+      test_lst(i) = 2
+    END DO
+    !$omp end target
+
+    DO i=1, N
+      listsum = listsum + test_lst(i)
+    END DO
+
+    OMPVV_TEST_AND_SET(errors, listsum .NE. 2*N)
+
+    test_map_iterator = errors
+  END FUNCTION test_map_iterator
+END PROGRAM test_target_map_iterator
+
