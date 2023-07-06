cbffc07e1338a595944393b76a82cdef89c51e8e
Andrew Kallai <kallaiandrew@gmail.com>
2023-07-06 13:35:48 -0400

    Modified test to follow simple and controllable features which allow for easy testing of otherwise

--- tests/5.2/metadirective/test_metadirective_otherwise.c
+++ tests/5.2/metadirective/test_metadirective_otherwise.c
@@ -6,47 +6,40 @@
 // *************************
 // The otherwise clause of the metadirective construct is being tested. The
 // metadirective construct contains a when clause, which itself contains a
-// context-selector (device = {kind(host)}) and a directive-variant (nothing).
+// context-selector (device = {kind(nohost)}) and a directive-variant (nothing).
 // The metadirective construct also contains an otherwise clause, which itself
-// contains a directive-variant (parallel for).
+// contains a directive-variant (target map(tofrom : on_host)).
 // The when clause of the metadirective construct is
 // evaluated for a valid context-selector pertaining to a device related
-// characteristic of a certain kind (host), defined in the OpenMP additional
-// definitions. If no target construct were specified, the evaluation would
+// characteristic of a certain kind (nohost), defined in the OpenMP additional
+// definitions. If a target construct were specified, the evaluation would
 // be true, and the directive-variant nothing would be executed as if
-// #pragma omp nothing were present. Target is present, so the directive-variant
-// of the otherwise clause will be executed. This would not occur if
-// device = {kind(nohost)} were specified. If the given for loop executes in
-// a nested parallel region, the test will pass.
+// #pragma omp nothing were present. Target is not present, so the
+// directive-variant of the otherwise clause will be executed. This would not
+// occur if device = {kind(host)} were specified. If omp_is_initial_device()
+// evaluates to false (0), then the test will pass.
 //----------------------------------------------------------------------------//
 
 #include "ompvv.h"
 #include <omp.h>
 
-#define N 16
-
 int test_otherwise() {
   int errors = 0;
-  int total = 0;
+  int on_host = 1;
   int num_devices = omp_get_num_devices();
 
-  #pragma omp target map(tofrom : total)
-  {
   #pragma omp metadirective \
-    when(device = {kind(host)}: nothing) \
-    otherwise(parallel for)
+    when(device = {kind(nohost)}: nothing) \
+    otherwise(target map(tofrom : on_host))
     {
-      for (int i = 0; i < N; ++i) {
-        if (omp_get_level()) {
-          #pragma omp atomic update
-          ++total;
-        }
-      }
+      on_host = omp_is_initial_device();
     }
-  }
+
   if (num_devices > 0) {
-    OMPVV_ERROR_IF(total != N, "Value of total is %i, not %i", total, N);
-    OMPVV_TEST_AND_SET_VERBOSE(errors, total != N);
+    OMPVV_ERROR_IF(
+        on_host == 1,
+        "A device was available, but the target directive was not executed");
+    OMPVV_TEST_AND_SET_VERBOSE(errors, on_host == 1);
   } else {
     OMPVV_WARNING(
         "NO DEVICES ARE AVAILABLE, OTHERWISE CLAUSE WAS NOT ACCESSED");
@@ -57,7 +50,6 @@ int test_otherwise() {
 
 int main() {
   int errors = 0;
-  OMPVV_INFOMSG("IMPLEMENTATION DEFINED OPENMP VERSION: %d", _OPENMP);
   OMPVV_TEST_OFFLOADING;
   OMPVV_TEST_AND_SET_VERBOSE(errors, test_otherwise() != 0);
   OMPVV_REPORT_AND_RETURN(errors);
