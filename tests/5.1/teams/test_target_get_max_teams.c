1590d683939a0f9c6b0b87b0ccfeaf77b4eb3154
Andrew Kallai <kallaiandrew@gmail.com>
2023-07-12 13:54:32 -0400

    Changed test header file per suggestions and removed the check for num_teams when the ICV is non-positive

--- tests/5.1/teams/test_target_get_max_teams.c
+++ tests/5.1/teams/test_target_get_max_teams.c
@@ -1,18 +1,19 @@
 //--------------- test_target_get_max_teams.c --------------------------------//
 // OpenMP API Version 5.1 Nov 2020
+// Pg. 669, line 29
 // ***************************
 // ROUTINE: omp_get_max_teams
 // ***************************
 // This test uses the omp_get_max_teams routine to check what the max teams
-// capacity is for the current device. If the routine returns 0, the nteams-var
-// ICV is 0, thus it is not treated as an upper bound and the behavior of the
-// program is implementation defined. If the routine were to return a negative
-// integer, the same behavior applies. If the routine returns a positive
-// integer, the value of the ICV is treated as an upperbound. If the value is a
-// number greater than the maximum capacity which an implementation supports,
-// than the test will pass as the number of teams is less than that value, but
-// the value itself has no functional purpose. The ICV is initialized to 0 per
-// device. Thus, if omp_get_num_teams returns a positive number as
+// capacity is for host and the current device. If the routine returns 0, the
+// nteams-var ICV is 0, thus it is not treated as an upper bound and the
+// behavior of the program is implementation defined. If the routine were to
+// return a negative integer, the same behavior applies. If the routine returns
+// a positive integer, the value of the ICV is treated as an upperbound. If the
+// value is a number greater than the maximum capacity that the implementation
+// supports, then the test will pass if the number of teams is less than that
+// value, but the value itself has no functional purpose. The ICV is initialized
+// to 0 per device. Thus, if omp_get_num_teams returns a positive number as
 // expected, the test will pass when the ICV value is 0 or less than 0. In the
 // case where the OMP_NUM_TEAMS environmental variable is set to a positive
 // integer, or the omp_set_num_teams routine is given a positive integer as an
@@ -27,7 +28,6 @@ int test_get_max_teams(int offload) {
   int max_teams = omp_get_max_teams(); // on host
   int num_teams = 0; // a value that is not possible for omp_get_num_teams()
 
-  // clang-format off
   #pragma omp target map(tofrom : num_teams) if(offload)
   {
     #pragma omp teams 
@@ -35,15 +35,10 @@ int test_get_max_teams(int offload) {
       num_teams = omp_get_num_teams();
     }
   }
-  // clang-format on
 
-  OMPVV_ERROR_IF(
-      max_teams <= 0 && num_teams <= 0,
-      "Number of teams reported is non-positive, should not be possible");
   OMPVV_ERROR_IF(
       max_teams > 0 && num_teams > max_teams,
       "Number of teams reported exceeded max number of teams (max no. > 0)");
-  OMPVV_TEST_AND_SET(errors, max_teams <= 0 && num_teams <= 0);
   OMPVV_TEST_AND_SET(errors, max_teams > 0 && num_teams > max_teams);
 
   return errors;
