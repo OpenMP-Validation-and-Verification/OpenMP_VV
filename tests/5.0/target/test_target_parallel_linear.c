279495b6f5e4f27e70d3ff51cc1dacdced84bb53
Tobias Burnus <tobias@codesourcery.com>
2023-09-18 10:23:54 +0200

    test_target_parallel_linear.c: Fix memory allocation

--- tests/5.0/target/test_target_parallel_linear.c
+++ tests/5.0/target/test_target_parallel_linear.c
@@ -15,7 +15,7 @@
 
 
 int Runtst(int gpu) {
-  int *A = malloc(sizeof(int) * THREADS), errors = 0;
+  int *A = malloc(sizeof(int) * N), errors = 0;
   for (int i = 0; i < N; ++i) {
     A[i] = i;
   }
