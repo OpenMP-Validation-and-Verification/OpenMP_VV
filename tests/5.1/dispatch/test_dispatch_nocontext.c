e754d2dc706b3a858777ddff9f9a11dabcfae456
Andrew Kallai <kallaiandrew@gmail.com>
2023-10-25 15:15:47 -0400

    Changed or conditional to and, added comment, changed warning to infomsg, fixed one conditional

--- tests/5.1/dispatch/test_dispatch_nocontext.c
+++ tests/5.1/dispatch/test_dispatch_nocontext.c
@@ -1,79 +1,86 @@
-//===---- test_dispatch_nocontext.c -----------------------------------------===//
-// 
+//===---- test_dispatch_nocontext.c ---------------------------------------===//
+//
+//
 // OpenMP API Version 5.1
 //
-// Uses dispatch construct as context for variant directive. Uses nocontext clause
-// which can determine whether dispatch is a viable construct for the variant directive.
-// When nocontext is true, then dispatch is not a viable construct and vice versa. 
+// Uses dispatch construct as context for variant directive. Uses nocontext
+// clause which can determine whether dispatch is a viable construct for the
+// variant directive. When nocontext is true, then dispatch is not a viable
+// construct and vice versa.
 //
 // Inspired by "OpenMP 5.1 Features: The Dispatch Construct" video:
 // https://www.youtube.com/watch?v=ruugaX95gIs
-// 
-//===-------------------------------------------------------------------------------===//
+//
+//===----------------------------------------------------------------------===//
 
+#include "ompvv.h"
+#include <math.h>
 #include <omp.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include "ompvv.h"
-#include <stdbool.h>
 
 #define N 1024
 
-int arr[N]; // implicit map array 
+int arr[N]; // implicit map array
 int errors;
 int i = 0;
 
 void add_two(int *arr);
 
-#pragma omp declare variant(add_two) match(construct={dispatch})
-void add(int *arr){
-   for (int i = 0; i < N; i++){ // Base function adds 1 to array values
-      arr[i] = i+1;
-   }
+#pragma omp declare variant(add_two) match(construct = {dispatch})
+void add(int *arr) {
+  for (int i = 0; i < N; i++) { // Base function adds 1 to array values
+    arr[i] = i + 1;
+  }
 }
 
-void add_two(int *arr){
-   for (int i = 0; i < N; i++){
-      arr[i] = i+2; // Variant function adds 2 to array values
-   }
+void add_two(int *arr) {
+  for (int i = 0; i < N; i++) {
+    arr[i] = i + 2; // Variant function adds 2 to array values
+  }
 }
 
-int test_wrapper() { 
-   errors = 0;
-   int err_ar[2] = {0, 0};
-   bool nocontext_arg;
-   add(arr);
-   for(i = 0; i < N; i++){
-      OMPVV_TEST_AND_SET(errors, arr[i] != i+1);
-   } 
-   OMPVV_ERROR_IF(errors > 0, "Base function is not working properly");
-   nocontext_arg = true;
-   #pragma omp dispatch nocontext(nocontext_arg)
-      add(arr);
-   
-   for(i = 0; i < N; i++){
-      OMPVV_TEST_AND_SET(err_ar[0], (arr[i] != i+1) || (arr[i] != i+2));
-   }
-   errors += err_ar[0];
-   OMPVV_ERROR_IF(errors > 0, "Dispatch w/ nocontext true is not working properly");
+int test_wrapper() {
+  errors = 0;
+  int err_ar[2] = {0, 0};
+  bool nocontext_arg;
+  add(arr);
+  for (i = 0; i < N; i++) {
+    OMPVV_TEST_AND_SET(errors, arr[i] != i + 1);
+  }
+  OMPVV_ERROR_IF(errors > 0, "Base function is not working properly");
+  nocontext_arg = true;
+  #pragma omp dispatch nocontext(nocontext_arg)
+  add(arr);
 
-   nocontext_arg = false;
-   #pragma omp dispatch nocontext(nocontext_arg)
-      add(arr);
+  for (i = 0; i < N; i++) {
+    OMPVV_TEST_AND_SET(err_ar[0], (arr[i] != i + 1));
+  }
+  errors += err_ar[0];
+  OMPVV_ERROR_IF(errors > 0,
+                 "Dispatch w/ nocontext true is not working properly");
 
-   for(i = 0; i < N; i++){
-      OMPVV_TEST_AND_SET(err_ar[1], (arr[i] != i+1) || (arr[i] != i+2));
-   }
-   errors += err_ar[1];
-   OMPVV_ERROR_IF(errors > 0, "Dispatch w/ nocontext false is not working properly");
-   OMPVV_WARNING_IF(errors > 0, "Dispatch is either not working or was not considered" 
-                    " by the implementation as part of the context selector.");
-   return errors;
+  nocontext_arg = false;
+  #pragma omp dispatch nocontext(nocontext_arg)
+  add(arr);
+
+  for (i = 0; i < N; i++) {
+    OMPVV_TEST_AND_SET(err_ar[1], (arr[i] != i + 1) && (arr[i] != i + 2));
+  }
+  // OMP 5.1, Pg54:6 -Whether the dispatch construct is added to the construct
+  // set is implementation defined.
+  errors += err_ar[1];
+  OMPVV_ERROR_IF(errors > 0,
+                 "Dispatch w/ nocontext false is not working properly");
+  OMPVV_INFOMSG_IF(errors > 0,
+                   "Dispatch is either not working or was not considered"
+                   " by the implementation as part of the context selector.");
+  return errors;
 }
 
-int main () {
-   OMPVV_TEST_OFFLOADING;
-   OMPVV_TEST_AND_SET_VERBOSE(errors, test_wrapper());
-   OMPVV_REPORT_AND_RETURN(errors);
-}  
+int main() {
+  OMPVV_TEST_OFFLOADING;
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_wrapper());
+  OMPVV_REPORT_AND_RETURN(errors);
+}
