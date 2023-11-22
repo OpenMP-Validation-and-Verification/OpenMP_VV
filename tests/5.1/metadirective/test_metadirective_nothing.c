899c9554803891988dbbd38e78efb120dba42364
Seyong Lee <lees2@ornl.gov>
2023-11-22 10:46:52 -0500

    Changed the C test to use thread-limit-var ICV and omp_get_thread_limit() API.

--- tests/5.1/metadirective/test_metadirective_nothing.c
+++ tests/5.1/metadirective/test_metadirective_nothing.c
@@ -18,30 +18,34 @@
 #include "ompvv.h"
 
 #define N 1024
+#define THREAD_LIMIT 32
 
 int metadirectiveOnDevice() {
    int errors = 0;
    int A[N];
-   int max_num_threads_target = 0;
-   int max_num_threads_parallel = 0;
+   int thread_limit_target = THREAD_LIMIT;
+   int thread_limit_teams = 0;
 
    for (int i = 0; i < N; i++) {
       A[i] = 0;
    }
-
-   #pragma omp target map(tofrom: A, max_num_threads_target, max_num_threads_parallel)
+   #pragma omp target map(from: thread_limit_target) thread_limit(thread_limit_target)
    {
-      max_num_threads_target = omp_get_max_threads();
+      thread_limit_target = omp_get_thread_limit();
+   }
+
+   #pragma omp target map(tofrom: A, thread_limit_teams) thread_limit(thread_limit_target)
+   {
       // We expect at least one of these when conditons to eval to true, thus having the nothing directive utilized
       #pragma omp metadirective \
          when( device={kind(nohost)}: nothing ) \
          when( device={arch("nvptx")}: nothing) \
          when( implementation={vendor(amd)}: nothing ) \
-         default( parallel for num_threads(max_num_threads_target+1) )
+         default( teams distribute parallel for thread_limit(thread_limit_target+1) )
             for (int i = 0; i < N; i++) {
                A[i] += omp_in_parallel();
                if( i == 0 ) {
-                 max_num_threads_parallel = omp_get_max_threads();
+                 thread_limit_teams = omp_get_thread_limit();
                }
             }
    }
@@ -49,10 +53,10 @@ int metadirectiveOnDevice() {
    for (int i = 0; i < N; i++) {
       OMPVV_TEST_AND_SET(errors, A[i] != 0);
    }
-   OMPVV_TEST_AND_SET(errors, max_num_threads_parallel != max_num_threads_target);
+   OMPVV_TEST_AND_SET(errors, thread_limit_target != thread_limit_teams);
 
    OMPVV_INFOMSG("Test ran with a number of available devices greater than 0");
-   OMPVV_INFOMSG_IF(max_num_threads_parallel == max_num_threads_target, "Test recognized device was of arch/vendor/kind nvidia, amd, or nohost");
+   OMPVV_INFOMSG_IF(thread_limit_target == thread_limit_teams, "Test recognized device was of arch/vendor/kind nvidia, amd, or nohost");
    OMPVV_WARNING_IF(A[0] == 1, "Test could not recognize if device was of arch/vendor/kind nvidia, amd or, nohost, even though there are devices available.");
 
    return errors;
@@ -61,24 +65,24 @@ int metadirectiveOnDevice() {
 int metadirectiveOnHost() {
   int errors = 0;
   int A[N];
-  int max_num_threads_initial = 0;
-  int max_num_threads_loop = 0;
+  int thread_limit_initial = THREAD_LIMIT;
+  int thread_limit_teams = 0;
 
   for (int i = 0; i < N; i++) {
      A[i] = 0;
   }
 
-  max_num_threads_initial = omp_get_max_threads();
+  thread_limit_initial = omp_get_thread_limit();
   // We expect all of these when statements to eval to false, causing body of code to run using 'nothing' as the default pragma
   #pragma omp metadirective \
-     when( device={kind(nohost)}: parallel for num_threads(max_num_threads_initial+1) ) \
-     when( device={arch("nvptx")}: parallel for num_threads(max_num_threads_initial+1) ) \
-     when( implementation={vendor(amd)}: parallel for num_threads(max_num_threads_initial+1) ) \
+     when( device={kind(nohost)}: teams distribute parallel for thread_limit(thread_limit_initial+1) ) \
+     when( device={arch("nvptx")}: teams distribute parallel for thread_limit(thread_limit_initial+1) ) \
+     when( implementation={vendor(amd)}: teams distribute parallel for thread_limit(thread_limit_initial+1) ) \
      default( nothing )
         for (int i = 0; i < N; i++) {
            A[i] += omp_in_parallel();
            if( i == 0 ) {
-              max_num_threads_loop = omp_get_max_threads();
+              thread_limit_teams = omp_get_thread_limit();
            }
         }
 
@@ -87,7 +91,7 @@ int metadirectiveOnHost() {
   for (int i = 0; i < N; i++) {
      OMPVV_TEST_AND_SET(errors, A[i] != 0);
   }
-  OMPVV_TEST_AND_SET(errors, max_num_threads_initial != max_num_threads_loop);
+  OMPVV_TEST_AND_SET(errors, thread_limit_initial != thread_limit_teams);
 
   return errors;
 } 
