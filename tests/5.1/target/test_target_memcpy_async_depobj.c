16667ef7a6905d38fe7e6f3ea2a2feb1ad8b9bf7
fel-cab <cabarcas@udel.edu>
2024-01-22 13:04:13 -0800

    Adding capture and error conditions in Fortran and C tests

--- tests/5.1/target/test_target_memcpy_async_depobj.c
+++ tests/5.1/target/test_target_memcpy_async_depobj.c
@@ -22,7 +22,7 @@ int errors, i;
 
 int test_target_memcpy_async_depobj() {
 
-    int h, t, i;
+    int h, t, i, err_r;
     errors = 0;
     double *mem;
     double *mem_dev_cpy;
@@ -32,6 +32,11 @@ int test_target_memcpy_async_depobj() {
 
     mem = (double *)malloc( sizeof(double)*N);
     mem_dev_cpy = (double *)omp_target_alloc( sizeof(double)*N, t);
+
+    OMPVV_TEST_AND_SET_VERBOSE(errors, mem == NULL);
+    if(mem == NULL) {
+       return errors;
+    }
 
     OMPVV_TEST_AND_SET_VERBOSE(errors, mem_dev_cpy == NULL);
     if(mem_dev_cpy == NULL) {
@@ -46,10 +51,16 @@ int test_target_memcpy_async_depobj() {
     omp_depend_t obj_arr[1] = {obj};
 
     /* copy to device memory */
-    omp_target_memcpy_async(mem_dev_cpy, mem, sizeof(double)*N,
+    err_r = omp_target_memcpy_async(mem_dev_cpy, mem, sizeof(double)*N,
                                 0,          0,
                                 t,          h,
                                 1,          obj_arr);
+    if(err_r) {
+       OMPVV_ERROR("omp_target_memcpy_async returns not 0");
+       free(mem);
+       omp_target_free(mem_dev_cpy, t);
+       return errors;
+    }
 
     #pragma omp taskwait depend(depobj: obj)
     #pragma omp target is_device_ptr(mem_dev_cpy) device(t) depend(depobj: obj)
