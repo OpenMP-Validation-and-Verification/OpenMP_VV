1fdce2ae9866a1c6d8c8179157bba0e265f73507
Lakhan singh Thakur <Lakhan-singh.Thakur@amd.com>
2024-01-23 16:33:00 +0530

    Addressed review comment

--- tests/4.5/target_teams_distribute_parallel_for/test_target_teams_distribute_parallel_for_dist_schedule.c
+++ tests/4.5/target_teams_distribute_parallel_for/test_target_teams_distribute_parallel_for_dist_schedule.c
@@ -13,6 +13,7 @@
 #include "ompvv.h"
 
 #define THRD 10
+#define NUM_TEAMS 2
 
 int Dist_ScheduleStatic() {
   int ErrCount = 0;
@@ -21,34 +22,42 @@ int Dist_ScheduleStatic() {
     Arr[i] = i;
   }
 
-  int TrackUsedThread[THRD];
-  for (int i = 0; i < THRD; ++i) {
-    TrackUsedThread[i] = -1;
+  int TrackUsedThread[NUM_TEAMS][THRD];
+  for (int j = 0; j < NUM_TEAMS; ++j) {
+    for (int i = 0; i < THRD; ++i) {
+      TrackUsedThread[j][i] = 0;
+    }
   }
 
-#pragma omp target teams distribute parallel for num_teams(2)\
-        thread_limit(THRD)  map(tofrom: TrackUsedThread[0:THRD])\
+#pragma omp target teams distribute parallel for num_teams(NUM_TEAMS)\
+        thread_limit(THRD)  map(tofrom: TrackUsedThread[0:NUM_TEAMS])\
         dist_schedule(static, 4)
   for (int i = 0; i < 32; ++i) {
     if (Arr[i] != i) {
 #pragma omp atomic
       ErrCount += 1;
     }
-    TrackUsedThread[omp_get_thread_num()]++;
+    TrackUsedThread[omp_get_team_num()][omp_get_thread_num()] += 1;
   }
 
-
-  for (int i = 0; i < 4; ++i) {
-    if (TrackUsedThread[i] == -1) {
-      ErrCount++;
+  // Check - 1: First four columns of each row should not be zero
+  for (int i = 0; i < NUM_TEAMS; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      if (TrackUsedThread[i][j] == 0) {
+        ErrCount++;
+      }
+    }
+  }
+  // Check - 2: column index 4 to 9 must be zero as we used dist_schedule(static, 4)
+  for (int i = 0; i < NUM_TEAMS; ++i) {
+    for (int j = 4; j < THRD; ++j) {
+      if (TrackUsedThread[i][j] != 0) {
+        ErrCount++;
+      }
     }
   }
 
-  for (int i = 4; i < THRD; ++i) {
-    if (TrackUsedThread[i] != -1) {
-      ErrCount++;
-    }
-  }
+
   return ErrCount;
 }
 
