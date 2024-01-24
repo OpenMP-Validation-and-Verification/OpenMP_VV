9ff45351434f3c781c89f4655588f31f8624a41a
Swaroop Pophale <swaroop.pophale23@gmail.com>
2024-01-24 13:28:59 -0500

    Update tests/5.1/teams/test_target_set_num_teams.c
    Co-authored-by: andrewkallai <73764771+andrewkallai@users.noreply.github.com>

--- tests/5.1/teams/test_target_set_num_teams.c
+++ tests/5.1/teams/test_target_set_num_teams.c
@@ -30,7 +30,7 @@ int main() {
 	}               
 	
 	OMPVV_ERROR_IF(num_teams <= 4, "Number of teams detected was not the number set by omp_set_num_teams()");
-	OMPVV_TEST_AND_SET(errors, num_teams != 4);
+	OMPVV_TEST_AND_SET(errors, num_teams <= 4);
 
 	#pragma omp teams num_teams(OMPVV_NUM_TEAMS_HOST)
 	{
