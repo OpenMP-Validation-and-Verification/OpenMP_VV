a7baf36cf550b368196742b2589a80b717bb0426
Lakhan singh Thakur <Lakhan-singh.Thakur@amd.com>
2024-01-25 14:28:20 +0530

    Addressed review comment...

--- tests/4.5/target_teams_distribute_parallel_for/test_target_teams_distribute_parallel_for_dist_schedule.c
+++ tests/4.5/target_teams_distribute_parallel_for/test_target_teams_distribute_parallel_for_dist_schedule.c
@@ -18,41 +18,20 @@
 int Dist_ScheduleStatic() {
   int ErrCount = 0;
 int num_teams, num_threads;
-  int TrackUsedThread[NUM_TEAMS][THRD];
-  for (int j = 0; j < NUM_TEAMS; ++j) {
-    for (int i = 0; i < THRD; ++i) {
-      TrackUsedThread[j][i] = 0;
-    }
-  }
 
 #pragma omp target teams distribute parallel for num_teams(NUM_TEAMS)\
-        thread_limit(THRD)  map(tofrom: TrackUsedThread[0:NUM_TEAMS], num_teams, num_threads)\
+        thread_limit(THRD)  map(tofrom: num_teams, num_threads)\
         dist_schedule(static, 4)
   for (int i = 0; i < 32; ++i) {
-    TrackUsedThread[omp_get_team_num()][omp_get_thread_num()] += 1;
-if(omp_get_team_num()==0 && omp_get_thread_num() == 0){
-  num_teams = omp_get_num_teams();
-  num_threads = omp_get_num_threads();
-  }
-    }
-
-  // Check - 1: First four columns of each row should not be zero
-  for (int i = 0; i < NUM_TEAMS; ++i) {
-    for (int j = 0; j < 4; ++j) {
-      if (TrackUsedThread[i][j] == 0) {
-        ErrCount++;
-      }
-    }
-  }
-  // Check - 2: column index 4 to 9 must be zero as we used dist_schedule(static, 4)
-  for (int i = 0; i < NUM_TEAMS; ++i) {
-    for (int j = 4; j < THRD; ++j) {
-      if (TrackUsedThread[i][j] != 0) {
-        ErrCount++;
-      }
+    if(omp_get_team_num()==0 && omp_get_thread_num() == 0){
+      num_teams = omp_get_num_teams();
+      num_threads = omp_get_num_threads();
     }
   }
 
+  if ((num_teams != 2) || (num_threads != 10)) {
+    ErrCount += 1;
+  }
 
   return ErrCount;
 }
