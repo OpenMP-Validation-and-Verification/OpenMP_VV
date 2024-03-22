9f2eabcbefb058134f13a5133268dd3a41ca63c2
shivapdr <shivaprasad.dr@amd.com>
2024-03-22 16:04:06 +0530

    added test_taskloop_final.c, test_taskloop_private.c and test_taskloop_num_tasks.c

---
+++ tests/4.5/taskloop/test_taskloop_num_tasks.c
@@ -0,0 +1,76 @@
+//------------------------------------------- test_taskloop_num_tasks.c --------------------------------------//
+//
+// OpenMP API Version 4.5 September 2015
+//
+// This test checks the 'taskloop' directive with the 'num_tasks' clause specified.
+// The 'taskloop' construct parallelizes loops with independent iterations by creating tasks. 
+// It allows for efficient parallel execution of loop iterations by distributing them among multiple threads. 
+// The 'num_tasks' clause variable ensures that the loop iterations are shared amoung created 'num_tasks'. 
+// i.e the loop iterations are divided amoung number of tasks (each num_task group is run by same the thread).   
+//----------------------------------------------------------------------------------------------------------//
+
+#include <stdio.h>
+#include <omp.h>
+#include "ompvv.h"
+
+#define NUM_THREADS 100
+
+#define NUM_TASKS 6
+#define NUM_ITERATIONS 12
+
+int isGroupIdsSame(int thread_ids[])
+{
+        int iterationsPerGroup = NUM_ITERATIONS / NUM_TASKS;
+
+        for(int i = 0; i < NUM_ITERATIONS; i = i+iterationsPerGroup)
+        {
+                for(int j = 0; j<iterationsPerGroup; j++) {
+                        if (thread_ids[i+j] != thread_ids[i]) {
+                                return 0; // Return false if any id is different in a group
+                        }
+                }
+        }
+
+        return 1; // Return true if all id's are same per group
+}
+
+int test_taskloop_num_tasks() {
+
+   int errors = 0;
+
+   long int var = 0;
+
+   int thread_ids[NUM_THREADS];
+
+   #pragma omp parallel num_threads(NUM_THREADS)
+   {
+      #pragma omp single
+      {
+        #pragma omp taskloop num_tasks(NUM_TASKS)
+        for(int i = 0; i < NUM_ITERATIONS; i++)
+        {
+	    #pragma omp atomic 
+	    var = var + i;
+
+            thread_ids[i] = omp_get_thread_num();
+        }
+      }
+   }
+
+   //if all the tasks in a group are run by a same thread, get TRUE else FALSE
+   OMPVV_TEST_AND_SET_VERBOSE(errors, (isGroupIdsSame(thread_ids) != 1));
+   
+   OMPVV_TEST_AND_SET_VERBOSE(errors, var != ((NUM_ITERATIONS-1)*(NUM_ITERATIONS)/2));
+
+   return errors;
+}
+
+int main() {
+  
+  int errors = 0;
+  
+  OMPVV_TEST_AND_SET_VERBOSE(errors, (test_taskloop_num_tasks()) != 0);
+  OMPVV_REPORT_AND_RETURN(errors);
+
+  return 0;
+}
---
+++ tests/4.5/taskloop/test_taskloop_private.c
@@ -0,0 +1,52 @@
+//---------------------------------------- test_taskloop_private.c -----------------------------------------//
+//
+// OpenMP API Version 4.5 August 2015
+//
+// This test checks the taskloop directive with the 'private' clause specified.
+// The 'taskloop' construct parallelizes loops with independent iterations by creating tasks. 
+// It allows for efficient parallel execution of loop iterations by distributing them among multiple threads. 
+// The 'private' clause ensures that each task should have private copies. 
+//----------------------------------------------------------------------------------------------------------//
+
+#include <stdio.h>
+#include <omp.h>
+#include "ompvv.h"
+
+#define NUM_THREADS 100
+#define NUM_TASKS 1000
+
+int test_taskloop_private() {
+
+  int errors = 0;
+
+  int private_var_sum;  
+  int val_not_in_openmp = 10; 
+  
+   #pragma omp parallel num_threads(NUM_THREADS)
+   {
+      #pragma omp single
+      {
+	#pragma omp taskloop private(private_var_sum) 
+        for(int i = 0; i < NUM_TASKS; i++) {
+	  private_var_sum = i;
+	 }      
+      }	   
+   }
+  
+   //check if the private variable is same before and after openmp region. i.e it should be same
+   OMPVV_TEST_AND_SET_VERBOSE(errors, val_not_in_openmp != private_var_sum);
+   
+   return errors;
+}
+
+
+int main() {
+  
+  int errors = 0;
+  
+  OMPVV_TEST_AND_SET_VERBOSE(errors, (test_taskloop_private()) != 0);
+  OMPVV_REPORT_AND_RETURN(errors);
+
+  return 0;
+}
+
