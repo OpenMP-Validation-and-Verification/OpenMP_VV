77d3f8bbf2a98d1d7525e2880821800eb5ac4841
Swaroop Pophale <swaroop.pophale23@gmail.com>
2024-03-28 13:57:19 -0400

    Update test_taskloop_private.c
    Edits to testing.

--- tests/4.5/taskloop/test_taskloop_private.c
+++ tests/4.5/taskloop/test_taskloop_private.c
@@ -13,28 +13,34 @@
 #include "ompvv.h"
 
 #define NUM_THREADS 100
-#define NUM_TASKS 1000
+#define NUM_TASKS 100
 
 int test_taskloop_private() {
 
   int errors = 0;
 
-  int private_var_sum;  
-  int val_not_in_openmp = 10; 
+  int private_var = -2; 
+  int shared_var_sum = 0;	
+  int num_threads = 0;
   
    #pragma omp parallel num_threads(NUM_THREADS)
    {
+      num_threads = omp_get_num_threads();
       #pragma omp single
       {
-	#pragma omp taskloop private(private_var_sum) 
+	#pragma omp taskloop private(private_var) 
         for(int i = 0; i < NUM_TASKS; i++) {
-	  private_var_sum = i;
+	  private_var = omp_get_thread_num();
+          #pragma omp atomic
+	    shared_var_sum += private_var;
 	 }      
       }	   
    }
   
    //check if the private variable is same before and after openmp region. i.e it should be same
-   OMPVV_TEST_AND_SET_VERBOSE(errors, val_not_in_openmp != private_var_sum);
+   OMPVV_TEST_AND_SET_VERBOSE(errors, private_var != -2);
+   if(num_threads == 100){
+     OMPVV_TEST_AND_SET_VERBOSE(errors, shared_var_sum != NUM_THREADS * ((NUM_THREADS + 1)/2) );
    
    return errors;
 }
