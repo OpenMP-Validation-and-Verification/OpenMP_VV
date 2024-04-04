55e63b2f46cf7995b8b0b8651aa47e95a0f3377a
mjcarr458 <54590896+mjcarr458@users.noreply.github.com>
2024-04-04 14:05:45 -0400

    map virtual function test (#702)
    * map virtual function test
    
    * adding virtual function map test
    
    * adding description
    
    * updating test to map class into target region. Illegal memory access error
    
    * Preventing illegall memory access by having variable initializations happen only on device
    
    * Update test_virtual_function_map.cpp
    
    minor edits
    
    * Spelling fix for map clause: tofrom
    
    ---------
    
    Co-authored-by: Andrew Kallai <kallaiandrew@gmail.com>
    Co-authored-by: Swaroop Pophale <swaroop.pophale23@gmail.com>

---
+++ tests/5.1/target/test_virtual_function_map.cpp
@@ -0,0 +1,59 @@
+//-----------test_map_virtual_function.cpp------------------
+//
+// OpenMP API Version 5.1 Mar 2023
+//
+//This test is designed to ensure that virtual functions 
+//are correctly passed into a target region. The class base
+//should use the derived classes test function instead of 
+//the virtual one declared in the base class
+//----------------------------------------------------------
+
+
+#include <iostream>
+#include <omp.h>
+#include "ompvv.h"
+
+using namespace std;
+
+#pragma omp begin declare target
+class base {
+public:
+	virtual int test()
+	{
+		return 1;
+	}
+
+};
+#pragma omp end declare target
+
+#pragma omp begin declare target
+class derived : public base {
+	public: 
+		int test()
+		{
+			return 2;
+		}
+
+};
+#pragma omp end declare target
+
+int test_case(){
+    base *bptr;
+    derived d;
+    bptr = &d;
+    int test_val = 0;
+
+    #pragma omp target map(tofrom: test_val)
+    {
+	test_val = bptr->test();
+    }
+	return test_val;
+}
+
+int main(){
+	int errors = 0;
+	OMPVV_TEST_OFFLOADING;
+	OMPVV_TEST_AND_SET_VERBOSE(errors, test_case() != 2);
+	OMPVV_REPORT_AND_RETURN(errors)
+	return errors;
+}
