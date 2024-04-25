36bce1f11a8ac053fe63be9a09928c66248c8da6
Swaroop Pophale <swaroop.pophale23@gmail.com>
2024-04-25 13:45:31 -0400

    Update test_parallel_sections.c
    Removing pthread calls and adding omp atomics.

--- tests/4.5/parallel_sections/test_parallel_sections.c
+++ tests/4.5/parallel_sections/test_parallel_sections.c
@@ -12,24 +12,22 @@
 #include <unistd.h>
 #include <omp.h>
 #include "ompvv.h"
-#include <pthread.h>
 
-pthread_mutex_t Mutex;
 int Var = 0;
 
 // The following function waits for Var value to become 1 and then increments
 // it to Var = 1
 void function1(int *Var) {
   while(1) {
-    pthread_mutex_lock(&Mutex);
-    if (*Var == 1) {
+   #pragma omp atomic read
+    int temp = *Var
+    
+    if (temp == 1) {
+      #pragma omp atomic write
       *Var += 1;
-      pthread_mutex_unlock(&Mutex);
       break;
     }
-  pthread_mutex_unlock(&Mutex);
   }
-  pthread_mutex_unlock(&Mutex);
 }
 
 // The following function check if Var == 0 and then increments to Var = 1
@@ -37,59 +35,65 @@ void function1(int *Var) {
 // Var = 4
 void function2(int *Var) {
   while(1) {
-    pthread_mutex_lock(&Mutex);
-    if (*Var == 0) {
+    #pragma omp atomic read
+    int temp = *Var
+    
+    if (temp == 0) {
+      #pragma omp atomic write
       *Var += 1;
-      pthread_mutex_unlock(&Mutex);
       break;
     }
-  pthread_mutex_unlock(&Mutex);
   }
-  pthread_mutex_unlock(&Mutex);
 
   while(1) {
-    pthread_mutex_lock(&Mutex);
-    if (*Var == 3) {
+    #pragma omp atomic read
+    int temp = *Var
+    
+    if (temp == 3) {
+      #pragma omp atomic write
       *Var += 1;
-      pthread_mutex_unlock(&Mutex);
       break;
     }
-  pthread_mutex_unlock(&Mutex);
   }
-  pthread_mutex_unlock(&Mutex);
 }
 
 // The following function checks if Var == 2 and then increments to Var = 3
 void function3(int *Var) {
   while(1) {
-    pthread_mutex_lock(&Mutex);
+    #pragma omp atomic read
+    int temp = *Var
+    
     if (*Var == 2) {
+      #pragma omp atomic write
       *Var += 1;
-      pthread_mutex_unlock(&Mutex);
       break;
     }
-  pthread_mutex_unlock(&Mutex);
   }
-  pthread_mutex_unlock(&Mutex);
 }
 
 
 int main() {
-
-  pthread_mutex_init(&Mutex, NULL);
+  int errors = 0;
+  
 #pragma omp parallel sections
   {
-#pragma omp section
+    if(omp_num_threads() == 1) 
+    {
+      OMPVV_WARNING("Sections are executed by a single thread, test cannot execute.")
+      exit 0;
+    }
+      
+    #pragma omp section
     function1(&Var);
 
-#pragma omp section
+    #pragma omp section
     function2(&Var);
 
-#pragma omp section
+    #pragma omp section
     function3(&Var);
   }
-  pthread_mutex_destroy(&Mutex);
-  int errors = 0;
+ 
+  
   // The final expected value of Var is '4'
   OMPVV_TEST_AND_SET_VERBOSE(errors, (Var != 4));
   OMPVV_REPORT_AND_RETURN(errors);
