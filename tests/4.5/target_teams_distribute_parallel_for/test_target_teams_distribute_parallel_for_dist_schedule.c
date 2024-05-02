1a7294cd5e07b5dd021cf20c62205906a7a17e1d
Swaroop Pophale <swaroop.pophale23@gmail.com>
2024-05-02 13:33:07 -0400

    Update test_target_teams_distribute_parallel_for_dist_schedule.c
    Modified to include testing of chunks associated with each thread.

--- tests/4.5/target_teams_distribute_parallel_for/test_target_teams_distribute_parallel_for_dist_schedule.c
+++ tests/4.5/target_teams_distribute_parallel_for/test_target_teams_distribute_parallel_for_dist_schedule.c
@@ -12,25 +12,40 @@
 #include <stdlib.h>
 #include "ompvv.h"
 
-#define THRD 10
-#define NUM_TEAMS 2
+int arr[32] = {0};
+int chunked = 1;
 
 int Dist_ScheduleStatic() {
   int ErrCount = 0;
 int num_teams, num_threads;
 
-#pragma omp target teams distribute parallel for num_teams(NUM_TEAMS)\
-        thread_limit(THRD)  map(tofrom: num_teams, num_threads)\
+#pragma omp target teams distribute parallel for num_teams(OMPVV_NUM_TEAMS_DEVICE)\
+        thread_limit(OMPVV_NUM_THREADS_DEVICE)  map(tofrom: num_teams, num_threads)\
         dist_schedule(static, 4)
   for (int i = 0; i < 32; ++i) {
     if(omp_get_team_num()==0 && omp_get_thread_num() == 0){
       num_teams = omp_get_num_teams();
       num_threads = omp_get_num_threads();
+      arr[i] = omp_get_thread_num();
     }
   }
 
-  if ((num_teams != 2) || (num_threads != 10)) {
+  if ((num_teams > OMPVV_NUM_TEAMS_DEVICE) || (num_threads > OMPVV_NUM_THREADS_DEVICE)) {
     ErrCount += 1;
+  }
+  
+  for(int i=0; i<32; i+=4){
+    temp = arr[i];
+    for(int j=0; j< 4;j++)
+      if(arr[i+j] != temp){
+        chunked = 0;
+        break;
+      }
+    if(chunked == 0) {
+      ErrCount++;
+      OMPVV_WARNING("The given iterations were not scheduled according to the static chunks requested");
+      break;
+    }
   }
 
   return ErrCount;
