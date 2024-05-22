5cab9442cf0155a510d7504911a4d8b2d0871da6
Swaroop Pophale <pophaless@ornl.gov>
2024-05-22 13:31:27 -0400

    Resolving issue 817.

--- tests/5.0/target_update/test_target_update_mapper_from_discontiguous.c
+++ tests/5.0/target_update/test_target_update_mapper_from_discontiguous.c
@@ -14,17 +14,16 @@
 #include <stdlib.h>
 #include "ompvv.h"
 
-#define N 1024
+#define N 10
 
 typedef struct{
   size_t len;
   double *data;
 } T;
 
-int i;
 int errors = 0;
 
-#pragma omp declare mapper(S: T v) map(to: v, v.len, v.data[0:v.len])
+#pragma omp declare mapper(custom: T v) map(to: v, v.len, v.data[0:v.len])
 
 int target_update_from_mapper() {
   
@@ -34,30 +33,24 @@ int target_update_from_mapper() {
   s.data = (double *)calloc(N,sizeof(double));
   
   
-  #pragma omp target  
+   #pragma omp target data map(mapper(custom), to:s) 
   { 
-    for (int i = 0; i < s.len; i++) {
-      s.data[i] = i;
-    }
-  }//end target
+    #pragma omp target
+    { 
+      for (int i = 0; i < s.len; i++) {
+        s.data[i] = 10;
+      }
+    }//end target
   
-  #pragma omp target data map(mapper(S), to:s) 
-  { 
-    #pragma omp target map(mapper(S), to:s)
-    for (int i = 0; i < s.len; i++) {       
-      s.data[i] += i ;
-    }
-  
-  #pragma omp target update from(s.data[0:s.len/2:2]) //only update even array elements
-  } 
-  
+  #pragma omp target update from(s.data[:N/2:2]) //only update even array elements
+  }//end-target-data 
 
-  for (i =0; i < s.len; i++) {
+  for (int i =0; i < s.len; i++) {
     if(i%2){
-      OMPVV_TEST_AND_SET(errors, s.data[i] != i);
+      OMPVV_TEST_AND_SET(errors, s.data[i] != 0.00);
     }
     else{
-      OMPVV_TEST_AND_SET(errors, s.data[i] != 2 * i);
+      OMPVV_TEST_AND_SET(errors, s.data[i] != 10);
     }
   }
 
