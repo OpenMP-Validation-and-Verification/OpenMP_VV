6257d00c61eb7528f4b73f1dd1b96470cff622e8
Raymundo Escobar <rjen@udel.edu>
2024-06-17 20:44:59 +0000

    Tests to check lvalue expressions in depend clause

---
+++ tests/5.0/task/test_task_depend_lvalue_ptr.cpp
@@ -0,0 +1,61 @@
+//===--- test_task_depend_lvalue_ptr.cpp -------------------------------------===//
+//
+// OpenMP API Version 5.0 Nov 2018
+//
+// This test checks the depend clause on task generating constructs are extended to
+// allow any lvalue such as dereferenced pointers.
+//
+////===----------------------------------------------------------------------===//
+
+#include <omp.h>
+#include <iostream>
+#include <cstdlib>
+#include "ompvv.h"
+
+int test_task_depend_lvalue_ptr() {
+	int errors = 0;
+	int *ptr = new int;
+	int value;
+
+	// Initialize the pointer to -1
+	*ptr = -1;
+
+	#pragma omp parallel
+	{
+		#pragma omp single
+		{
+			#pragma omp task depend(out: *ptr)
+			{
+				*ptr = 1;
+			}
+
+			#pragma omp task depend(in: *ptr)
+			{
+				value = *ptr;
+			}
+			#pragma omp taskwait
+		}
+	}
+
+	if (value != 1) {
+		errors++;
+	}
+	
+	delete ptr;
+	
+	OMPVV_TEST_AND_SET(errors, errors != 0);
+	
+	return errors;
+}
+
+int main() {
+	OMPVV_TEST_OFFLOADING;
+	int errors = 0;
+
+	OMPVV_TEST_AND_SET_VERBOSE(errors, test_task_depend_lvalue_ptr() != 0);
+
+	OMPVV_REPORT_AND_RETURN(errors);
+
+	return errors;
+}
+
---
+++ tests/5.0/task/test_task_depend_lvalue_ternary.cpp
@@ -0,0 +1,67 @@
+//===--- test_task_depend_lvalue_ternary.cpp ---------------------------------===//
+//
+// OpenMP API Version 5.0 Nov 2018
+//
+// This test checks the depend clause on task generating constructs are extended to
+// allow any lvalue such as ternary operators.
+//
+////===----------------------------------------------------------------------===//
+
+#include <omp.h>
+#include <iostream>
+#include <cstdlib>
+#include "ompvv.h"
+
+int test_task_depend_ternary() {
+	int errors = 0;
+	int a = 1;
+	int b = 0;
+	int c = -1;
+	int value;
+
+	#pragma omp parallel
+	{
+		#pragma omp single
+		{
+			// Task should run first and set b = 1
+			#pragma omp task depend(out: (a > b) ? b : c) shared(a,b)
+			{
+				if ( a > b) {
+					b = 1;
+				}
+				else {
+					c = 1;
+				}
+			}
+
+			// Task should compute (a > b) ? b : c -> this will return
+			// false. It should set value = c => -1
+			#pragma omp task depend(in: (a > b) ? b : c) shared(a, b)
+			{
+				value = (a > b) ? b : c;
+			}
+
+			#pragma omp taskwait
+		}
+	}
+	
+	// Check to make sure value was correctly set to -1
+	if (value != -1) {
+		errors++;
+	}
+	
+	OMPVV_TEST_AND_SET(errors, errors != 0);
+	return errors;
+}
+
+int main() {
+	OMPVV_TEST_OFFLOADING;
+	int errors = 0;
+
+	OMPVV_TEST_AND_SET_VERBOSE(errors, test_task_depend_ternary() != 0);
+
+	OMPVV_REPORT_AND_RETURN(errors);
+
+	return errors;
+}
+
