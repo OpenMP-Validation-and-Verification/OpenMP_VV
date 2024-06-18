3fe0f0ac20a087709ebd171db06d7d40ac164644
Raymundo Escobar <rjen@udel.edu>
2024-06-18 19:42:41 +0000

    Added map clause

---
+++ tests/5.0/target/test_target_update_to_from_map_lvalue_func.cpp
@@ -0,0 +1,80 @@
+//===----------- test_target_update_to_from_map_lvalue_func.cpp ----------------===//
+//
+// OpenMP API Version 5.0 Nov 2018
+//
+// This test evaluates the target update directive using to and from clauses, 
+// supporting various lvalue expressions, such as functions that return references.
+//  Additionally, it examines the map clause and its handling of lvalue expressions. 
+//===--------------------------------------------------------------------------===//
+#include <omp.h>
+#include <iostream>
+#include "ompvv.h"
+
+//Global Variable
+int var;
+
+
+//Map the variable to the device using the function
+#pragma omp declare target to(var)
+
+
+//Function that will return a reference to the variable var
+int& returnRef() {
+	return var;
+}
+
+
+
+int test_target_update(){
+	int errors = 0;
+	int before_value;
+	int after_value;
+
+	// Initialize var
+	var = -1;
+
+	//Set var to the initial value of before_value from the device
+	#pragma omp target map(from: before_value) map(returnRef())
+	{
+		before_value = returnRef();
+	}
+
+	//Update the value of var on the host
+	var = 1;
+
+	//Update the device with the new value of var from the host
+	#pragma omp target update to(returnRef())
+
+	//Set after_value with the update value of var and update var on the device
+	#pragma omp target map(from: after_value)
+	{
+		after_value = returnRef();
+		returnRef() = 2;
+	}
+
+	//Update the host with the new value of var form the device
+	#pragma omp target update from(returnRef())
+
+	//Check for Errors
+	if (var != 2){
+		errors++;
+	}
+	if (before_value != -1){
+		errors++;
+	}
+	if (after_value != 1){
+		errors++;
+	}
+
+	OMPVV_TEST_AND_SET(errors, errors != 0);
+
+	return errors;
+}
+
+	int main() {
+		OMPVV_TEST_OFFLOADING;
+		int errors = 0;
+
+		OMPVV_TEST_AND_SET_VERBOSE(errors, test_target_update() != 0);
+		OMPVV_REPORT_AND_RETURN(errors);
+	}
