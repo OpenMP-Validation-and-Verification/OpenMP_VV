2ffa23527458ff477cb62738fcdbe4bbd0ccc5b4
Raymundo Escobar <rjen@udel.edu>
2024-06-20 19:54:46 +0000

    Created Initial test for lastprivate and conditional

---
+++ tests/5.0/parallel_for_simd/test_parallel_for_simd_lastprivate_conditional.c
@@ -0,0 +1,45 @@
+//===------ test_parallel_for_simd_lastprivate_conditional.c ------===//
+//
+// OpenMP API Version 5.0 Nov 2018
+//
+// This test evaluates the lastprivate directive using the conditional clause,
+// ensuring proper behavior and variable updates in parallel regions.
+//===-------------------------------------------------------------===//
+
+#include <omp.h>
+#include <stdio.h>
+#include "ompvv.h"
+
+#define N 1024
+
+int test_lastprivate_conditional() {
+        int errors = 0;
+        int a[N];
+        int x = 0;
+
+        #pragma omp parallel for simd lastprivate(conditional: x)
+        for (int k = 0; k < N; k++) {
+                a[k] = k;
+                if (k == 24) {
+                        x = a[k];
+		}
+        }
+
+        // Check to make sure x is set to 24.
+        if (x != 24) {
+                errors++;
+        }
+
+        OMPVV_TEST_AND_SET(errors, errors != 0);
+        return errors;
+}
+
+int main() {
+        OMPVV_TEST_OFFLOADING;
+        int errors = 0;
+
+        OMPVV_TEST_AND_SET_VERBOSE(errors, test_lastprivate_conditional() != 0);
+
+        OMPVV_REPORT_AND_RETURN(errors);
+}
+
