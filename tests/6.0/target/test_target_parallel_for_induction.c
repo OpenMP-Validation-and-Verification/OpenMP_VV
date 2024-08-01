38f8fc731878b2e0046511b4b861ab914806d844
rjenaa <rjen@udel.edu>
2024-08-01 10:08:19 -0700

    Initial test for induction clause

---
+++ tests/6.0/target/test_target_parallel_for_induction.c
@@ -0,0 +1,45 @@
+//===------ test_target_parallel_for_induction.c ------------------===//
+//
+// OpenMP API Version 6.0
+//
+// This test evaluates the parallel for directives using the induction
+// clause, ensuring proper behavior and proper updates
+//
+//===-------------------------------------------------------------===//
+
+#include <omp.h>
+#include <stdio.h>
+#include "ompvv.h"
+
+#define N 1024
+
+int test_parallel_for_induction() {
+        int errors = 0;
+        int arr[N];
+	int x;
+	// Initialize array
+	for (int i = 0; i < N; i++){
+		arr[i] = i;
+	}
+
+        #pragma omp target parallel for induction(x : int) map(tofrom: a[0:N])
+        for (int i = 0; i < N; i++) {
+		x = i;
+                arr[i] += x;
+        }
+
+	for (int i = 0; i < N; i++){
+		OMPVV_TEST_AND_SET(errors, arr[N] != i*2);
+	}
+
+        return errors;
+}
+
+int main() {
+        OMPVV_TEST_OFFLOADING;
+        int errors = 0;
+
+        OMPVV_TEST_AND_SET_VERBOSE(errors, test_parallel_for_induction() != 0);
+
+        OMPVV_REPORT_AND_RETURN(errors);
+}
