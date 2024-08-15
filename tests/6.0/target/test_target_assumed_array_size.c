54fd2434cb4b4a8521691602effaac99a35ae01d
rjenaa <rjen@udel.edu>
2024-08-15 08:24:08 -0700

    Modified the array sections

--- tests/6.0/target/test_target_assumed_array_size.c
+++ tests/6.0/target/test_target_assumed_array_size.c
@@ -1,7 +1,7 @@
 //===--- test_target_assumed_array_size.c ------------------------------------===//
 //
 // OpenMP API Version 6.0
-//
+// Tests the target directive with the map clause on an assumed-size array.  
 //
 ////===----------------------------------------------------------------------===//
 
@@ -12,32 +12,27 @@
 
 #define N 1024
 
-#pragma omp begin declare target
-
-void update_array(int* array){
-
-	#pragma omp target parallel map(tofrom: array[:N])
-	for (int i = 0; i < N; i++){
-		array[i] = i * 2;
-	}
-
-}
-
-#pragma omp end declare target
-
 int test_assumed_array_size(){
 	int errors = 0;
-	int *array = (int *)malloc(N * sizeof(int));
+	int array[N];
+	int *pointer;
 
 	for (int i = 0; i < N; i++){
-		array[i] = i;
+		array[i] = 0;
 	}
 	
-	update_array(array);
-	for(int i = 0; i < N; i++){
-		OMPVV_TEST_AND_SET(errors, array[i] != i * 2);
+	pointer = &array[0];
+ 	#pragma omp target data map(tofrom: array[0:N])	
+	#pragma omp target parallel for map(pointer[:])
+	for (int i = 0; i < N; i++){
+		*pointer[i] = i;
 	}
-	free(array);
+
+
+	for (int i = 0; i < N; i++){
+		OMPVV_TEST_AND_SET_VERBOSE(errors, array[i] != i);
+	}
+
 	return errors;
 }
 
