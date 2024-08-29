a1b2222615adf77f03ba3b3d35b696f05683043f
rjenaa <rjen@udel.edu>
2024-08-29 09:45:56 -0700

    Changed inductor to expression

--- tests/6.0/target/test_target_declare_induction.c
+++ tests/6.0/target/test_target_declare_induction.c
@@ -12,33 +12,29 @@
 #include "ompvv.h"
 #include <math.h>
 
-#define N 16
+#define N 4
 
-#pragma omp declare target 
-int inductor_func(int inductor_var, int step) {
-	return inductor_var * 2 * step;
-}
-#pragma omp end declare target
 int test_target_declare_induction() {
         int errors = 0;
         int arr[N];
 	int step_var = 2;
 	int induction_var = 1;
-	
+	int expected = induction_var;	
 	for (int i = 0; i < N; i++){
 		arr[i] = 0;
 	}
 
-	#pragma declare induction(triple: int) inductor(omp_var=inductor_func(omp_var, omp_step) collector(omp_out)
+	#pragma declare induction(user_def_induc: int) inductor(omp_var=omp_var * 2 * omp_step) collector(pow(2 * omp_step, omp_idx))
 
-        #pragma omp target parallel for induction(step(step_var), triple : induction_var) map(tofrom: a[:N])
+        #pragma omp target parallel for induction(step(step_var), user_def_induc : induction_var) map(tofrom: a[:N])
         for (int i = 0; i < N; i++) {
                 arr[i] = induction_var;
         }
 
-	for (int i = 0; i < N; i++){
-		OMPVV_TEST_AND_SET(errors, arr[i] != induction_var * 2 * step_var);
-	}
+	for (int i = 0; i < N; i++) {
+        	expected *= 2 * step_var;  
+        	OMPVV_TEST_AND_SET(errors, arr[i] != expected);
+    	}
 
         return errors;
 }
