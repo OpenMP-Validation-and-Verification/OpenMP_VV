4509c5e0773ea1bb329ba78287fcfa35e7da54b3
rjenaa <rjen@udel.edu>
2024-09-12 20:56:37 -0700

    Initial test for the interchange construct

---
+++ tests/6.0/target/test_target_interchange.c
@@ -0,0 +1,54 @@
+//===------ test_target_interchange.c ----------------===//
+//
+// OpenMP API Version 6.0
+//
+// This test evaluates the interchagne directive, ensuring
+// proper behavior.
+//
+//===--------------------------------------------===//
+
+#include <omp.h>
+#include <stdio.h>
+#include "ompvv.h"
+
+#define N 10
+#define M 10
+
+int test_target_interchange() {
+        int errors = 0;
+        int arrayDevice[N][M];
+	int arrayHost[N][M];
+
+	for (int i = 0; i < N; i++){
+	  for (int j = 0; j < M; j++){
+            arrayHost[i][j] = i * j;
+	    arrayDevice[i][j] = 0;
+	  }
+	}
+        
+	#pragma omp target map(tofrom: arrayDevice)
+        {
+	  #pragma omp interchange
+          for (int i = 0; i < N; i++) {
+  	    for(int j = 0; j < M; j++){
+	      arrayDevice[i][j] = i*j;
+	    }
+	  }
+	}
+
+        for (int i = 0; i < N; i++){
+	  for(int j = 0; j < M; j++){
+	    OMPVV_TEST_AND_SET(errors, arrayHost[i][j] != arrayDevice[i][j]);
+	  }
+	}
+        return errors;
+}
+
+int main() {
+        OMPVV_TEST_OFFLOADING;
+        int errors = 0;
+
+        OMPVV_TEST_AND_SET_VERBOSE(errors, test_target_interchange() != 0);
+
+        OMPVV_REPORT_AND_RETURN(errors);
+}
