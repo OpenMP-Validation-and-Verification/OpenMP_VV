2f41a6b1bfedab081b420e8b419a9d7e03bdbcf5
Andrew Kallai <kallaiandrew@gmail.com>
2024-10-03 13:45:18 -0400

    commit

--- tests/6.0/fuse/test_fuse_looprange.c
+++ tests/6.0/fuse/test_fuse_looprange.c
@@ -1,6 +1,6 @@
-//--------------- test_parallel_severity.c------------------------------------//
+//--------------- test_fuse_looprange.c------------------------------------//
 // OpenMP API Version 6.0 August 2024
-// Pg. 870, line 25
+// Pg. 868, line 16
 // ***********
 // DIRECTIVE:fuse
 // CLAUSE:looprange
@@ -12,33 +12,34 @@
 // be non-zero.
 //----------------------------------------------------------------------------//
 #include "ompvv.h"
-//#include <omp.h>
 
 #define N 10
-int test_severity() {
+int test_looprange() {
   int return_value = 1;
   int A[N][N] = {0};
+  int sum = 0;
+
   #pragma omp fuse looprange(1, 1)
   for (int i = 0; i < N; ++i){
     for (int j = 0; j < N; ++j){
-      A[i][j] = i*(N) + j + 1
+      A[i][j] = i*(N) + j + 1;
     }
   }
 
   for (int i = 0; i < N; ++i){
     for (int j = 0; j < N; ++j){
-      check_sum += A[i][j];
+      sum += A[i][j];
     }
   }
 
-  if (check_sum != (N*N*(N*N-1))/2)
+  if (sum != (N*N*(N*N-1))/2)
     return 1;
   return 0;
 }
 
 int main() {
   int errors = 0;
-  OMPVV_TEST_AND_SET(errors, test_severity() != 0);
+  OMPVV_TEST_AND_SET(errors, test_looprange() != 0);
   OMPVV_REPORT_AND_RETURN(errors);
   return errors;
 }
