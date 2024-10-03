3715a5d0a260f99ecaa6c43f39afeea33e7c6fa4
Andrew Kallai <kallaiandrew@gmail.com>
2024-10-03 15:43:01 -0400

    Changed test to perform a simple loop merge with looprange.

--- tests/6.0/fuse/test_fuse_looprange.c
+++ tests/6.0/fuse/test_fuse_looprange.c
@@ -1,40 +1,44 @@
-//--------------- test_fuse_looprange.c------------------------------------//
+//--------------- test_fuse_looprange.c---------------------------------------//
 // OpenMP API Version 6.0 August 2024
 // Pg. 868, line 16
 // ***********
 // DIRECTIVE:fuse
 // CLAUSE:looprange
 // ***********
-// The severity clause for the parallel directive is being tested. If the clause
-// executes correctly, than the user will see a message corresponding to the top
-// parallel region. Otherwise, the user should notice a message corresponding to
-// the second region. Additionally, the return value of the test function should
-// be non-zero.
+// The looprange clause for the fuse directive is being tested. The clause takes
+// the 4 loops and merges every 2 loops (right count) so that the total number
+// of loops is 2 (left count). The entries of the 4 * N matrix will be from 1 to
+// 4 * N. If an entry is not entered correctly, then errors will be incremented.
 //----------------------------------------------------------------------------//
 #include "ompvv.h"
 
-#define N 10
+#define N 4
+
 int test_looprange() {
-  int return_value = 1;
-  int A[N][N] = {0};
-  int sum = 0;
+  int A[4][N] = {0};
+  int errors = 0;
+  int j;
 
-  #pragma omp fuse looprange(1, 1)
-  for (int i = 0; i < N; ++i){
-    for (int j = 0; j < N; ++j){
-      A[i][j] = i*(N) + j + 1;
+#pragma omp fuse looprange(2, 2)
+  {
+    for (j = 0; j < N; ++j)
+      A[0][j] = j + 1;
+    for (j = 0; j < N; ++j)
+      A[1][j] = N + j + 1;
+    for (j = 0; j < N; ++j)
+      A[2][j] = 2 * N + j + 1;
+    for (j = 0; j < N; ++j)
+      A[3][j] = 3 * N + j + 1;
+  }
+
+  for (int i = 0; i < 4; ++i) {
+    for (j = 0; j < N; ++j) {
+      if (A[i][j] != i * N + j + 1)
+        ++errors;
     }
   }
 
-  for (int i = 0; i < N; ++i){
-    for (int j = 0; j < N; ++j){
-      sum += A[i][j];
-    }
-  }
-
-  if (sum != (N*N*(N*N-1))/2)
-    return 1;
-  return 0;
+  return errors;
 }
 
 int main() {
