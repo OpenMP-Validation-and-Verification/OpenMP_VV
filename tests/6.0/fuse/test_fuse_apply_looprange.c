950b23122febacc3a682c5ef1015b8876fc2b26f
Andrew Kallai <kallaiandrew@gmail.com>
2024-10-31 13:00:17 -0400

    Put changes to the test for correct description and correct apply modifier.

--- tests/6.0/fuse/test_fuse_apply_looprange.c
+++ tests/6.0/fuse/test_fuse_apply_looprange.c
@@ -1,16 +1,17 @@
-//--------------- test_fuse_looprange.c---------------------------------------//
+//-------------- test_fuse_apply_looprange.c----------------------------------//
 // OpenMP API Version 6.0 August 2024
 // Pg. 868, line 16
 // ***********
 // DIRECTIVE:fuse
-// CLAUSE:looprange
+// CLAUSE:apply, looprange
 // ***********
-// IGNORE IGNORE IGNORE IGNORE IGNORE IGNORE IGNORE IGNORE IGNORE IGNORE IGNORE 
-// The looprange clause for the fuse directive is being tested. The clause takes
-// counts the number of loops (4) and begins fusion at loop 2 (left value). 2
-// loops are specified as the right value, so only 2 consecutive loops will be
-// merged. Thus, the number of remaining loops will be 3. If the array updates
-// occur incorrectly during fusion, the test will fail.
+// The looprange clause for the fuse directive is being tested with the apply
+// clause. The looprange clause takes count of the number of loops (4) and
+// begins fusion at loop 2 (left value). 2 loops are specified as the right
+// value, so only 2 consecutive loops will be merged. Thus, the number of
+// remaining loops will be 3. The apply clause performs an unroll transformation
+// on the fused loop resulting from the application of looprange. If the array
+// updates occur incorrectly during fusion, the test will fail.
 //----------------------------------------------------------------------------//
 #include "ompvv.h"
 
@@ -22,7 +23,8 @@ int test_apply_looprange() {
   int j;
 
 //  #pragma omp tile sizes(4) apply(grid: unroll)
-  #pragma omp tile sizes(4) apply(grid: unroll) looprange(2, 2)
+//  #pragma omp tile sizes(4) apply(grid: unroll) looprange(2, 2)
+  #pragma omp fuse apply(fused : unroll) looprange(2, 2)
   {
     for (j = 0; j < N; ++j)
       A[0][j] = j + 1;
