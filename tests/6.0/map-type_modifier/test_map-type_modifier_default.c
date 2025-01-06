19eab76aa0b0a17dd72a49757d9980f3b3a50594
Andrew Kallai <kallaiandrew@gmail.com>
2025-01-06 17:16:16 -0500

    Adding the test file.

---
+++ tests/6.0/map-type_modifier/test_map-type_modifier_default.c
@@ -0,0 +1,35 @@
+//--------------test_map-type_modifier_default.c-----------------------------------//
+// OpenMP API Version 6.0 November 2024
+// Pg. 899, line 12
+// ***********
+// DIRECTIVE:target
+// CLAUSE:map
+// ***********
+// The default properties behavior of the map-type modifier supplied to the map
+// clause is being tested. Providing two arguments (to and from) ensures that
+// more than one modifier can be supplied at once to map. This is in accordance
+// with the modifier defaults column in Table 5.1 on page 159. If a tofrom
+// behavior is observed at runtime, then the test passes.
+//---------------------------------------------------------------------------------//
+#include "ompvv.h"
+
+#define N 2
+
+int test_maptype_modifier_default() {
+  int errors = 0, a = 1;
+
+  #pragma omp target map(to: a, from: a)
+  {
+    a = a * N;
+  }
+
+  OMPVV_TEST_AND_SET_VERBOSE(errors, a != N)
+  return errors;
+}
+
+int main() {
+  int errors = 0;
+  OMPVV_TEST_AND_SET(errors, test_maptype_modifier_default() != 0);
+  OMPVV_REPORT_AND_RETURN(errors);
+  return errors;
+}
