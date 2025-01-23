c56f83011d1efc9814e446d11a9cddb838783913
rjenaa <54691205+rjenaa@users.noreply.github.com>
2025-01-23 13:05:20 -0500

    New 6.0 test for map with self modifier (#847)
    * initial test for self map modifier
    
    * added explicit to
    
    * added requires unified memory

---
+++ tests/6.0/target/test_target_map_self.c
@@ -0,0 +1,37 @@
+//===---------------------- test_target_map_self.c ---------------------------===//
+//
+// OpenMP API Version 6.0
+// Tests the target directive with the map clause using the self-modifier to ensure
+// proper behavior.
+//
+////===----------------------------------------------------------------------===//
+
+
+#include <omp.h>
+#include <stdio.h>
+#include "ompvv.h"
+
+#pragma omp requires unified_shared_memory
+
+int test_target_map_self(){
+    int errors = 0;
+    int var = 1;
+    int* host_pointer = &var;
+    int* device_pointer = NULL;
+
+    #pragma omp target map(self, to: var) map(from: device_pointer)
+    {
+            device_pointer = &var;
+    }
+
+    OMPVV_TEST_AND_SET_VERBOSE(errors, host_pointer != device_pointer);
+    return errors;
+}
+
+int main(){
+        OMPVV_TEST_OFFLOADING;
+        int errors = 0;
+        OMPVV_TEST_AND_SET_VERBOSE(errors, test_target_map_self() != 0);
+        OMPVV_REPORT_AND_RETURN(errors);
+}
+
