fbf1221afcc7d294c7b3d7c025860506edb6a6fc
rjenaa <rjen@udel.edu>
2025-01-23 10:01:37 -0800

    changed variant

--- tests/6.0/target/test_target_teams_groupprivate_devicetype_nohost.c
+++ tests/6.0/target/test_target_teams_groupprivate_devicetype_nohost.c
@@ -13,41 +13,34 @@
 
 #define NUM_TEAMS 4
 
-#pragma omp declare target
 int group_sum;
-#pragma omp groupprivate(group_sum) device_type(nohost)
-#pragma omp end declare target
+//#pragma omp groupprivate(group_sum) device_type(nohost)
 
-int group_sum_host;
-
-int* target_var(){
-    return &group_sum;
+int target_func(){
+    return omp_get_team_num();
 }
 
-#pragma omp declare variant(target_var) match(device={kind(nohost)})
-int* get_group_sum_func() {
-    return &group_sum_host;
+#pragma omp declare variant(target_func) match(device={kind(nohost)})
+int host_func() {
+    return 1;
 }
 
 int test_target_groupprivate_devicetype_nohost(){
     int errors = 0;
-    int host_sum = 0;
     int team_sum = 0;
-
+    int host_sum = 0;
     #pragma omp target teams num_teams(NUM_TEAMS) map(tofrom: team_sum, errors) reduction(+: team_sum)
     
     {
 	if (omp_get_team_num() == 0 && omp_is_initial_device()){
 	    errors++;
         }	    
-	int *group_sum_pointer = get_group_sum_func();
+	group_sum = host_func();
         	
-        *group_sum_pointer = omp_get_team_num();
-
-        team_sum += *group_sum_pointer;
+        team_sum += group_sum;
     
     }
-
+    printf("%d",team_sum);
     for (int i = 0; i < NUM_TEAMS; i++){
         host_sum += i;
     }
