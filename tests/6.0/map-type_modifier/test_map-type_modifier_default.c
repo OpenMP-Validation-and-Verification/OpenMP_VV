69dcf0a426b3d89e3f5f6981a1af6d874deba7fd
Andrew Kallai <kallaiandrew@gmail.com>
2025-02-27 13:19:36 -0500

    Adding the changes with present.

--- tests/6.0/map-type_modifier/test_map-type_modifier_default.c
+++ tests/6.0/map-type_modifier/test_map-type_modifier_default.c
@@ -6,10 +6,10 @@
 // CLAUSE:map
 // ***********
 // The default properties behavior of the map-type modifier supplied to the map
-// clause is being tested. Providing two arguments (to and from) ensures that
-// more than one modifier can be supplied at once to map. This is in accordance
-// with the modifier defaults column in Table 5.1 on page 159. If a tofrom
-// behavior is observed at runtime, then the test passes.
+// clause is being tested. Providing the present modifier and a from modifier
+// ensures that more than one modifier can be supplied at once to map. This is
+// in accordance with the modifier defaults column in Table 5.1 on page 159. If
+// a tofrom behavior is observed at runtime, then the test passes.
 //---------------------------------------------------------------------------------//
 #include "ompvv.h"
 
@@ -18,10 +18,13 @@
 int test_maptype_modifier_default() {
   int errors = 0, a = 1;
 
-  #pragma omp target map(present, tofrom:a)
+  #pragma omp target enter data map(to : a)
+
+  #pragma omp target map(present, from : a)
   {
     a = a * N;
   }
+  #pragma omp target update from(a)
 
   OMPVV_TEST_AND_SET_VERBOSE(errors, a != N)
   return errors;
