c5ea382d1d8d96a3363d99159521ba82173138af
rjenaa <rjen@udel.edu>
2025-04-10 05:36:57 -0700

    Updated to modify on target and host

--- tests/6.0/target/test_target_map_self.c
+++ tests/6.0/target/test_target_map_self.c
@@ -1,8 +1,11 @@
 //===---------------------- test_target_map_self.c ---------------------------===//
 //
 // OpenMP API Version 6.0
-// Tests the target directive with the map clause using the self-modifier to ensure
-// proper behavior.
+// Tests the target directive with map(self:) clause to verify proper behavior of the
+// self map-type modifier with scalar variables. This test confirms that the memory 
+// address is identical on both host and device, and that data modifications are 
+// visible across both environments without data copying, demonstrating correct 
+// shared memory operation in a unified shared memory environment.
 //
 ////===----------------------------------------------------------------------===//
 
@@ -11,25 +14,24 @@
 #include <stdio.h>
 #include "ompvv.h"
 
-#define N 1024
-
 #pragma omp requires unified_shared_memory
 
 int test_target_map_self(){
     int errors = 0;
-    int arr[N];
+    int var = 1;
     int* host_pointer = &var;
     int* device_pointer = NULL;
-    
-    for(int i = 0; i < N; i++){
-        arr[i] = i;
-    }
-    
+
     #pragma omp target map(self: var) map(from: device_pointer)
     {
-            device_pointer = &var;
+        var += 2;
+        device_pointer = &var;
     }
 
+    var *= 10;
+    
+    OMPVV_TEST_AND_SET_VERBOSE(errors, var != 30);
+   
     OMPVV_TEST_AND_SET_VERBOSE(errors, host_pointer != device_pointer);
     return errors;
 }
