29bd8edaf1d6729d14ac614468e81bf4637ffad1
rjenaa <rjen@udel.edu>
2025-05-01 10:39:47 -0700

    updated to use exit data

--- tests/6.0/target/test_target_access.c
+++ tests/6.0/target/test_target_access.c
@@ -2,7 +2,8 @@
 //
 // OpenMP API Version 6.0
 // Tests that the target_access single allocator trait works
-// as intended for basic device-host operations and multiple device access.
+// as intended for basic device-host operations and multiple 
+// device access.
 //
 //===----------------------------------------------------===//
 #include <omp.h>
@@ -11,69 +12,68 @@
 #define N 16
 
 int test_target_access_single() {
-    int errors = 0;
-    int *array;
-    int num_devices = omp_get_num_devices();
-    
-    
-    OMPVV_TEST_AND_SET_VERBOSE(errors, num_devices < 2);
-    if (num_devices < 2) {
-        OMPVV_ERROR("This test requires at least 2 devices, but only %d found", num_devices);
-        return errors;
-    }
-    
-    int first_dev = 0;
-    int second_dev = 1; 
-    
-    omp_alloctrait_t trait = { omp_atk_target_access, omp_atv_single };
-    omp_allocator_handle_t single_allocator = omp_init_allocator(omp_default_mem_space, 1, &trait);
-    
-    array = (int*)omp_alloc(N * sizeof(int), single_allocator);
-    
-    for (int i = 0; i < N; i++) {
-        array[i] = i;
-    }
-    
-    #pragma omp target data map(to: array[:N]) device(first_dev)
-    {
-        #pragma omp target is_device_ptr(array) device(first_dev)
-        {
-            for (int i = 0; i < N; i++) {
-                array[i] *= 2;
-            }
-        }
-        
-        #pragma omp target update from(array[:N]) device(first_dev)
-    }
-    
-    #pragma omp target data map(to: array[:N]) device(second_dev)
-    {
-        #pragma omp target is_device_ptr(array) device(second_dev)
-        {
-            for (int i = 0; i < N; i++) {
-                array[i] += 5; 
-            }
-        }
-        
-        #pragma omp target update from(array[:N]) device(second_dev)
-    }
-    
-    for (int i = 0; i < N; i++) {
-        OMPVV_TEST_AND_SET_VERBOSE(errors, array[i] != (i * 2) + 5);
-    }
-    
-    omp_free(array, single_allocator);
-    omp_destroy_allocator(single_allocator);
-    
+  int errors = 0;
+  int *array;
+  int num_devices = omp_get_num_devices();
+  
+  OMPVV_TEST_AND_SET_VERBOSE(errors, num_devices < 2);
+  OMPVV_WARNING_IF(num_devices < 2, "Test requires at least 2 devices, but only %d found.", num_devices);
+  if (num_devices < 2) {
     return errors;
+  }
+  
+  int first_dev = 0;
+  int second_dev = 1;
+  
+  omp_alloctrait_t trait = { omp_atk_target_access, omp_atv_single };
+  omp_allocator_handle_t single_allocator = omp_init_allocator(omp_default_mem_space, 1, &trait);
+  
+  array = (int*)omp_alloc(N * sizeof(int), single_allocator);
+  
+  for (int i = 0; i < N; i++) {
+    array[i] = i;
+  }
+  
+  // Using first device
+  #pragma omp target enter data map(to: array[:N]) device(first_dev)
+  
+  #pragma omp target is_device_ptr(array) device(first_dev)
+  {
+    for (int i = 0; i < N; i++) {
+      array[i] = i * N;
+    }
+  }
+  
+  #pragma omp target exit data map(delete: array[:N]) device(first_dev)
+  
+  // Using second device
+  #pragma omp target enter data map(to: array[:N]) device(second_dev)
+  
+  #pragma omp target is_device_ptr(array) device(second_dev)
+  {
+    for (int i = 0; i < N; i++) {
+      array[i] += 5; 
+    }
+  }
+  
+  #pragma omp target exit data map(delete: array[:N]) device(second_dev)
+  
+  for (int i = 0; i < N; i++) {
+    OMPVV_TEST_AND_SET_VERBOSE(errors, array[i] != i * N + 5);
+  }
+  
+  omp_free(array, single_allocator);
+  omp_destroy_allocator(single_allocator);
+  
+  return errors;
 }
 
 int main() {
-    OMPVV_TEST_OFFLOADING;
-    
-    int errors = 0;
-    
-    OMPVV_TEST_AND_SET_VERBOSE(errors, test_target_access_single() != 0);
-    
-    OMPVV_REPORT_AND_RETURN(errors);
+  OMPVV_TEST_OFFLOADING;
+  
+  int errors = 0;
+  
+  OMPVV_TEST_AND_SET_VERBOSE(errors, test_target_access_single() != 0);
+  
+  OMPVV_REPORT_AND_RETURN(errors);
 }
