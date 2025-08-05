498b8574c1e58bfecc2f74d1b7b222d7c9019d93
Andrew Kallai <kallai.andrew@gmail.com>
2025-08-05 13:19:11 -0700

    Updated test comment block and formatting.

--- tests/6.0/parallel/test_parallel_safesync.c
+++ tests/6.0/parallel/test_parallel_safesync.c
@@ -1,4 +1,4 @@
-//--------------- test_directive_clause.c ------------------------------------//
+//--------------- test_parallel_safesync.c------------------------------------//
 // OpenMP API Version 6.0 November 2024
 // Pg. 901, line 9
 // ***********
@@ -6,7 +6,7 @@
 // CLAUSE:safesync
 // ***********
 // Code is adapted from the safesync clause example in the 6.0 examples
-// document. This example uses a ticket lock implementation to which executes in
+// document. This example uses a ticket lock implementation which executes in
 // the target region. Certain offloading implementations may be unable to
 // properly synchronize the threads pertaining to different progress groups at
 // the end of the parallel region. The result of this can be deadlock. The
@@ -26,25 +26,25 @@ int test_directive() {
 
   for (int i = 0; i < N; i++)
     b[i] = i;
-  // clang-format off
-  #pragma omp target thread_limit(OMPVV_NUM_THREADS_DEVICE) map(to: count1, count2) map(a) map(to:b)
+
+  #pragma omp target thread_limit(OMPVV_NUM_THREADS_DEVICE) \
+    map(to : count1, count2) map(a) map(to : b)
   #pragma omp parallel num_threads(OMPVV_NUM_THREADS_DEVICE) safesync(1)
   {
     int t, u;
     #pragma omp atomic capture
     t = ++count1;
     do {
-        #pragma omp atomic read acquire
-        u = count2;
+      #pragma omp atomic read acquire
+      u = count2;
     } while (u < t);
 
     for (int i = 0; i < N; i++) {
-        a[i] += b[i];
+      a[i] += b[i];
     }
     #pragma omp atomic release
     count2++;
   }
-  // clang-format on
 
   for (int i = 0; i < N; i++) {
     if (a[i] != OMPVV_NUM_THREADS_DEVICE * b[i]) {
