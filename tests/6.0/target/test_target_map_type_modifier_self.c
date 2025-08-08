f42cf4c399ee32678866cef204bc9974605b7ccf
Andrew Kallai <kallai.andrew@gmail.com>
2025-08-08 10:40:01 -0700

    Removed update of scalar to avoid seg fault

--- tests/6.0/target/test_target_map_type_modifier_self.c
+++ tests/6.0/target/test_target_map_type_modifier_self.c
@@ -8,8 +8,7 @@
 // This test checks that the map type-modifer self correctly refers to the
 // original list-item when using the corresponding list-item on the device (the
 // mapped scalar value). Thus, the test will pass if the memory address of the
-// object on the device is the same as the host address. It is also required
-// that the scalar_value variable is updated accordingly for the test to pass.
+// object on the device is the same as the host address.
 //----------------------------------------------------------------------------//
 #include "ompvv.h"
 #include <omp.h>
@@ -22,16 +21,12 @@ int test_map_type_modifier_self() {
 
   #pragma omp target map(self : scalar_value) map(tofrom : device_address)
   {
-    scalar_value += 1;
     device_address = &scalar_value;
   }
 
-  OMPVV_ERROR_IF(scalar_value != 1, "Expected %i, received %i", 1,
-                 scalar_value);
   OMPVV_ERROR_IF(device_address != host_address, "Expected %p, received %p",
                  host_address, device_address);
-  OMPVV_TEST_AND_SET(errors,
-                     (scalar_value != 1) || (device_address != host_address));
+  OMPVV_TEST_AND_SET(errors, (device_address != host_address));
   return errors;
 }
 
