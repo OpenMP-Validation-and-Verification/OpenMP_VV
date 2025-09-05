3f3243476ac75b2fe7a11e86e3ca0ab53b2a87c9
Andrew Kallai <kallai.andrew@gmail.com>
2025-09-05 09:21:47 -0700

    Version two

--- tests/6.0/target/test_target_data.c
+++ tests/6.0/target/test_target_data.c
@@ -1,38 +1,39 @@
-//--------------- test_directive_clause.c ------------------------------------//
-// OpenMP API Version //Number Month Year
-// Pg. X, line X
+//--------------- test_target_data.c -----------------------------------------//
+// OpenMP API Version 6.0 November 2024
+// Pg. 902, line 4
 // ***********
-// DIRECTIVE:
-// CLAUSE:
+// DIRECTIVE:target
+// CLAUSE:data
 // ***********
 // DESCRIPTION
 //----------------------------------------------------------------------------//
 #include "ompvv.h"
-// #include <omp.h>
-
-int test_directive() {
+#include <omp.h>
+int work_function(int var){
+  return ++var;
+}
+int test_target_data() {
   int errors = 0;
   int value = 0;
+  int a = 0;
 
   // clang-format off
-  #pragma omp target_data map(a)
+  #pragma omp target_data map(tofrom: a)
   {
-    #pragma omp target map(a) nowait
-    do_stuff_with_a(a);
+    #pragma omp target map(tofrom: a) nowait
+    work_function(a)
   }
   // clang-format on
 
-  OMPVV_ERROR_IF(value != 0, "Expected %i, received %i", 0, value);
-  OMPVV_TEST_AND_SET(errors, value != 0);
+  OMPVV_ERROR_IF(a != 1, "Expected %i, received %i", 1, a);
+  OMPVV_TEST_AND_SET(errors, value != 1);
   return errors;
 }
 
 int main() {
   int errors = 0;
-  // requires omp.h
   OMPVV_TEST_OFFLOADING;
   OMPVV_TEST_AND_SET(errors, test_directive() != 0);
-  // omp_display_env(1);
   OMPVV_REPORT_AND_RETURN(errors);
   return errors;
 }
