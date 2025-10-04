c2ee0b441ee45d1204c8e79b71af0c16469f30de
Romain Pereira <rpereira@anl.gov>
2025-10-04 02:35:32 +0000

    Added tests for the transparent clause

---
+++ tests/6.0/task/test_task_transparent_export.c
@@ -0,0 +1,50 @@
+//===-- test_taskgraph.c ------------------------------------------------===//
+//
+// OpenMP API Version 6.0 Nov 2024
+//
+// Description
+// testTaskTransparentExport():
+// Ensure the correct order of execution an exporting task and its uncle task
+//===----------------------------------------------------------------------===//
+
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <omp.h>
+#include "ompvv.h"
+
+int testTaskTransparentExport(void)
+{
+    int errors = 0;
+    int x = 0;
+
+    #pragma omp parallel shared(x)
+    {
+        # pragma omp single
+        {
+            # pragma omp task shared(x, errors) transparent(omp_export)
+            {
+                # pragma omp task shared(x, errors) depend(out: x)
+                {
+                    OMPVV_TEST_AND_SET_VERBOSE(errors, x == 0);
+                    ++x;
+                }
+            }
+
+            # pragma omp task shared(x, errors) depend(in: x)
+            {
+                OMPVV_TEST_AND_SET_VERBOSE(errors, x == 1);
+                ++x;
+            }
+        }
+    }
+    OMPVV_TEST_AND_SET_VERBOSE(errors, x == 2);
+    return errors;
+}
+
+int main(void)
+{
+    int errors = 0;
+    OMPVV_TEST_AND_SET_VERBOSE(errors, testTaskTransparentExport());
+    OMPVV_REPORT_AND_RETURN(errors);
+}
---
+++ tests/6.0/task/test_task_transparent_import.c
@@ -0,0 +1,50 @@
+//===-- test_taskgraph.c ------------------------------------------------===//
+//
+// OpenMP API Version 6.0 Nov 2024
+//
+// Description
+// testTaskTransparentImport():
+// Ensure the correct order of execution an importing task and its uncle task
+//===----------------------------------------------------------------------===//
+
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <omp.h>
+#include "ompvv.h"
+
+int testTaskTransparentImport(void)
+{
+    int errors = 0;
+    int x = 0;
+
+    #pragma omp parallel shared(x)
+    {
+        # pragma omp single
+        {
+            # pragma omp task shared(x, errors) depend(out: x)
+            {
+                OMPVV_TEST_AND_SET_VERBOSE(errors, x == 0);
+                ++x;
+            }
+
+            # pragma omp task shared(x, errors) transparent(omp_import)
+            {
+                # pragma omp task shared(x, errors) depend(in: x)
+                {
+                    OMPVV_TEST_AND_SET_VERBOSE(errors, x == 1);
+                    ++x;
+                }
+            }
+        }
+    }
+    OMPVV_TEST_AND_SET_VERBOSE(errors, x == 2);
+    return errors;
+}
+
+int main(void)
+{
+    int errors = 0;
+    OMPVV_TEST_AND_SET_VERBOSE(errors, testTaskTransparentImport());
+    OMPVV_REPORT_AND_RETURN(errors);
+}
