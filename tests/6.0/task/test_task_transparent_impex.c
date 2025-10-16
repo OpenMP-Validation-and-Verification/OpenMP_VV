0578c0ff97bf254e67981548a4143477fa80466f
Swaroop Pophale <swaroop.pophale23@gmail.com>
2025-10-16 13:57:07 -0400

    Update tests/6.0/task/test_task_transparent_impex.c

--- tests/6.0/task/test_task_transparent_impex.c
+++ tests/6.0/task/test_task_transparent_impex.c
@@ -8,8 +8,6 @@
 //===----------------------------------------------------------------------===//
 
 #include <stdio.h>
-#include <assert.h>
-#include <stdlib.h>
 #include <omp.h>
 #include "ompvv.h"
 
