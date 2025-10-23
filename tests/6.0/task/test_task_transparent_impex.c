49ad6cbd5281c461447c3c18272e4371496b8981
PEREIRA Romain <romain.pereira@outlook.com>
2025-10-23 12:16:43 -0500

    Transparent clause on task construct in C (#887)
    * Added tests for the transparent clause
    
    * all conditions were inverted, fixed it
    
    * Update tests/6.0/task/test_task_transparent.c
    
    * Update tests/6.0/task/test_task_transparent.c
    
    * Update tests/6.0/task/test_task_transparent.c
    
    * Update tests/6.0/task/test_task_transparent_export.c
    
    * Update tests/6.0/task/test_task_transparent_export.c
    
    * Update tests/6.0/task/test_task_transparent_import.c
    
    * Update tests/6.0/task/test_task_transparent_export.c
    
    * Update tests/6.0/task/test_task_transparent_impex.c
    
    * Update tests/6.0/task/test_task_transparent_import.c
    
    * Update tests/6.0/task/test_task_transparent_import.c
    
    * Update tests/6.0/task/test_task_transparent_impex.c
    
    * Update tests/6.0/task/test_task_transparent_impex.c
    
    * Update tests/6.0/task/test_task_transparent_export.c
    
    Co-authored-by: Seyong Lee <lees2@ornl.gov>
    
    * Update tests/6.0/task/test_task_transparent_import.c
    
    Co-authored-by: Seyong Lee <lees2@ornl.gov>
    
    ---------
    
    Co-authored-by: Romain Pereira <rpereira@anl.gov>
    Co-authored-by: Swaroop Pophale <swaroop.pophale23@gmail.com>
    Co-authored-by: Seyong Lee <lees2@ornl.gov>

---
+++ tests/6.0/task/test_task_transparent_impex.c
@@ -0,0 +1,51 @@
+//===-- test_task_transparent_impex.c ------------------------------------------------===//
+//
+// OpenMP API Version 6.0 Nov 2024
+//
+// Description
+// testTaskTransparentImpex():
+// Ensure the correct order of execution of cousin tasks, with impex parents
+//===----------------------------------------------------------------------===//
+
+#include <stdio.h>
+#include <omp.h>
+#include "ompvv.h"
+
+int testTaskTransparentImpex(void)
+{
+    int errors = 0;
+    int x = 0;
+
+    #pragma omp parallel shared(x) num_threads(OMPVV_NUM_THREADS_HOST)
+    {
+        # pragma omp single
+        {
+            # pragma omp task shared(x, errors) transparent(omp_impex)
+            {
+                # pragma omp task shared(x, errors) depend(out: x)
+                {
+                    OMPVV_TEST_AND_SET_VERBOSE(errors, x != 0);
+                    ++x;
+                }
+            }
+
+            # pragma omp task shared(x, errors) transparent(omp_impex)
+            {
+                # pragma omp task shared(x, errors) depend(out: x)
+                {
+                    OMPVV_TEST_AND_SET_VERBOSE(errors, x != 1);
+                    ++x;
+                }
+            }
+        }
+    }
+    OMPVV_TEST_AND_SET_VERBOSE(errors, x != 2);
+    return errors;
+}
+
+int main(void)
+{
+    int errors = 0;
+    OMPVV_TEST_AND_SET_VERBOSE(errors, testTaskTransparentImpex());
+    OMPVV_REPORT_AND_RETURN(errors);
+}
