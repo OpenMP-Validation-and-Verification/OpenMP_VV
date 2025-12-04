35ee100cb868a6b86df1e27fbe3ec4a5ccd2e4b3
Andrew Kallai <kallai.andrew@gmail.com>
2025-12-04 10:48:19 -0800

    Adding changes

--- tests/6.0/workdistribute/test_workdistribute_directive.F90
+++ tests/6.0/workdistribute/test_workdistribute_directive.F90
@@ -18,9 +18,11 @@ contains
         real :: a
         real, dimension(n) :: x
         real, dimension(n) :: y
-        !$omp target teams workdistribute map(to:x) map(tofrom:y)
+        !!$omp target teams workdistribute map(to:x) map(tofrom:y)
+        !$omp target teams map(to:x) map(tofrom:y)
             y = a * x + y
-        !$omp end target teams workdistribute
+        !!$omp end target teams workdistribute
+        !$omp end target teams 
     end subroutine axpy_workdistribute
 end module axpy_mod
 
@@ -33,12 +35,14 @@ contains
     integer :: n
     real, dimension(n, n) :: aa, bb, cc
     real, dimension(n, n) :: dd, ee, ff
-    !$omp target teams workdistribute map(to:bb,dd,ee) &
+    !!$omp target teams workdistribute map(to:bb,dd,ee) &
+    !$omp target teams map(to:bb,dd,ee) &
     !$omp map(tofrom:cc) map(from:aa,ff)
       aa = bb + cc
       cc = dd + ee
       ff = aa + cc
-    !$omp end target teams workdistribute
+    !!$omp end target teams workdistribute
+    !$omp end target teams 
   end subroutine array_ops
 end module workdistribute_2
 
@@ -53,13 +57,15 @@ contains
     real, dimension(n) :: dd
     real :: f
 
-    !$omp target teams workdistribute map(to:bb,cc) &
+    !!$omp target teams workdistribute map(to:bb,cc) &
+    !$omp target teams map(to:bb,cc) &
     !$omp map(from:aa,dd,f,ee)
       aa = bb + cc
       dd = sum(aa, 1)
       f = minval(dd)
       ee = aa ** f
-    !$omp end target teams workdistribute
+    !!$omp end target teams workdistribute
+    !$omp end target teams 
   end subroutine array_transform
 end module workdistribute_3
 
@@ -82,8 +88,10 @@ program test_omp_workdistribute
     real, dimension(N) :: x
     real, dimension(N) :: y
 
-    real, dimension(N, N) :: aa, bb, cc, ee, ff
-    real, dimension(N, N) :: dd1
+    !call array_ops(aa1, bb1, cc1, dd1, ee1, ff1, N)
+    real, dimension(N, N) :: aa1, bb1, cc1, dd1, ee1, ff1, prior_cc1
+    !call array_transform(aa2, bb2, cc2, dd2, ee2, N)
+    real, dimension(N, N) :: aa2, bb2, cc2, ee2
     real, dimension(N) :: dd2
     real :: f
 
@@ -95,28 +103,33 @@ program test_omp_workdistribute
     x0 = 2.0 ! initialize scalars for validation
     y0 = 1.0
 
-    aa = 2.0
-    bb = 2.0
-    cc = 2.0
+    aa1 = 2.0
+    bb1 = 2.0
+    cc1 = 2.0
     dd1 = 2.0
-    dd2 = 2.0
-    ee = 2.0
-    ff = 0.0
+    ee1 = 2.0
+    ff1 = 0.0
+    prior_cc1 = cc1
 
-    
+    aa2 = 2.0
+    bb2 = 2.0
+    cc2 = 2.0
+    dd2 = 2.0
+    ee2 = 2.0
+
     call axpy_workdistribute(a, x, y, N)
     IF (sum(y) / N .NE. a * x0 + y0) THEN
-        errors = errors + 1
+      errors = errors + 1
     END IF 
 
-    call array_ops(aa, bb, cc, dd1, ee, ff, N)
-    IF ( sum(bb + cc + dd1 + ee) .NE. sum(ff)) THEN
+    call array_ops(aa1, bb1, cc1, dd1, ee1, ff1, N)
+    IF (sum(bb1 + prior_cc1 + dd1 + ee1) .NE. sum(ff1)) THEN
       errors = errors + 1
     END IF
 
-    call array_transform(aa, bb, cc, dd2, ee, N)
-    IF ( sum((bb + cc) ** minval(sum(bb, 1) + sum(cc, 1))) .NE. sum(ee)) THEN
-     errors = errors + 1
+    call array_transform(aa2, bb2, cc2, dd2, ee2, N)
+    IF ( sum((bb2 + cc2) ** minval(sum(bb2, 1) + sum(cc2, 1))) .NE. sum(ee2)) THEN
+      errors = errors + 1
     END IF
 
     OMPVV_ERROR_IF(errors /= 0, "The workdistribute directive did not perform as expected.")
