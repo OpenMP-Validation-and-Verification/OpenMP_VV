47ebdb21a20000430ee47ca69683102c92a7d5f4
Andrew Kallai <kallai.andrew@gmail.com>
2026-01-21 18:28:13 -0800

    Updated comment block

--- tests/6.0/modifiers/test_directive_name_modifier.c
+++ tests/6.0/modifiers/test_directive_name_modifier.c
@@ -1,36 +1,35 @@
-//--------------- test_directive_name_modifier.c -----------------------------------------//
+//--------------- test_directive_name_modifier.c -----------------------------//
 // OpenMP API Version 6.0 November 2024
-// Pg. 902, line 4
-// DIRECTIVE:CHANGEtarget
-// CLAUSE:data
-// The target_data directive is being used in three scenaries of various task
-// configurations for the same functional task. Because target_data is a
-// task-generating composite construct, it will have expected effects that are
-// being tested in each scenario. If the work_function executes correctly, then
-// the value of a should expectedly change to 1.
+// Pg. 898, line 3
+// CLAUSE:if, private
+// All modifiers to clauses were extended to accept directive-name-modifiers.
+// In this case, the private clause is being used with the parallel
+// directive-name-modifier. If the threads correctly privatize thread_id,
+// then the test will pass.
 //----------------------------------------------------------------------------//
+
 #include "ompvv.h"
 #include <omp.h>
 
 #define N OMPVV_NUM_THREADS_HOST
 
-
 int test_directive_name_modifier() {
   int errors = 0;
   int thread_array[N] = {0};
   int thread_id = -1;
-  #pragma omp parallel for private(parallel: thread_id)
+
+#pragma omp parallel for private(parallel : thread_id)
   {
-    for (int i=0; i<N; i++) {
-        thread_id = omp_get_thread_num();
-        thread_array[thread_id]=thread_id
+    for (int i = 0; i < N; i++) {
+      thread_id = omp_get_thread_num();
+      thread_array[thread_id] = thread_id;
     }
   }
 
-  for (int i=0; i<N; ++i) {
-    for (int j=i+1; j<N; ++j){
-        if (thread_array[i] == thread_array[j])
-            errors++;
+  for (int i = 0; i < N; ++i) {
+    for (int j = i + 1; j < N; ++j) {
+      if (thread_array[i] == thread_array[j])
+        errors++;
     }
   }
 
