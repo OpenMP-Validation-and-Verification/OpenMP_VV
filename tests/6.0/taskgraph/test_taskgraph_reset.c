8d2b0e876ba991c89092947749e6656249b7eefd
PEREIRA Romain <romain.pereira@outlook.com>
2026-02-05 12:31:46 -0600

    Taskgraphs - initial tests (#886)
    * [wip] Adding tests for taskgraphs
    
    * Warning on static assert
    
    * Replaced some target tasks with host task, and set shared where appropriated
    
    * all conditions were inverted, fixed it
    
    * Update tests/6.0/taskgraph/test_taskgraph_if.c
    
    Co-authored-by: Swaroop Pophale <swaroop.pophale23@gmail.com>
    
    * Fix structured block execution tests
    
    The spec says: '[...] Execution of the structured block associated with the construct, while optionally creating a taskgraph record of all encountered replayable constructs and the sequence in which they are encountered; or [...]'
    
    Tests now check that the structured block executed at least once, and no more than 'N' times in iterative taskgraph calls
    
    * Update tests/6.0/taskgraph/test_taskgraph_id.c
    
    Co-authored-by: Swaroop Pophale <swaroop.pophale23@gmail.com>
    
    * Update tests/6.0/taskgraph/test_taskgraph_id.c
    
    Co-authored-by: Swaroop Pophale <swaroop.pophale23@gmail.com>
    
    * Added a missing '!' and fixed comment
    
    * Update tests/6.0/taskgraph/test_taskgraph_if.c
    
    Co-authored-by: Swaroop Pophale <swaroop.pophale23@gmail.com>
    
    * Update tests/6.0/taskgraph/test_taskgraph_parallel.c
    
    Co-authored-by: Swaroop Pophale <swaroop.pophale23@gmail.com>
    
    * See https://github.com/OpenMP-Validation-and-Verification/OpenMP_VV/pull/886\#discussion_r2695299881
    
    * Update tests/6.0/taskgraph/test_taskgraph_nogroup.c
    
    Co-authored-by: Swaroop Pophale <swaroop.pophale23@gmail.com>
    
    * Updated test_taskgraph so it only tests for compiler support without any semantical consideration
    
    * Fixed checking: the taskgraph record may never be created (then x == N*nthreads); or created prior to first region encounter (then x == 0)
    
    * Fix tests that assumed the taskgraph structured block should execute at least once: it may not be the case if it created prior to its first encounter by the compiler
    
    * Update tests/6.0/taskgraph/test_taskgraph_heterogeneous.c
    
    Co-authored-by: Swaroop Pophale <swaroop.pophale23@gmail.com>
    
    * Removed test_taskgraph, since compiler support will already be tested in every other tests
    
    * Removed test_taskgraph_parallel.c - we need clarifications on having multiple threads concurrently create/replay the same taskgraph record
    
    * Renaming to test_taskgraph_target.c
    
    * Rename test_taskgraph_replay.c to test_taskgraph.c
    
    ---------
    
    Co-authored-by: Romain Pereira <rpereira@anl.gov>
    Co-authored-by: Swaroop Pophale <swaroop.pophale23@gmail.com>

---
+++ tests/6.0/taskgraph/test_taskgraph_reset.c
@@ -0,0 +1,71 @@
+//===-- test_taskgraph_reset.c ------------------------------------------------===//
+//
+// OpenMP API Version 6.0 Nov 2024
+//
+// Description
+// testTaskgraphGraphReset():
+// Run the same taskgraph constructs N times, that spawns 3 tasks
+//  T1 -> T2 -> T3
+// with (T1, T3) on device, (T2) on host.
+// while reseting every second iterations.
+//
+// Ensures that structured block executed N/2 to N times
+//===----------------------------------------------------------------------===//
+
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <omp.h>
+#include "ompvv.h"
+
+int testTaskgraphGraphReset(void)
+{
+    int errors = 0;
+
+    # define N 16
+    _Static_assert(N % 2 == 0, "N must be even");
+
+    int x = 0;
+    int y = 0;
+
+    # pragma omp parallel shared(x, y)
+    {
+        # pragma omp single
+        {
+            for (int i = 0 ; i < N ; ++i)
+            {
+                # pragma omp taskgraph graph_reset(i % 2 == 0)
+                {
+                    # pragma omp atomic
+                        ++x;
+
+                    for (int i = 0 ; i < 3 ; ++i)
+                    {
+                        # pragma omp task depend(out: y) shared(y)
+                        {
+                            # pragma omp atomic
+                                ++y;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // taskgraph structured block must have executed at least N/2 times
+    // (if always creating a taskgraph record on the first taskgraph construct encounter),
+    // and at most N times (if never creating a taskgraph record)
+    OMPVV_TEST_AND_SET_VERBOSE(errors, !(N/2 <= x && x <= N));
+
+    // each triplets must have executed N times
+    OMPVV_TEST_AND_SET_VERBOSE(errors, y != 3*N);
+
+    return errors;
+}
+
+int main(void)
+{
+    int errors = 0;
+    OMPVV_TEST_AND_SET_VERBOSE(errors, testTaskgraphGraphReset());
+    OMPVV_REPORT_AND_RETURN(errors);
+}
---
+++ tests/6.0/taskgraph/test_taskgraph_target.c
@@ -0,0 +1,65 @@
+//===-- test_taskgraph_target.c ------------------------------------------------===//
+//
+// OpenMP API Version 6.0 Nov 2024
+//
+// Description
+// testTaskgraphHeterogeneous():
+// Create a taskgraph, spawns 3 dependent tasks
+//  T1 -> T2 -> T3
+// with (T1, T3) on device, (T2) on host.
+//
+// Ensures that
+//   - the structured block is executed 0 to N times
+//      - 0 if taskgraph record created prior to execution
+//      - N if taskgraph record is never created
+//   - 3*N tasks are executed/replayed
+//===----------------------------------------------------------------------===//
+
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <omp.h>
+#include "ompvv.h"
+
+int testTaskgraphHeterogeneous(void)
+{
+    int errors = 0;
+
+    # define N 16
+    int x = 0;
+    int y = 0;
+
+    # pragma omp parallel shared(x, y)
+    {
+        # pragma omp single
+        {
+            for (int i = 0 ; i < N ; ++i)
+            {
+                # pragma omp taskgraph
+                {
+                    ++x;
+
+                    # pragma omp target nowait map(tofrom: y) depend(out: y) shared(y)
+                        ++y;
+
+                    # pragma omp task depend(out: y) shared(y)
+                        ++y;
+
+                    # pragma omp target nowait map(tofrom: y) depend(out: y) shared(y)
+                        ++y;
+                }
+            }
+        }
+    }
+
+    OMPVV_TEST_AND_SET_VERBOSE(errors, !(0 <= x && x <= N));
+    OMPVV_TEST_AND_SET_VERBOSE(errors, y != 3*N);
+    return errors;
+}
+
+int main(void)
+{
+    int errors = 0;
+    OMPVV_TEST_AND_SET_VERBOSE(errors, testTaskgraphHeterogeneous());
+    OMPVV_REPORT_AND_RETURN(errors);
+}
