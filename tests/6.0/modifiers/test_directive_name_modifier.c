780bd5662df85f368a435f3c21535887586203dc
Andrew Kallai <kallai.andrew@gmail.com>
2026-02-19 10:16:51 -0800

    changes

--- tests/6.0/modifiers/test_directive_name_modifier.c
+++ tests/6.0/modifiers/test_directive_name_modifier.c
@@ -12,6 +12,7 @@
 #include <omp.h>
 
 #define N OMPVV_NUM_THREADS_DEVICE
+#define M OMPVV_NUM_TEAMS_DEVICE
 
 int test_directive_name_modifier() {
   int errors = 0;
@@ -19,24 +20,26 @@ int test_directive_name_modifier() {
   int final_shared = 0;
   int i = 0;
 
-  #pragma omp target teams distribute firstprivate(target : shared_value) \
+  //#pragma omp target teams distribute firstprivate(target : shared_value)
+  #pragma omp target teams distribute firstprivate(shared_value) \
     map(from : final_shared)
   for (i = 0; i < N; ++i) {
     #pragma omp atomic update
     shared_value++;
     final_shared = shared_value;
   }
+  OMPVV_TEST_AND_SET(errors, shared_value != 0);
+
   shared_value = 5;
-  int shared_val_arr[OMPVV_NUM_TEAMS_TARGET] = {-1};
-  #pragma omp target teams distribute firstprivate(teams : shared_value) \
-    map(from : shared_val_arr[])
-  for (i = 0; i < N; ++i) {
+  int shared_val_arr[M] = {-1};
+  //#pragma omp target teams distribute firstprivate(teams : shared_value)
+  #pragma omp target teams distribute firstprivate(shared_value) \
+    map(from : shared_val_arr[M])
+  for (i = 0; i < M; ++i) {
     shared_value += omp_get_team_num();
     shared_val_arr[omp_get_team_num()] = shared_value;
   }
-
-  if (shared_value != 0)
-    errors++;
+  OMPVV_TEST_AND_SET(errors, shared_value != 5);
 
   return errors;
 }
