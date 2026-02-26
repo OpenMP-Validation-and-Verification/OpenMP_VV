9d30d72d3f258fabf92bc2363d34b0cb1413a1c0
Andrew Kallai <kallai.andrew@gmail.com>
2026-02-26 11:35:46 -0800

    Adding the changes for error checking and teams

--- tests/6.0/modifiers/test_directive_name_modifier.c
+++ tests/6.0/modifiers/test_directive_name_modifier.c
@@ -20,21 +20,21 @@ int test_directive_name_modifier() {
   int final_shared = 0;
   int i = 0;
 
-  //#pragma omp target teams distribute firstprivate(target : shared_value)
-  #pragma omp target teams distribute firstprivate(shared_value) \
+  #pragma omp target teams distribute firstprivate(target : shared_value) \
     map(from : final_shared)
   for (i = 0; i < N; ++i) {
     #pragma omp atomic update
     shared_value++;
+    #pragma omp atomic write
     final_shared = shared_value;
   }
   OMPVV_TEST_AND_SET(errors, shared_value != 0);
+  OMPVV_TEST_AND_SET(errors, final_shared != N);
 
   shared_value = 5;
   int shared_val_arr[M] = {-1};
-  //#pragma omp target teams distribute firstprivate(teams : shared_value)
-  #pragma omp target teams distribute firstprivate(shared_value) \
-    map(from : shared_val_arr[M])
+  #pragma omp target teams distribute firstprivate(teams : shared_value) \
+    map(from : shared_val_arr)
   for (i = 0; i < M; ++i) {
     shared_value += omp_get_team_num();
     shared_val_arr[omp_get_team_num()] = shared_value;
