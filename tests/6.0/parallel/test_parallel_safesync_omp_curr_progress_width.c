65e5b5e27b65b9d0d5b148b3cfecbc40abcd9e1e
Andrew Kallai <kallai.andrew@gmail.com>
2026-08-06 10:39:06 -0700

    Adding the changes to the logic and the file comments.

--- tests/6.0/parallel/test_parallel_safesync_omp_curr_progress_width.c
+++ tests/6.0/parallel/test_parallel_safesync_omp_curr_progress_width.c
@@ -7,14 +7,12 @@
 // ***********
 // Code is adapted from the safesync clause example in the 6.0 examples
 // document. This example uses a ticket lock implementation which executes in
-// the target region. On certain device architectures, OpenMP threads may not be
-// able to synchronize with each other from logically divergent code. The result
-// of this can be deadlock. The safesync clause is suppossed to ensure that such
-// synchronization is possible. If no deadlock or improper update of the arrays
+// the target region. Only a single thread will execute this region.
+// If no deadlock or improper update of the arrays
 // occurs, then this indicates the safesync clause is working correctly.
-// #########################################################################################
 //----------------------------------------------------------------------------//
 #include "ompvv.h"
+#include <math.h>
 #include <omp.h>
 
 #define N 100
@@ -28,12 +26,12 @@ int test_directive() {
   for (int i = 0; i < N; i++)
     b[i] = i, a[i] = 0;
 
-  
-  #pragma omp target thread_limit(OMPVV_NUM_THREADS_DEVICE) \
+  #pragma omp target \
     map(to : count1, count2) map(a) map(to : b) map(tofrom : num_threads)
-  #pragma omp parallel num_threads(OMPVV_NUM_THREADS_DEVICE) safesync(omp_curr_progress_width)
+  #pragma omp parallel num_threads(OMPVV_NUM_THREADS_DEVICE) \
+    safesync(omp_curr_progress_width)
   {
-    if(omp_get_thread_num() % omp_curr_progress_width == 0){
+    if (omp_get_thread_num() % omp_curr_progress_width == 0) {
       if (omp_get_thread_num() == 0) {
         num_threads = omp_get_num_threads();
       }
@@ -54,7 +52,7 @@ int test_directive() {
   }
 
   for (int i = 0; i < N; i++) {
-    if (a[i] != num_threads * b[i]) {
+    if (a[i] != ceil(num_threads / omp_curr_progress_width) * b[i]) {
       ++errors;
     }
   }
